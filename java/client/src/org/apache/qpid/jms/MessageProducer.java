c33a880ae41693e2e99b5fd36c8134579cd99a94
Steven Shaw <steshaw@apache.org>
2006-09-20 14:25:07 +0000

    Closeable's checkNotClosed now throws a javax.jms.IllegalStateException rather than a java.lang.IllegalStateException.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@448211 13f79535-47bb-0310-9956-ffa450edef68

--- java/client/src/org/apache/qpid/jms/MessageProducer.java
+++ java/client/src/org/apache/qpid/jms/MessageProducer.java
@@ -30,14 +30,14 @@ public interface MessageProducer extends javax.jms.MessageProducer
      * Set the default MIME type for messages produced by this producer. This reduces the overhead of each message.
      * @param mimeType
      */
-    void setMimeType(String mimeType);
+    void setMimeType(String mimeType) throws JMSException;
 
     /**
      * Set the default encoding for messages produced by this producer. This reduces the overhead of each message.
      * @param encoding the encoding as understood by XXXX how do I specify this?? RG
      * @throws UnsupportedEncodingException if the encoding is not understood
      */
-    void setEncoding(String encoding) throws UnsupportedEncodingException;
+    void setEncoding(String encoding) throws UnsupportedEncodingException, JMSException;
     
     void send(Destination destination, Message message, int deliveryMode,
                      int priority, long timeToLive, boolean immediate)
