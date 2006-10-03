48c59abc4bfeb381497a0749e4e9ede011410ae5
Martin Ritchie <ritchiem@apache.org>
2006-10-03 06:47:30 +0000

    Optimised Transaction handling. When using a a transaction don't start a store transaction on a commit UNLESS a persistent message has been sent. As non-persistent messages are not stored.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid/java@452323 13f79535-47bb-0310-9956-ffa450edef68

--- broker/src/org/apache/qpid/server/txn/TxnBuffer.java
+++ broker/src/org/apache/qpid/server/txn/TxnBuffer.java
@@ -17,6 +17,7 @@
  */
 package org.apache.qpid.server.txn;
 
+import org.apache.log4j.Logger;
 import org.apache.qpid.AMQException;
 import org.apache.qpid.server.store.MessageStore;
 
@@ -25,21 +26,32 @@ import java.util.List;
 
 public class TxnBuffer
 {
+    private boolean _persistentMessageRecevied = false;
     private final MessageStore _store;
     private final List<TxnOp> _ops = new ArrayList<TxnOp>();
+    private static final Logger _log = Logger.getLogger(TxnBuffer.class);
 
     public TxnBuffer(MessageStore store)
     {
         _store = store;
     }
 
+    public void setPersistentMessageRecevied()
+    {
+        _persistentMessageRecevied = true;
+    }
+
     public void commit() throws AMQException
     {
-        _store.beginTran();
+        if (_persistentMessageRecevied)
+        {
+            _log.info("Begin Transaction.");
+            _store.beginTran();
+        }
         boolean failed = true;
         try
         {
-            for(TxnOp op : _ops)
+            for (TxnOp op : _ops)
             {
                 op.commit();
             }
@@ -48,20 +60,25 @@ public class TxnBuffer
         }
         finally
         {
-            if(failed)
+            if (_persistentMessageRecevied)
             {
-                _store.abortTran();
-            }
-            else
-            {
-                _store.commitTran();
+                if (failed)
+                {
+                    _log.info("Transaction Failed");
+                    _store.abortTran();
+                }
+                else
+                {
+                    _log.info("Transaction Succeeded");
+                    _store.commitTran();
+                }
             }
         }
     }
 
     public void rollback() throws AMQException
     {
-        for(TxnOp op : _ops)
+        for (TxnOp op : _ops)
         {
             op.rollback();
         }
