5eb36a210ab9e2bed9d06fc0e5a92ffad09cc66a
Martin Ritchie <ritchiem@apache.org>
2006-10-31 13:02:20 +0000

    Updated to programmatically set QPID_WORK for logging, to stop failed builds
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@469470 13f79535-47bb-0310-9956-ffa450edef68

--- java/client/test/src/org/apache/qpid/ack/RecoverTest.java
+++ java/client/test/src/org/apache/qpid/ack/RecoverTest.java
@@ -38,6 +38,13 @@ public class RecoverTest
 
     static
     {
+        String workdir = System.getProperty("QPID_WORK");
+        if (workdir == null || workdir.equals(""))
+        {
+            String tempdir = System.getProperty("java.io.tmpdir");
+            System.out.println("QPID_WORK not set using tmp directory: " + tempdir);
+            System.setProperty("QPID_WORK", tempdir);
+        }
         //DOMConfigurator.configure("../etc/log4j.xml");
         DOMConfigurator.configure("broker/etc/log4j.xml");
     }
@@ -45,7 +52,7 @@ public class RecoverTest
     @After
     public void stopVmBroker()
     {
-       TransportConnection.killVMBroker(1);
+        TransportConnection.killVMBroker(1);
     }
 
     @Test
