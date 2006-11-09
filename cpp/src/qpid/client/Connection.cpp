4a5f4754bd1402a56cf13f3781e8b5e8ea715f6f
Gordon Sim <gsim@apache.org>
2006-11-09 10:29:41 +0000

    Removed invalid and redundant include.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@472844 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/client/Connection.cpp
+++ cpp/src/qpid/client/Connection.cpp
@@ -17,7 +17,6 @@
  */
 #include "qpid/client/Connection.h"
 #include "qpid/client/Channel.h"
-#include "qpid/sys/Connector.h"
 #include "qpid/client/Message.h"
 #include "qpid/QpidError.h"
 #include <iostream>
