5a356cd1ddb15922a7477f7095864a906206b608
Stephen Vinoski <vinoski@apache.org>
2006-11-09 22:20:13 +0000

    add Apache license
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@473097 13f79535-47bb-0310-9956-ffa450edef68

--- bin/set_svn_properties.sh
+++ bin/set_svn_properties.sh
@@ -1,7 +1,24 @@
+#
+#
+# Copyright (c) 2006 The Apache Software Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# this script will set the proper svn properties on all the files in the tree
+# This script will set the proper svn properties on all the files in the tree.
 # It pretty much requires a gnu compatible xargs (for the -r flag).  Running
-# on Linux is probably the best option
+# on Linux is probably the best option, or gnuxargs is available for OS X from
+# darwinports.
 
 
 find . -name "*.java" | grep -v ".svn" | xargs -n 1 -r svn propset svn:eol-style native
