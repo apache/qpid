9856b26ac19cfe6510b799af707dfdac095c8f59
Gordon Sim <gsim@apache.org>
2006-11-27 16:22:27 +0000

    Bug fix, basic_qos_ok was set to wrong type.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@479677 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/client/MethodBodyInstances.h
+++ cpp/src/qpid/client/MethodBodyInstances.h
@@ -40,40 +40,40 @@ public:
 	const qpid::framing::BasicConsumeOkBody		basic_consume_ok;
 	const qpid::framing::BasicDeliverBody		basic_deliver;
 	const qpid::framing::BasicGetEmptyBody		basic_get_empty;
-	const qpid::framing::BasicGetOkBody			basic_get_ok;
-	const qpid::framing::BasicQosBody			basic_qos_ok;
+	const qpid::framing::BasicGetOkBody		basic_get_ok;
+	const qpid::framing::BasicQosOkBody		basic_qos_ok;
 	const qpid::framing::BasicReturnBody		basic_return;
 	const qpid::framing::ChannelCloseBody		channel_close;
-    const qpid::framing::ChannelCloseOkBody		channel_close_ok;
+        const qpid::framing::ChannelCloseOkBody		channel_close_ok;
 	const qpid::framing::ChannelFlowBody		channel_flow;
-    const qpid::framing::ChannelOpenOkBody		channel_open_ok;
-    const qpid::framing::ConnectionCloseBody	connection_close;
-    const qpid::framing::ConnectionCloseOkBody	connection_close_ok;
-    const qpid::framing::ConnectionOpenOkBody	connection_open_ok;
-    const qpid::framing::ConnectionRedirectBody	connection_redirect;
-    const qpid::framing::ConnectionStartBody	connection_start;
-    const qpid::framing::ConnectionTuneBody		connection_tune;
+        const qpid::framing::ChannelOpenOkBody		channel_open_ok;
+        const qpid::framing::ConnectionCloseBody	connection_close;
+        const qpid::framing::ConnectionCloseOkBody	connection_close_ok;
+        const qpid::framing::ConnectionOpenOkBody	connection_open_ok;
+        const qpid::framing::ConnectionRedirectBody	connection_redirect;
+        const qpid::framing::ConnectionStartBody	connection_start;
+        const qpid::framing::ConnectionTuneBody		connection_tune;
 	const qpid::framing::ExchangeDeclareOkBody	exchange_declare_ok;      
 	const qpid::framing::ExchangeDeleteOkBody	exchange_delete_ok;
 	const qpid::framing::QueueDeclareOkBody		queue_declare_ok;
 	const qpid::framing::QueueDeleteOkBody		queue_delete_ok;
 	const qpid::framing::QueueBindOkBody		queue_bind_ok;
-	const qpid::framing::TxCommitOkBody			tx_commit_ok;
+	const qpid::framing::TxCommitOkBody		tx_commit_ok;
 	const qpid::framing::TxRollbackOkBody		tx_rollback_ok;
-	const qpid::framing::TxSelectOkBody			tx_select_ok;
+	const qpid::framing::TxSelectOkBody		tx_select_ok;
 
     MethodBodyInstances(u_int8_t major, u_int8_t minor) :
     	version(major, minor),
-		basic_cancel_ok(version),
-		basic_consume_ok(version),
-		basic_deliver(version),
-		basic_get_empty(version),
-		basic_get_ok(version),
-		basic_qos_ok(version),
-		basic_return(version),
-		channel_close(version),
+        basic_cancel_ok(version),
+        basic_consume_ok(version),
+        basic_deliver(version),
+        basic_get_empty(version),
+        basic_get_ok(version),
+        basic_qos_ok(version),
+        basic_return(version),
+        channel_close(version),
         channel_close_ok(version),
-		channel_flow(version),
+        channel_flow(version),
         channel_open_ok(version),
         connection_close(version),
         connection_close_ok(version),
@@ -81,14 +81,14 @@ public:
         connection_redirect(version),
         connection_start(version),
         connection_tune(version),
-		exchange_declare_ok(version),      
-		exchange_delete_ok(version),
-		queue_declare_ok(version),
-		queue_delete_ok(version),
-		queue_bind_ok(version),
-		tx_commit_ok(version),
-		tx_rollback_ok(version),
-		tx_select_ok(version)
+        exchange_declare_ok(version),      
+        exchange_delete_ok(version),
+        queue_declare_ok(version),
+        queue_delete_ok(version),
+        queue_bind_ok(version),
+        tx_commit_ok(version),
+        tx_rollback_ok(version),
+        tx_select_ok(version)
     {}
 
 };
