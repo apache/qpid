e78bdde1d14cdfb00b7c88e5ad2f133a0cc5db30
Rajith Muditha Attapattu <rajith@apache.org>
2006-12-06 15:56:03 +0000

    This is a fix for QPID-159. These implementations now throw the correct exceptions as required by the spec
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@483129 13f79535-47bb-0310-9956-ffa450edef68

--- java/client/src/main/java/org/apache/qpid/client/TopicPublisherAdapter.java
+++ java/client/src/main/java/org/apache/qpid/client/TopicPublisherAdapter.java
@@ -1,6 +1,7 @@
 package org.apache.qpid.client;
 
 import javax.jms.Destination;
+import javax.jms.IllegalStateException;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageProducer;
@@ -11,6 +12,7 @@ public class TopicPublisherAdapter implements TopicPublisher {
 
 	private MessageProducer delegate;
 	private Topic topic;
+	private boolean closed = false;
 	
 	public TopicPublisherAdapter(MessageProducer msgProducer, Topic topic){
 		delegate = msgProducer;
@@ -18,29 +20,35 @@ public class TopicPublisherAdapter implements TopicPublisher {
 	}
 	
 	public Topic getTopic() throws JMSException {
+		checkPreConditions();
 		return topic;
 	}
 
 	public void publish(Message msg) throws JMSException {
+		checkPreConditions();
 		delegate.send(msg);
 	}
 
 	public void publish(Topic topic, Message msg) throws JMSException {
+		checkPreConditions();
 		delegate.send(topic,msg);
 	}
 
 	public void publish(Message msg, int deliveryMode, int priority, long timeToLive)
 			throws JMSException {
+		checkPreConditions();
 		delegate.send(msg, deliveryMode,priority,timeToLive);
 	}
 
 	public void publish(Topic topic, Message msg, int deliveryMode, int priority, long timeToLive)
 			throws JMSException {
+		checkPreConditions();
 		delegate.send(topic,msg, deliveryMode,priority,timeToLive);
 	}
 
 	public void close() throws JMSException {
 		delegate.close();
+		closed = true;
 	}
 
 	public int getDeliveryMode() throws JMSException {
@@ -68,40 +76,63 @@ public class TopicPublisherAdapter implements TopicPublisher {
 	}
 
 	public void send(Message msg) throws JMSException {
+		checkPreConditions();
 		delegate.send(msg);
 	}
 
 	public void send(Destination dest, Message msg) throws JMSException {
+		checkPreConditions();
 		delegate.send(dest,msg);
 	}
 
 	public void send(Message msg, int deliveryMode, int priority, long timeToLive)
 			throws JMSException {
+		checkPreConditions();
 		delegate.send(msg, deliveryMode,priority,timeToLive);
 	}
 
 	public void send(Destination dest, Message msg, int deliveryMode, int priority, long timeToLive) throws JMSException {
+		checkPreConditions();
 		delegate.send(dest,msg, deliveryMode,priority,timeToLive);
 	}
-
+	
 	public void setDeliveryMode(int deliveryMode) throws JMSException {
+		checkPreConditions();
 		delegate.setDeliveryMode(deliveryMode);
 	}
 
 	public void setDisableMessageID(boolean disableMessageID) throws JMSException {
+		checkPreConditions();
 		delegate.setDisableMessageID(disableMessageID);
 	}
 
 	public void setDisableMessageTimestamp(boolean disableMessageTimestamp) throws JMSException {
+		checkPreConditions();
 		delegate.setDisableMessageTimestamp(disableMessageTimestamp);
 	}
 
 	public void setPriority(int priority) throws JMSException {
+		checkPreConditions();
 		delegate.setPriority(priority);
 	}
 
 	public void setTimeToLive(long timeToLive) throws JMSException {
+		checkPreConditions();
 		delegate.setTimeToLive(timeToLive);
 	}
 
+	private void checkPreConditions() throws IllegalStateException, IllegalStateException {
+		if (closed){
+			throw new javax.jms.IllegalStateException("Publisher is closed");
+		}
+		
+		if(topic == null){
+			throw new UnsupportedOperationException("Topic is null");
+		}
+		
+		AMQSession session = ((BasicMessageProducer)delegate).getSession();
+		if(session == null || session.isClosed()){
+			throw new UnsupportedOperationException("Invalid Session");
+		}
+	}
 }
