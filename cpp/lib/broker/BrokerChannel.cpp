c86dae01d62b137416be63a8c5f4f545ea836a44
Steven Shaw <steshaw@apache.org>
2006-12-08 11:30:55 +0000

    Thanks Alan the static_cast does prevent the warning on rhel4/x86_64
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@483943 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/lib/broker/BrokerChannel.cpp
+++ cpp/lib/broker/BrokerChannel.cpp
@@ -204,7 +204,7 @@ void Channel::ack(u_int64_t deliveryTag, bool multiple){
             throw InvalidAckException();
         }else if(multiple){     
             ack_iterator end = ++i;
-            for_each(unacked.begin(), end, bind2nd(mem_fun_ref(&DeliveryRecord::discard), 0));
+            for_each(unacked.begin(), end, bind2nd(mem_fun_ref(&DeliveryRecord::discard), static_cast<qpid::broker::TransactionContext*>(0)));
             unacked.erase(unacked.begin(), end);
 
             //recalculate the prefetch:
