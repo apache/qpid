d9aa5a9c9a8622e12ebc897222b6c0f38676f3e4
Robert Greig <rgreig@apache.org>
2006-12-16 13:36:26 +0000

    QPID-202 : Implement TemporaryQueue.delete
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@487801 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/client/src/test/java/org/apache/qpid/test/unit/client/temporaryqueue/TemporaryQueueTest.java
@@ -0,0 +1,81 @@
+package org.apache.qpid.test.unit.client.temporaryqueue;
+
+import junit.framework.TestCase;
+import org.apache.qpid.client.transport.TransportConnection;
+import org.apache.qpid.client.AMQConnection;
+import org.apache.qpid.client.AMQAuthenticationException;
+import org.apache.qpid.AMQException;
+import org.apache.qpid.AMQConnectionException;
+import org.apache.qpid.AMQUnresolvedAddressException;
+import org.apache.qpid.url.URLSyntaxException;
+import org.apache.qpid.test.unit.client.connection.ConnectionTest;
+
+import javax.jms.*;
+
+public class TemporaryQueueTest extends TestCase
+{
+
+    String _broker = "vm://:1";
+
+
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        TransportConnection.createVMBroker(1);
+    }
+
+    protected void tearDown() throws Exception
+    {
+        TransportConnection.killAllVMBrokers();
+    }
+
+    protected Connection createConnection() throws AMQException, URLSyntaxException
+    {
+        return new AMQConnection(_broker, "guest", "guest",
+                                                      "fred", "/test");
+    }
+
+    public void testTempoaryQueue() throws Exception
+    {
+        Connection conn = createConnection();
+        Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        TemporaryQueue queue = session.createTemporaryQueue();
+        assertNotNull(queue);
+        MessageProducer producer = session.createProducer(queue);
+        MessageConsumer consumer = session.createConsumer(queue);
+        conn.start();
+        producer.send(session.createTextMessage("hello"));
+        TextMessage tm = (TextMessage) consumer.receive(2000);
+        assertNotNull(tm);
+        assertEquals("hello",tm.getText());
+
+        try
+        {
+            queue.delete();
+            fail("Expected JMSException : should not be able to delete while there are active consumers");
+        }
+        catch(JMSException je)
+        {
+            ; //pass
+        }
+
+        consumer.close();
+
+        try
+        {
+            queue.delete();
+        }
+        catch(JMSException je)
+        {
+            fail("Unexpected Exception: " + je.getMessage());
+        }
+
+        conn.close();
+    }
+
+
+    public static junit.framework.Test suite()
+    {
+        return new junit.framework.TestSuite(TemporaryQueueTest.class);
+    }
+}
