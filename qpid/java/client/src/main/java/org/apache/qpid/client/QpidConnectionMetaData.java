2a1ea737ef65377bd4b08b45d72d2df7f9fc6cfc
Robert Greig <rgreig@apache.org>
2006-12-19 09:29:19 +0000

    QPID-215 : Patch supplied by Rob Godfrey - Implement custom JMSX properties
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@488596 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/client/src/main/java/org/apache/qpid/client/QpidConnectionMetaData.java
+++ qpid/java/client/src/main/java/org/apache/qpid/client/QpidConnectionMetaData.java
@@ -1,3 +1,23 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
 package org.apache.qpid.client;
 
 import java.util.Enumeration;
@@ -5,46 +25,73 @@ import java.util.Enumeration;
 import javax.jms.ConnectionMetaData;
 import javax.jms.JMSException;
 
-public class QpidConnectionMetaData implements ConnectionMetaData {
+public class QpidConnectionMetaData implements ConnectionMetaData
+{
 
-	private static QpidConnectionMetaData _instance = new QpidConnectionMetaData();
-	
-	private QpidConnectionMetaData(){		
-	}
-	
-	public static QpidConnectionMetaData instance(){
-		return _instance;
-	}	
-	
-	public int getJMSMajorVersion() throws JMSException {
-		return 1;
-	}
 
-	public int getJMSMinorVersion() throws JMSException {
-		return 1;
-	}
 
-	public String getJMSProviderName() throws JMSException {
-		return "Apache Qpid";
-	}
+    QpidConnectionMetaData(AMQConnection conn)
+    {
+    }
 
-	public String getJMSVersion() throws JMSException {
-		return "1.1";
-	}
+    public int getJMSMajorVersion() throws JMSException
+    {
+        return 1;
+    }
 
-	public Enumeration getJMSXPropertyNames() throws JMSException {
-		return null;
-	}
+    public int getJMSMinorVersion() throws JMSException
+    {
+        return 1;
+    }
 
-	public int getProviderMajorVersion() throws JMSException {
-		return 0;
-	}
+    public String getJMSProviderName() throws JMSException
+    {
+        return "Apache Qpid";
+    }
 
-	public int getProviderMinorVersion() throws JMSException {
-		return 9;
-	}
+    public String getJMSVersion() throws JMSException
+    {
+        return "1.1";
+    }
 
-	public String getProviderVersion() throws JMSException {
-		return "Incubating-M1";
-	}
+    public Enumeration getJMSXPropertyNames() throws JMSException
+    {
+        return CustomJMXProperty.asEnumeration();
+    }
+
+    public int getProviderMajorVersion() throws JMSException
+    {
+        return 0;
+    }
+
+    public int getProviderMinorVersion() throws JMSException
+    {
+        return 8;
+    }
+
+    public String getProviderVersion() throws JMSException
+    {
+        return "QPID (Client: [" + getClientVersion() + "] ; Broker [" + getBrokerVersion() + "] ; Protocol: [ "
+                + getProtocolVersion() + "] )";
+    }
+
+    private String getProtocolVersion()
+    {
+        // TODO - Implement based on connection negotiated protocol
+        return "0.8";
+    }
+
+    public String getBrokerVersion()
+    {
+        // TODO - get broker version
+        return "<unkown>";
+    }
+
+    public String getClientVersion()
+    {
+        // TODO - get client build version from properties file or similar
+        return "<unknown>";
+    }
+
+
 }
