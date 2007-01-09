0d4cb521192494c8a7ccb1b2d5295126d84fc5e4
Kim van der Riet <kpvdr@apache.org>
2007-01-09 20:07:52 +0000

    Corrected errors in AMQResponseBody
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/branches/qpid.0-9@494549 13f79535-47bb-0310-9956-ffa450edef68

--- java/common/src/main/java/org/apache/qpid/framing/AMQResponseBody.java
+++ java/common/src/main/java/org/apache/qpid/framing/AMQResponseBody.java
@@ -22,24 +22,39 @@ package org.apache.qpid.framing;
 
 import org.apache.mina.common.ByteBuffer;
 
-public class AMQResponseBody extends AMQRequestBody
+public class AMQResponseBody extends AMQBody
 {
     public static final byte TYPE = (byte)AmqpConstants.frameResponseAsInt();
        
     // Fields declared in specification
+    public long responseId;
+    public long requestId;
     public int batchOffset;
+    public AMQMethodBody methodPayload;
 
     // Constructor
     public AMQResponseBody() {}
 
     // Field methods
-    
+    public long getResponseId() { return responseId; }
+    public long getRequestId() { return requestId; }
     public int  getBatchOffset() { return batchOffset; }
+    public AMQMethodBody getMethodPayload() { return methodPayload; }
+    
+    protected byte getFrameType()
+    {
+    	return TYPE;
+    }
+    
+    protected int getSize()
+    {
+    	return 8 + 8 + 4 + methodPayload.getBodySize();
+    }
         
     protected void writePayload(ByteBuffer buffer)
     {
+        EncodingUtils.writeLong(buffer, responseId);
         EncodingUtils.writeLong(buffer, requestId);
-        EncodingUtils.writeLong(buffer, responseMark);
         EncodingUtils.writeUnsignedShort(buffer, batchOffset);
         methodPayload.writePayload(buffer);
     }
@@ -47,8 +62,8 @@ public class AMQResponseBody extends AMQRequestBody
     protected void populateFromBuffer(ByteBuffer buffer, long size)
         throws AMQFrameDecodingException, AMQProtocolVersionException
     {
+        responseId = EncodingUtils.readLong(buffer);
         requestId = EncodingUtils.readLong(buffer);
-        responseMark = EncodingUtils.readLong(buffer);
         batchOffset = EncodingUtils.readShort(buffer);
         methodPayload.populateFromBuffer(buffer, size - 8 - 8 - 4);
     }
@@ -57,8 +72,8 @@ public class AMQResponseBody extends AMQRequestBody
             long responseMark, int batchOffset, AMQMethodBody methodPayload)
     {
         AMQResponseBody responseFrame = new AMQResponseBody();
+        responseFrame.responseId = responseId;
         responseFrame.requestId = requestId;
-        responseFrame.responseMark = responseMark;
         responseFrame.batchOffset = batchOffset;
         responseFrame.methodPayload = methodPayload;
         
