998387a8b1a32a9d1545ba30ad749aa3ffd5e3d0
Bhupendra Bhusman Bhardwaj <bhupendrab@apache.org>
2007-01-16 10:04:57 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@496648 13f79535-47bb-0310-9956-ffa450edef68

---
+++ qpid/java/perftests/bin/serviceRequestingClient-createLogFile.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+##LOGDIR=$QPID_HOME/logs
+LOGDIR=../logs
+LOGFILE=$LOGDIR/perftest.log
+
+## create the log dir
+if [ ! -d $LOGDIR ]; then
+    mkdir $LOGDIR
+fi
+
+echo "********** Running the test **************"
+echo "creating logfile $LOGFILE"
+echo
+
+./serviceRequestingClient.sh $@ 2>&1 | tee $LOGFILE
+
+echo "********** End of test ******************"
+echo
