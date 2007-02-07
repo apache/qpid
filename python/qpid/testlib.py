c9ca9e1767b66d31fe9180fbd245508f60eaba8f
Gordon Sim <gsim@apache.org>
2007-02-07 15:36:01 +0000

    Added support for receiving and sending of references Added asynchronous mode to channels (responses can be tracked via a future, rather than blocking on each request) Added ability to override server suggested connection tune params Added two tests for reference functionality (more to follow)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/branches/qpid.0-9@504590 13f79535-47bb-0310-9956-ffa450edef68

--- python/qpid/testlib.py
+++ python/qpid/testlib.py
@@ -129,7 +129,7 @@ Options:
             print "======================================="
         return result.wasSuccessful()
 
-    def connect(self, host=None, port=None, spec=None, errata=None, user=None, password=None):
+    def connect(self, host=None, port=None, spec=None, errata=None, user=None, password=None, tune_params=None):
         """Connect to the broker, returns a qpid.client.Client"""
         host = host or self.host
         port = port or self.port
@@ -138,7 +138,7 @@ Options:
         user = user or self.user
         password = password or self.password
         client = qpid.client.Client(host, port, qpid.spec.load(spec, errata))
-        client.start({"LOGIN": user, "PASSWORD": password})
+        client.start({"LOGIN": user, "PASSWORD": password}, tune_params=tune_params)
         return client
 
 
