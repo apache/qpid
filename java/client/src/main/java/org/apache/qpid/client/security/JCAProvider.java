53dfa7e61494fc38de8b527a91dfdb6051260e2a
Robert Greig <rgreig@apache.org>
2007-02-20 16:51:32 +0000

    (Path submitted by Rupert Smith) Qpid-338. Custom SASL implementation for Java 1.4 retrotranslation of the Java client.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@509642 13f79535-47bb-0310-9956-ffa450edef68

--- java/client/src/main/java/org/apache/qpid/client/security/JCAProvider.java
+++ java/client/src/main/java/org/apache/qpid/client/security/JCAProvider.java
@@ -7,9 +7,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -26,22 +26,46 @@ import java.util.Map;
 
 import javax.security.sasl.SaslClientFactory;
 
+import org.apache.log4j.Logger;
+
+/**
+ * JCAProvider is a security provider for SASL client factories that is configured from a map of SASL mechanism names
+ * to client factories implementation class names. It is intended that the map of client factories can be read from a
+ * configuration file or other application configuration mechanism.
+ *
+ * <p><table id="crc"><caption>CRC Card</caption>
+ * <tr><th> Responsibilities <th> Collaborations
+ * <tr><td> Register SASL mechanism implementations.
+ * </table>
+ */
 public class JCAProvider extends Provider
 {
+    private static final Logger log = Logger.getLogger(JCAProvider.class);
+
+    /**
+     * Creates the security provider with a map from SASL mechanisms to implementing factories.
+     *
+     * @param providerMap The map from SASL mechanims to implementing factory classes.
+     */
     public JCAProvider(Map<String, Class<? extends SaslClientFactory>> providerMap)
     {
-        super("AMQSASLProvider", 1.0, "A JCA provider that registers all " +
-              "AMQ SASL providers that want to be registered");
+        super("AMQSASLProvider", 1.0, "A JCA provider that registers all "
+              + "AMQ SASL providers that want to be registered");
         register(providerMap);
         Security.addProvider(this);
     }
 
+    /**
+     * Registers client factory classes for a map of mechanism names to client factory classes.
+     *
+     * @param providerMap The map from SASL mechanims to implementing factory classes.
+     */
     private void register(Map<String, Class<? extends SaslClientFactory>> providerMap)
     {
-        for (Map.Entry<String, Class<? extends SaslClientFactory>> me :
-             providerMap.entrySet())
+        for (Map.Entry<String, Class<? extends SaslClientFactory>> me : providerMap.entrySet())
         {
             put("SaslClientFactory." + me.getKey(), me.getValue().getName());
+            log.debug("Registered SASL Client factory for " + me.getKey() + " as " + me.getValue().getName());
         }
     }
 }
