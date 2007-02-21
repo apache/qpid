272f0d2a1b2134c7cdddcc9e6e951edc696e6054
Martin Ritchie <ritchiem@apache.org>
2007-02-21 16:03:24 +0000

    QPID-348 Reverted unecessary nowait addition to amqp Basic.Recover spec.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@510076 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/broker/src/main/java/org/apache/qpid/server/handler/BasicRecoverMethodHandler.java
+++ qpid/java/broker/src/main/java/org/apache/qpid/server/handler/BasicRecoverMethodHandler.java
@@ -56,12 +56,9 @@ public class BasicRecoverMethodHandler implements StateAwareMethodListener<Basic
 
         channel.resend(session, body.requeue);
 
-        if (!body.nowait)
-        {
-            // AMQP version change: Hardwire the version to 0-8 (major=8, minor=0)
-            // TODO: Connect this to the session version obtained from ProtocolInitiation for this session.
-            // Be aware of possible changes to parameter order as versions change.
-            session.writeFrame(BasicRecoverOkBody.createAMQFrame(evt.getChannelId(), (byte) 8, (byte) 0));
-        }
+        // AMQP version change: Hardwire the version to 0-8 (major=8, minor=0)
+        // TODO: Connect this to the session version obtained from ProtocolInitiation for this session.
+        // Be aware of possible changes to parameter order as versions change.
+        session.writeFrame(BasicRecoverOkBody.createAMQFrame(evt.getChannelId(), (byte) 8, (byte) 0));
     }
 }
