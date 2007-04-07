a995f4d7016b6523c7deac5b853f0f13ccdfcd81
Robert Godfrey <rgodfrey@apache.org>
2007-04-07 16:01:43 +0000

    Refactoring and multi-version support
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk@526446 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/gentools/src/org/apache/qpid/gentools/AmqpConstant.java
+++ qpid/gentools/src/org/apache/qpid/gentools/AmqpConstant.java
@@ -25,151 +25,134 @@ import java.util.TreeMap;
 
 /**
  * @author kpvdr
- * Class to represent the &lt;constant&gt; declaration within the AMQP specification.
- * Currently, only integer values exist within the specification, however looking forward
- * to other possible types in the future, string and double types are also supported.
- * 
- * The &lt;constant&gt; declaration in the specification contains only two attributes:
- * name and value.
- * 
- * The value of the constant is mapped against the version(s) for which the name is defined.
- * This allows for a change in the value rather than the name only from one version to the next.
+ *         Class to represent the &lt;constant&gt; declaration within the AMQP specification.
+ *         Currently, only integer values exist within the specification, however looking forward
+ *         to other possible types in the future, string and double types are also supported.
+ *         <p/>
+ *         The &lt;constant&gt; declaration in the specification contains only two attributes:
+ *         name and value.
+ *         <p/>
+ *         The value of the constant is mapped against the version(s) for which the name is defined.
+ *         This allows for a change in the value rather than the name only from one version to the next.
  */
 @SuppressWarnings("serial")
 public class AmqpConstant extends TreeMap<String, AmqpVersionSet>
-    implements Printable, VersionConsistencyCheck, Comparable<AmqpConstant>
+        implements Printable, VersionConsistencyCheck, Comparable<AmqpConstant>
 {
     /**
      * Constant name as defined by the name attribute of the &lt;constant&gt; declaration.
      */
-    protected String name;
-    
+    private final String _name;
+
     /**
      * Set of versions for which this constant name is defined.
      */
-    protected AmqpVersionSet versionSet;
+    private final AmqpVersionSet _versionSet;
 
     /**
      * Constructor
-     * @param name Constant name as defined by the name attribute of the &lt;constant&gt; declaration.
-     * @param value Constant value as defined by the value attribute of the &lt;constant&gt; declaration.
+     *
+     * @param name    Constant name as defined by the name attribute of the &lt;constant&gt; declaration.
+     * @param value   Constant value as defined by the value attribute of the &lt;constant&gt; declaration.
      * @param version AMQP version for which this constant is defined
      */
-    public AmqpConstant (String name, String value, AmqpVersion version)
+    public AmqpConstant(String name, String value, AmqpVersion version)
     {
-        this.name = name;
-        versionSet = new AmqpVersionSet(version);
-        AmqpVersionSet valueVersionSet = new  AmqpVersionSet(version);
+        _name = name;
+        _versionSet = new AmqpVersionSet(version);
+        AmqpVersionSet valueVersionSet = new AmqpVersionSet(version);
         put(value, valueVersionSet);
-     }
-
-    /**
-     * Constructor
-     * @param name Constant name as defined by the name attribute of the &lt;constant&gt; declaration.
-     * @param value Constant value as defined by the value attribute of the &lt;constant&gt; declaration.
-     * @param version AMQP version for which this constant is defined
-     */
-    public AmqpConstant (String name, int value, AmqpVersion version)
-    {
-        this.name = name;
-        versionSet = new AmqpVersionSet(version);
-        AmqpVersionSet valueVersionSet = new  AmqpVersionSet(version);
-        put(String.valueOf(value), valueVersionSet);
     }
 
-    /**
-     * Constructor
-     * @param name Constant name as defined by the name attribute of the &lt;constant&gt; declaration.
-     * @param value Constant value as defined by the value attribute of the &lt;constant&gt; declaration.
-     * @param version AMQP version for which this constant is defined
-     */
-    public AmqpConstant (String name, double value, AmqpVersion version)
-    {
-        this.name = name;
-        versionSet = new AmqpVersionSet(version);
-        AmqpVersionSet valueVersionSet = new  AmqpVersionSet(version);
-        put(String.valueOf(value), valueVersionSet);
-    }
 
     /**
      * Get the name of this constant.
+     *
      * @return Name of this constant, being the name attribute of the &lt;constant&gt; declaration
-     * represented by this class.
+     *         represented by this class.
      */
     public String getName()
     {
-        return name;
+        return _name;
     }
-    
+
     /**
      * Get the value of this constant as a String.
+     *
      * @param version AMQP version for which this value is required.
      * @return Value of this constant, being the value attribute of the &lt;constant&gt; declaration
-     * represented by this class.
+     *         represented by this class.
      * @throws AmqpTypeMappingException when a value is requested for a version for which it is not
-     * defined in the AMQP specifications.
+     *                                  defined in the AMQP specifications.
      */
     public String getStringValue(AmqpVersion version)
-    throws AmqpTypeMappingException
+            throws AmqpTypeMappingException
     {
-    	for (String thisValue : keySet())
+        for (String thisValue : keySet())
         {
             AmqpVersionSet versionSet = get(thisValue);
             if (versionSet.contains(version))
+            {
                 return thisValue;
+            }
         }
-        throw new AmqpTypeMappingException("Unable to find value for constant \"" + name +
-            "\" for version " + version.toString() + ".");
+        throw new AmqpTypeMappingException("Unable to find value for constant \"" + getName() +
+                                           "\" for version " + version.toString() + ".");
     }
-    
+
     /**
      * Get the value of this constant as an integer.
+     *
      * @param version AMQP version for which this value is required.
      * @return Value of this constant, being the value attribute of the &lt;constant&gt; declaration
-     * represented by this class.
+     *         represented by this class.
      * @throws AmqpTypeMappingException when a value is requested for a version for which it is not
-     * defined in the AMQP specifications.
+     *                                  defined in the AMQP specifications.
      */
     public int getIntegerValue(AmqpVersion version)
-    throws AmqpTypeMappingException
+            throws AmqpTypeMappingException
     {
         return Integer.parseInt(getStringValue(version));
     }
-    
+
     /**
      * Get the value of this constant as a double.
+     *
      * @param version AMQP version for which this value is required.
      * @return Value of this constant, being the value attribute of the &lt;constant&gt; declaration
-     * represented by this class.
+     *         represented by this class.
      * @throws AmqpTypeMappingException when a value is requested for a version for which it is not
-     * defined in the AMQP specifications.
+     *                                  defined in the AMQP specifications.
      */
     public double getDoubleValue(AmqpVersion version)
-    throws AmqpTypeMappingException
+            throws AmqpTypeMappingException
     {
         return Double.parseDouble(getStringValue(version));
     }
-    
+
     /**
      * Get the version set for this constant. It contains the all the versions for which this
      * constant name exists.
+     *
      * @return Set of versions for which this constant exists.
      */
     public AmqpVersionSet getVersionSet()
     {
-        return versionSet;
+        return _versionSet;
     }
-    
+
     /* (non-Javadoc)
-     * @see java.lang.Comparable#compareTo(java.lang.Object)
-     */
-    
+    * @see java.lang.Comparable#compareTo(java.lang.Object)
+    */
+
     public int compareTo(AmqpConstant other)
     {
-        int res = name.compareTo(other.name);
+        int res = getName().compareTo(other.getName());
         if (res != 0)
+        {
             return res;
-        return versionSet.compareTo(other.versionSet);
+        }
+        return getVersionSet().compareTo(other.getVersionSet());
     }
 
     /* (non-Javadoc)
@@ -178,7 +161,9 @@ public class AmqpConstant extends TreeMap<String, AmqpVersionSet>
     public boolean isVersionConsistent(AmqpVersionSet globalVersionSet)
     {
         if (size() != 1)
+        {
             return false;
+        }
         return get(firstKey()).equals(globalVersionSet);
     }
 
@@ -191,15 +176,16 @@ public class AmqpConstant extends TreeMap<String, AmqpVersionSet>
         String tab = Utils.createSpaces(tabSize);
         if (size() == 1)
         {
-            out.println(margin + tab + "[C] " + name + " = \"" + firstKey() + "\" " + versionSet);
+            out.println(margin + tab + "[C] " + getName() + " = \"" + firstKey() + "\" " + getVersionSet());
         }
         else
         {
-            out.println(margin + tab + "[C] " + name + ": " + versionSet);
+            out.println(margin + tab + "[C] " + getName() + ": " + getVersionSet());
             for (String thisValue : keySet())
             {
-                 out.println(margin + tab + tab + "= \"" + thisValue + "\" " + get(thisValue));
+                out.println(margin + tab + tab + "= \"" + thisValue + "\" " + get(thisValue));
             }
         }
     }
+
 }
