2f5c3bf8ddbbd03b6e43fdaf3d6e70e9263e6a62
Alan Conway <aconway@apache.org>
2007-05-11 20:11:32 +0000

    * cpp/src/Makefile.am: Move generated code into src. Fix generation script. * cpp/src/generate.sh: New generator script, simplify & fix some problems. * cpp/Makefile.am: removed gen subdir * cpp/bootstrap: remove redundant comment * cpp/configure.ac: remove generator tests, now in generate.sh * cpp/gen,cpp/gen/Makefile.am,cpp/gen/gen-src-mk.sh: Replaced by generate.sh * cpp/src/tests/Makefile.am: Remove client_test from EXTRA_DEPS
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@537278 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cpp/src/generate.sh
@@ -0,0 +1,46 @@
+# !/bin/sh
+# Generate code from AMQP specification.
+# srcdir must 
+# 
+srcdir=`dirname $0`
+set -e
+
+gentools_dir="$srcdir/../gentools"
+specs_dir="$srcdir/../../specs"
+specs="$specs_dir/amqp.0-9.xml $specs_dir/amqp-errata.0-9.xml"
+test -z "$JAVA" && JAVA=java ; 
+test -z "$JAVAC" && JAVAC=javac ; 
+
+# Can we generate code?
+if { test -d $gentools_dir && test -d $specs_dir && \
+    which $JAVA && which $JAVAC; } > /dev/null;
+then
+    echo "Generating code."
+    mkdir -p gen/qpid/framing
+    ( cd $gentools_dir/src && $JAVAC `find -name '*.java' -print` ; ) 
+    $JAVA -cp $gentools_dir/src org.apache.qpid.gentools.Main \
+	-c -o gen/qpid/framing -t $gentools_dir/templ.cpp $specs 
+    GENERATED=yes
+fi
+
+# Print a Makefile variable assignment.
+make_assign() {
+    echo -n "$1 = "; shift
+    prefix=$1; shift
+    for f in $*; do echo "\\" ; echo -n "  $prefix$f "; done
+    echo
+}
+
+# Generate a Makefile fragment
+(
+    make_assign "generated_cpp" "" `find gen -name '*.cpp' -print`
+    make_assign "generated_h" "" `find gen -name '*.h' -print`
+    if test x$GENERATED = xyes; then
+	make_assign "generator" "" $specs \
+	    `find ../gentools \( -name '*.java' -o -name '*.tmpl' \) -print`
+    fi
+) > generate.mk-t
+mv generate.mk-t generate.mk
+
+
+
