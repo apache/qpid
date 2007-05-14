dda84019faf93b56028fd0c67f646362a137f0d1
Tomas Restrepo <tomasr@apache.org>
2007-05-14 14:06:40 +0000

    Merged revisions 537834-537845 via svnmerge from https://svn.apache.org/repos/asf/incubator/qpid/branches/M2
    ........
      r537834 | tomasr | 2007-05-14 08:37:45 -0500 (Mon, 14 May 2007) | 6 lines
      
      * QPID-489 Added Nant build scripts for Qpid.NET
      * Modified msbuild files to build to central location matching nant builds
      * Removed old files from Qpid.Buffer
      * Removed MonoDevelop project files
      * Updated build scripts, readme.txt, todo.txt
    ........
      r537835 | tomasr | 2007-05-14 08:38:14 -0500 (Mon, 14 May 2007) | 1 line
      
      * removed MonoDevelop solution
    ........
      r537841 | tomasr | 2007-05-14 08:50:18 -0500 (Mon, 14 May 2007) | 1 line
      
      Fixed Qpid.Client.Tests build on linux
    ........
      r537845 | tomasr | 2007-05-14 08:57:12 -0500 (Mon, 14 May 2007) | 1 line
      
      Fixed release script and made executable
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@537848 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/dotnet/build-dotnet11.bat
+++ qpid/dotnet/build-dotnet11.bat
@@ -1,1 +1,1 @@
-MSBuild Qpid.NET.sln /p:Configuration=Release /t:rebuild /p:TargetFX1_1=true "/p:CustomAfterMicrosoftCommonTargets=%ProgramFiles%\MSBuild\MSBee\MSBuildExtras.Fx1_1.CSharp.targets"
+nant -t:net-1.1
