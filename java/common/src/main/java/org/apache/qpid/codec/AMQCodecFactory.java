decfd77364e211bc8f8784e15f54e06a79e16675
Rupert Smith <rupertlssmith@apache.org>
2007-05-15 16:32:10 +0000

    Merged revisions 538109 via svnmerge from https://svn.apache.org/repos/asf/incubator/qpid/branches/M2
    ........
      r538109 | rupertlssmith | 2007-05-15 10:48:16 +0100 (Tue, 15 May 2007) | 1 line
      
      More Javadocing.
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@538247 13f79535-47bb-0310-9956-ffa450edef68

--- java/common/src/main/java/org/apache/qpid/codec/AMQCodecFactory.java
+++ java/common/src/main/java/org/apache/qpid/codec/AMQCodecFactory.java
@@ -7,9 +7,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -24,28 +24,52 @@ import org.apache.mina.filter.codec.ProtocolCodecFactory;
 import org.apache.mina.filter.codec.ProtocolDecoder;
 import org.apache.mina.filter.codec.ProtocolEncoder;
 
+/**
+ * AMQCodecFactory is a Mina codec factory. It supplies the encoders and decoders need to read and write the bytes to
+ * the wire.
+ *
+ * <p/><table id="crc"><caption>CRC Card</caption>
+ * <tr><th> Responsibilities <th> Collaborations.
+ * <tr><td> Supply the protocol encoder. <td> {@link AMQEncoder}
+ * <tr><td> Supply the protocol decoder. <td> {@link AMQDecoder}
+ * </table>
+ */
 public class AMQCodecFactory implements ProtocolCodecFactory
 {
+    /** Holds the protocol encoder. */
     private AMQEncoder _encoder = new AMQEncoder();
 
+    /** Holds the protocol decoder. */
     private AMQDecoder _frameDecoder;
 
     /**
-     * @param expectProtocolInitiation true if the first frame received is going to be
-     * a protocol initiation frame, false if it is going to be a standard AMQ data block.
-     * The former case is used for the broker, which always expects to received the
-     * protocol initiation first from a newly connected client.
+     * Creates a new codec factory, specifiying whether it is expected that the first frame of data should be an
+     * initiation. This is the case for the broker, which always expects to received the protocol initiation on a newly
+     * connected client.
+     *
+     * @param expectProtocolInitiation <tt>true</tt> if the first frame received is going to be a protocol initiation
+     *                                 frame, <tt>false</tt> if it is going to be a standard AMQ data block.
      */
     public AMQCodecFactory(boolean expectProtocolInitiation)
     {
         _frameDecoder = new AMQDecoder(expectProtocolInitiation);
     }
 
+    /**
+     * Gets the AMQP encoder.
+     *
+     * @return The AMQP encoder.
+     */
     public ProtocolEncoder getEncoder()
     {
         return _encoder;
     }
 
+    /**
+     * Gets the AMQP decoder.
+     *
+     * @return The AMQP decoder.
+     */
     public ProtocolDecoder getDecoder()
     {
         return _frameDecoder;
