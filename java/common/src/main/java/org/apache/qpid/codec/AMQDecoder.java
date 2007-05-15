f14a84d6e0789b24dde0af0982012358196b540a
Rupert Smith <rupertlssmith@apache.org>
2007-05-15 16:32:10 +0000

    Merged revisions 538109 via svnmerge from https://svn.apache.org/repos/asf/incubator/qpid/branches/M2
    ........
      r538109 | rupertlssmith | 2007-05-15 10:48:16 +0100 (Tue, 15 May 2007) | 1 line
      
      More Javadocing.
    ........
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@538247 13f79535-47bb-0310-9956-ffa450edef68

--- java/common/src/main/java/org/apache/qpid/codec/AMQDecoder.java
+++ java/common/src/main/java/org/apache/qpid/codec/AMQDecoder.java
@@ -7,9 +7,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -24,28 +24,61 @@ import org.apache.mina.common.ByteBuffer;
 import org.apache.mina.common.IoSession;
 import org.apache.mina.filter.codec.CumulativeProtocolDecoder;
 import org.apache.mina.filter.codec.ProtocolDecoderOutput;
+
 import org.apache.qpid.framing.AMQDataBlockDecoder;
 import org.apache.qpid.framing.ProtocolInitiation;
 
 /**
- * There is one instance of this class per session. Any changes or configuration done
- * at run time to the encoders or decoders only affects decoding/encoding of the
- * protocol session data to which is it bound.
+ * AMQDecoder delegates the decoding of AMQP either to a data block decoder, or in the case of new connections, to a
+ * protocol initiation decoder. It is a cumulative decoder, which means that it can accumulate data to decode in the
+ * buffer until there is enough data to decode.
  *
+ * <p/>One instance of this class is created per session, so any changes or configuration done at run time to the
+ * decoder will only affect decoding of the protocol session data to which is it bound.
+ *
+ * <p/><table id="crc"><caption>CRC Card</caption>
+ * <tr><th> Responsibilities <th> Collaborations
+ * <tr><td> Delegate protocol initiation to its decoder. <td> {@link ProtocolInitiation.Decoder}
+ * <tr><td> Delegate AMQP data to its decoder. <td> {@link AMQDataBlockDecoder}
+ * <tr><td> Accept notification that protocol initiation has completed.
+ * </table>
+ *
+ * @todo If protocol initiation decoder not needed, then don't create it. Probably not a big deal, but it adds to the
+ *       per-session overhead.
  */
 public class AMQDecoder extends CumulativeProtocolDecoder
 {
+    /** Holds the 'normal' AMQP data decoder. */
     private AMQDataBlockDecoder _dataBlockDecoder = new AMQDataBlockDecoder();
 
+    /** Holds the protocol initiation decoder. */
     private ProtocolInitiation.Decoder _piDecoder = new ProtocolInitiation.Decoder();
 
+    /** Flag to indicate whether this decoder needs to handle protocol initiation. */
     private boolean _expectProtocolInitiation;
 
+    /**
+     * Creates a new AMQP decoder.
+     *
+     * @param expectProtocolInitiation <tt>true</tt> if this decoder needs to handle protocol initiation.
+     */
     public AMQDecoder(boolean expectProtocolInitiation)
     {
         _expectProtocolInitiation = expectProtocolInitiation;
     }
 
+    /**
+     * Delegates decoding AMQP from the data buffer that Mina has retrieved from the wire, to the data or protocol
+     * intiation decoders.
+     *
+     * @param session The Mina session.
+     * @param in      The raw byte buffer.
+     * @param out     The Mina object output gatherer to write decoded objects to.
+     *
+     * @return <tt>true</tt> if the data was decoded, <tt>false<tt> if more is needed and the data should accumulate.
+     *
+     * @throws Exception If the data cannot be decoded for any reason.
+     */
     protected boolean doDecode(IoSession session, ByteBuffer in, ProtocolDecoderOutput out) throws Exception
     {
         if (_expectProtocolInitiation)
@@ -58,6 +91,17 @@ public class AMQDecoder extends CumulativeProtocolDecoder
         }
     }
 
+    /**
+     * Decodes AMQP data, delegating the decoding to an {@link AMQDataBlockDecoder}.
+     *
+     * @param session The Mina session.
+     * @param in      The raw byte buffer.
+     * @param out     The Mina object output gatherer to write decoded objects to.
+     *
+     * @return <tt>true</tt> if the data was decoded, <tt>false<tt> if more is needed and the data should accumulate.
+     *
+     * @throws Exception If the data cannot be decoded for any reason.
+     */
     protected boolean doDecodeDataBlock(IoSession session, ByteBuffer in, ProtocolDecoderOutput out) throws Exception
     {
         int pos = in.position();
@@ -72,10 +116,22 @@ public class AMQDecoder extends CumulativeProtocolDecoder
         else
         {
             _dataBlockDecoder.decode(session, in, out);
+
             return true;
         }
     }
 
+    /**
+     * Decodes an AMQP initiation, delegating the decoding to a {@link ProtocolInitiation.Decoder}.
+     *
+     * @param session The Mina session.
+     * @param in      The raw byte buffer.
+     * @param out     The Mina object output gatherer to write decoded objects to.
+     *
+     * @return <tt>true</tt> if the data was decoded, <tt>false<tt> if more is needed and the data should accumulate.
+     *
+     * @throws Exception If the data cannot be decoded for any reason.
+     */
     private boolean doDecodePI(IoSession session, ByteBuffer in, ProtocolDecoderOutput out) throws Exception
     {
         boolean enoughData = _piDecoder.decodable(session, in);
@@ -88,10 +144,18 @@ public class AMQDecoder extends CumulativeProtocolDecoder
         else
         {
             _piDecoder.decode(session, in, out);
+
             return true;
         }
     }
 
+    /**
+     * Sets the protocol initation flag, that determines whether decoding is handled by the data decoder of the protocol
+     * initation decoder. This method is expected to be called with <tt>false</tt> once protocol initation completes.
+     *
+     * @param expectProtocolInitiation <tt>true</tt> to use the protocol initiation decoder, <tt>false</tt> to use the
+     *                                data decoder.
+     */
     public void setExpectProtocolInitiation(boolean expectProtocolInitiation)
     {
         _expectProtocolInitiation = expectProtocolInitiation;
