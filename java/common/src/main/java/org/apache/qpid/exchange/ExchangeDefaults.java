c0908c3a2c22fcc99474f9dad27b6f94442e65c9
Rupert Smith <rupertlssmith@apache.org>
2007-05-17 13:17:59 +0000

    Added to the Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/branches/M2@538907 13f79535-47bb-0310-9956-ffa450edef68

--- java/common/src/main/java/org/apache/qpid/exchange/ExchangeDefaults.java
+++ java/common/src/main/java/org/apache/qpid/exchange/ExchangeDefaults.java
@@ -7,9 +7,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -22,25 +22,44 @@ package org.apache.qpid.exchange;
 
 import org.apache.qpid.framing.AMQShortString;
 
+/**
+ * Defines the names of the standard AMQP exchanges that every AMQP broker should provide. These exchange names
+ * and type are given in the specification.
+ *
+ * <p/><table id="crc"><caption>CRC Card</caption>
+ * <tr><th> Responsibilities <th> Collaborations
+ * <tr><td> Defines the standard AMQP exchange names.
+ * <tr><td> Defines the standard AMQP exchange types.
+ * </table>
+ *
+ * @todo A type safe enum, might be more appropriate for the exchange types.
+ */
 public class ExchangeDefaults
 {
-    public final static AMQShortString DEFAULT_EXCHANGE_NAME = new AMQShortString("<<default>>");
+    /** The default direct exchange, which is a special internal exchange that cannot be explicitly bound to. */
+    public static final AMQShortString DEFAULT_EXCHANGE_NAME = new AMQShortString("<<default>>");
 
-    public final static AMQShortString TOPIC_EXCHANGE_NAME = new AMQShortString("amq.topic");
+    /** The pre-defined topic exchange, the broker SHOULD provide this. */
+    public static final AMQShortString TOPIC_EXCHANGE_NAME = new AMQShortString("amq.topic");
 
-    public final static AMQShortString TOPIC_EXCHANGE_CLASS = new AMQShortString("topic");
+    /** Defines the identifying type name of topic exchanges. */
+    public static final AMQShortString TOPIC_EXCHANGE_CLASS = new AMQShortString("topic");
 
-    public final static AMQShortString DIRECT_EXCHANGE_NAME = new AMQShortString("amq.direct");
+    /** The pre-defined direct exchange, the broker MUST provide this. */
+    public static final AMQShortString DIRECT_EXCHANGE_NAME = new AMQShortString("amq.direct");
 
-    public final static AMQShortString DIRECT_EXCHANGE_CLASS = new AMQShortString("direct");
+    /** Defines the identifying type name of direct exchanges. */
+    public static final AMQShortString DIRECT_EXCHANGE_CLASS = new AMQShortString("direct");
 
-    public final static AMQShortString HEADERS_EXCHANGE_NAME = new AMQShortString("amq.match");
+    /** The pre-defined headers exchange, the specification does not say this needs to be provided. */
+    public static final AMQShortString HEADERS_EXCHANGE_NAME = new AMQShortString("amq.match");
 
-    public final static AMQShortString HEADERS_EXCHANGE_CLASS = new AMQShortString("headers");
+    /** Defines the identifying type name of headers exchanges. */
+    public static final AMQShortString HEADERS_EXCHANGE_CLASS = new AMQShortString("headers");
 
-    public final static AMQShortString FANOUT_EXCHANGE_NAME = new AMQShortString("amq.fanout");
+    /** The pre-defined fanout exchange, the boker MUST provide this. */
+    public static final AMQShortString FANOUT_EXCHANGE_NAME = new AMQShortString("amq.fanout");
 
-    public final static AMQShortString FANOUT_EXCHANGE_CLASS = new AMQShortString("fanout");
-
-
+    /** Defines the identifying type name of fanout exchanges. */
+    public static final AMQShortString FANOUT_EXCHANGE_CLASS = new AMQShortString("fanout");
 }
