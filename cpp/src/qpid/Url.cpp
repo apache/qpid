41c30308ad435c338633b97405fe7350d515f069
Alan Conway <aconway@apache.org>
2007-06-15 21:27:22 +0000

    Parser for AMQP-95 URLs
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@547789 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cpp/src/qpid/Url.cpp
@@ -0,0 +1,114 @@
+/*
+ *
+ * Copyright (c) 2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "Url.h"
+#include <sstream>
+#include <boost/spirit.hpp>
+#include <boost/spirit/actor.hpp>
+
+using namespace boost::spirit;
+using namespace std;
+
+namespace qpid {
+
+string Url::str() const {
+    ostringstream os;
+    os << *this;
+    return os.str();
+}
+
+ostream& operator<<(ostream& os, const Url& url) {
+    Url::const_iterator i = url.begin();
+    os << "amqp:";
+    if (i!=url.end()) {
+        os << *i++;
+        while (i != url.end()) 
+            os << "," << *i++;
+    }
+    return os;
+}
+
+// Addition to boost::spirit parsers: accept any character from a
+// string. Vastly more compile-time-efficient than long rules of the
+// form: ch_p('x') | ch_p('y') |...
+// 
+struct ch_in : public char_parser<ch_in> {
+    ch_in(const string& chars_) : chars(chars_) {}
+    bool test(char ch_) const {
+        return chars.find(ch_) != string::npos;
+    }
+    string chars;
+};
+
+inline ch_in ch_in_p(const string& chars) {
+    return ch_in(chars);
+}
+
+/** Grammar for AMQP URLs. */
+struct UrlGrammar : public grammar<UrlGrammar>
+{
+    Url& addr;
+    
+    UrlGrammar(Url& addr_) : addr(addr_) {}
+
+    template <class ScannerT>
+    struct definition {
+        TcpAddress tcp;
+
+        definition(const UrlGrammar& self)
+        {
+            first = eps_p[clear_a(self.addr)] >> amqp_url;
+            amqp_url = str_p("amqp:") >> prot_addr_list >>
+                !(str_p("/") >> !parameters);
+            prot_addr_list = prot_addr % ',';            
+            prot_addr      = tcp_prot_addr; // Extend for TLS etc.
+
+            // TCP addresses
+            tcp_prot_addr  = tcp_id >> tcp_addr[push_back_a(self.addr, tcp)];
+            tcp_id         = !str_p("tcp:"); 
+            tcp_addr       = !(host[assign_a(tcp.host)] >> !(':' >> port));
+            
+            // See http://www.apps.ietf.org/rfc/rfc3986.html#sec-A
+            // for real host grammar. Shortcut:
+            port           = uint_parser<uint16_t>()[assign_a(tcp.port)];
+            host           = *( unreserved | pct_encoded );
+            unreserved    = alnum_p | ch_in_p("-._~");
+            pct_encoded   = "%" >> xdigit_p >> xdigit_p;
+            parameters = *anychar_p >> end_p; // Ignore, not used yet.
+        }
+
+        const rule<ScannerT>& start() const { return first; }
+
+        rule<ScannerT> first, amqp_url, prot_addr_list, prot_addr,
+            tcp_prot_addr, tcp_id, tcp_addr, host, port,
+            unreserved, pct_encoded, parameters;
+    };
+};
+
+Url::Url(const string& url) {
+    if (!parse(url.c_str(), UrlGrammar(*this)).full)
+        throw InvalidUrl("Invalid AMQP url: "+url);
+    // TODO aconway 2007-06-15: Better error handling?
+}
+
+Url::Url(const string& url, const nothrow_t&) {
+    if (!parse(url.c_str(), UrlGrammar(*this)).full)
+        clear();
+}
+
+} // namespace qpid
---
+++ cpp/src/tests/unit/Url.cpp
@@ -0,0 +1,55 @@
+/*
+ *
+ * Copyright (c) 2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#define BOOST_AUTO_TEST_MAIN    // Must come before #include<boost/test/*>
+#include "test_tools.h"
+#include "qpid/Url.h"
+#include <boost/assign.hpp>
+
+using namespace std;
+using namespace qpid;
+using namespace boost::assign;
+
+BOOST_AUTO_TEST_CASE(testUrl_str) {
+    Url url;
+    url.push_back(TcpAddress("foo.com"));
+    url.push_back(TcpAddress("bar.com", 6789));
+    
+    BOOST_CHECK_EQUAL(
+        url.str(), "amqp:tcp:foo.com:5672,tcp:bar.com:6789");
+    BOOST_CHECK_EQUAL(Url().str(), "amqp:");
+}
+
+
+BOOST_AUTO_TEST_CASE(testUrl_ctor) {
+    BOOST_CHECK_EQUAL(
+        Url("amqp:foo.com,tcp:bar.com:1234").str(),
+        "amqp:tcp:foo.com:5672,tcp:bar.com:1234");
+    BOOST_CHECK_EQUAL(
+        Url("amqp:foo/ignorethis").str(),
+        "amqp:tcp:foo:5672");
+    BOOST_CHECK_EQUAL("amqp:tcp::5672", Url("amqp:").str());
+    BOOST_CHECK_EQUAL(0u, Url("xxx", nothrow).size());
+    try {
+        Url invalid("xxx");
+        BOOST_FAIL("Expected InvalidUrl exception");
+    }
+    catch (const Url::InvalidUrl&) {}
+}
+
+
