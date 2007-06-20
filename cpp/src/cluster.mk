20da85ae79d0f262765173396afe75eb0378bd09
Alan Conway <aconway@apache.org>
2007-06-20 22:19:10 +0000

    Dummy libcluster.so when openais is not available, keep rpmbuild happy.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@549277 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/cluster.mk
+++ cpp/src/cluster.mk
@@ -1,11 +1,16 @@
 #-*-Makefile-*-
 # Cluster library makefile fragment, to be included in Makefile.am
 # 
-
 lib_LTLIBRARIES += libqpidcluster.la
+
+if CLUSTER
 
 libqpidcluster_la_SOURCES = \
   qpid/cluster/Cpg.cpp \
   qpid/cluster/Cpg.h
-
 libqpidcluster_la_LIBADD= -lcpg
+
+else
+# Empty stub library to satisfy rpm spec file.
+libqpidcluster_la_SOURCES = 
+endif
