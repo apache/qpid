1bb5b8f7f75d22b9a5b3cfb06572a7f19a15c77a
Alan Conway <aconway@apache.org>
2007-06-26 04:44:13 +0000

    Missing file from last commit
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk@550682 13f79535-47bb-0310-9956-ffa450edef68

---
+++ qpid/cpp/src/tests/Cluster_child.cpp
@@ -0,0 +1,51 @@
+/*
+ *
+ * Copyright (c) 2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Child process for the Cluster test suite multi-process tests.
+
+#include "Cluster.h"
+#include "test_tools.h"
+
+using namespace std;
+using namespace qpid;
+using namespace qpid::cluster;
+using namespace qpid::framing;
+using namespace qpid::sys;
+
+
+static const ProtocolVersion VER;
+
+/** Chlid part of Cluster::clusterTwo test */
+void clusterTwo() {
+    VectorFrameHandler received;
+    TestCluster cluster("Test", "amqp::2", received, VER);
+    BOOST_REQUIRE(cluster.waitFor(2));
+    
+    BOOST_REQUIRE(received.waitFor(1));
+    BOOST_CHECK_TYPEID_EQUAL(ChannelOkBody, *received[0].getBody());
+    AMQFrame frame(VER, 1, new BasicGetOkBody(VER));
+    cluster.handle(frame);
+    BOOST_REQUIRE(received.waitFor(2));
+    BOOST_CHECK_TYPEID_EQUAL(BasicGetOkBody, *received[1].getBody());
+} 
+
+int test_main(int, char**) {
+    clusterTwo();
+    return 0;
+}
+
