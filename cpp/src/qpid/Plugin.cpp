ae896d73d4914fa96f053a0443f6b05003c12b35
Alan Conway <aconway@apache.org>
2007-07-04 03:24:48 +0000

    2007-07-02    <aconway@redhat.com
             * qpid/sys/Shlib.h, .cpp: Portable shared library abstraction.
              - Shlib: load, unload, getSymbol
              - AutoShlib: unload in destructor.
             * qpid/sys/apr/Shlib.cpp sys/posix/Shlib.cpp: APR/Posix impls
    
             * qpid/sys/Module.h: Reimplemented Module in terms of AutoShlib.
             * qpid/Plugin.cpp, .h: Removed dlopen - use Shlib instead.
    
             * qpid/broker/Broker.cpp: Added missing #include "qpid/log/Statement.h"
             * tests/Shlib.cpp, shlibtest.cpp: Unit test
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@553056 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/Plugin.cpp
+++ cpp/src/qpid/Plugin.cpp
@@ -1,18 +1,20 @@
 /*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Copyright (c) 2006 The Apache Software Foundation
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  *
  */
 
@@ -34,38 +36,5 @@ Options*  PluginProvider::getOptions() { return 0; }
 const std::vector<PluginProvider*>& PluginProvider::getProviders() {
     return providers;
 }
+
 } // namespace qpid
-
-// TODO aconway 2007-06-28: GNU lib has portable dlopen if we go that way.
-
-#ifdef USE_APR_PLATFORM
-
-#include "qpid/sys/apr/APRBase.h"
-#include "qpid/sys/apr/APRPool.h"
-#include <apr_dso.h>
-
-namespace qpid {
-void dlopen(const char* name) {
-    apr_dso_handle_t* handle;
-    CHECK_APR_SUCCESS(
-        apr_dso_load(&handle, name, sys::APRPool::get()));
-}
-} // namespace qpid
-
-#else // Posix
-
-#include "qpid/sys/posix/check.h"
-#include <dlfcn.h>
-
-namespace qpid {
-void dlopen(const char* name) {
-    ::dlerror();
-    ::dlopen(name, RTLD_NOW);
-    const char* error = ::dlerror();
-    if (error) {
-        THROW_QPID_ERROR(INTERNAL_ERROR, error);
-    }
-}
-} // namespace qpidpp
-
-#endif // USE_APR_PLATFORM
