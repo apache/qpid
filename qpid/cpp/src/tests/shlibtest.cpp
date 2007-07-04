50fcf52f0468f81316ed5d40f09602e2e3f49e36
Alan Conway <aconway@apache.org>
2007-07-04 03:24:48 +0000

    2007-07-02    <aconway@redhat.com
             * qpid/sys/Shlib.h, .cpp: Portable shared library abstraction.
              - Shlib: load, unload, getSymbol
              - AutoShlib: unload in destructor.
             * qpid/sys/apr/Shlib.cpp sys/posix/Shlib.cpp: APR/Posix impls
    
             * qpid/sys/Module.h: Reimplemented Module in terms of AutoShlib.
             * qpid/Plugin.cpp, .h: Removed dlopen - use Shlib instead.
    
             * qpid/broker/Broker.cpp: Added missing #include "qpid/log/Statement.h"
             * tests/Shlib.cpp, shlibtest.cpp: Unit test
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@553056 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/tests/shlibtest.cpp
+++ qpid/cpp/src/tests/shlibtest.cpp
@@ -1,8 +1,4 @@
-#ifndef QPID_SYS_MODULE_H
-#define QPID_SYS_MODULE_H
-
 /*
- *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -10,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -22,29 +18,11 @@
  *
  */
 
-#include "qpid/sys/Shlib.h"
-#include <boost/noncopyable.hpp>
+int* loaderData = 0;
+extern "C" void callMe(int *i) { loaderData=i; }
 
-namespace qpid {
-namespace sys {
+struct OnUnload { ~OnUnload() { *loaderData=42; } };
+OnUnload unloader;              // For destructor.
 
-template <class T> class Module : public AutoShlib, private boost::noncopyable
-{
-  public:
-    Module(const std::string& name) :
-        AutoShlib(name),
-        ptr(getSymbol<T*(*)()>("create")()) {}
-                
-    T* get() { return ptr; }
-    T* operator->() { return ptr; }
-    ~Module() throw() {
-        getSymbol<void (*)(T*)>("destroy")(ptr);
-    }
 
-  private:
-    T* ptr;
-};
 
-}}
-
-#endif  /*!QPID_SYS_MODULE_H*/
