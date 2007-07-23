7e7d6c3cb2bc0e72b7e8c03357f8bf09594b0f83
Rajith Muditha Attapattu <rajith@apache.org>
2007-07-23 15:22:50 +0000

    we need to build a parallel stack within the same client  module, hence removing the seperate module
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@558771 13f79535-47bb-0310-9956-ffa450edef68

--- java/newclient/src/main/java/org/apache/qpid/nclient/amqp/state/AMQPStateListener.java
+++
@@ -1,8 +0,0 @@
-package org.apache.qpid.nclient.amqp.state;
-
-import org.apache.qpid.nclient.core.AMQPException;
-
-public interface AMQPStateListener 
-{
-	public void stateChanged(AMQPStateChangedEvent event) throws AMQPException;
-}
