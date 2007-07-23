69e24fc5c63605b6a42a5a13f09d9f8213eed5cd
Arnaud Simon <arnaudsimon@apache.org>
2007-07-23 14:52:23 +0000

    JAve Qpid API
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@558756 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/client/src/main/java/org/apache/qpid/nclient/qpidapi/DeleteExchangeOption.java
@@ -0,0 +1,12 @@
+package org.apache.qpid.nclient.qpidapi;
+
+/**
+ * Created by Arnaud Simon
+ * Date: 23-Jul-2007
+ * Time: 12:55:55
+ */
+public enum DeleteExchangeOption
+{
+    IF_UNUSED,
+    NOWAIT;
+}
