ea33af43ceab1aebfce1f9499bb7e2a7c406e453
Alan Conway <aconway@apache.org>
2007-07-27 14:01:36 +0000

    * src/tests/ais_check, cluster.mk: Run AIS tests only if: 	 - CLUSTER makefile conditional set by configure. 	 - Effective gid == ais 	 - aisexec is running 	Otherwise print a warning.
    	* src/tests/EventChannelConnectionTest.cpp
    	* src/qpid/cluster/doxygen_overview.h
    	Removed unused files.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@560234 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/tests/cluster.mk
+++ qpid/cpp/src/tests/cluster.mk
@@ -12,16 +12,23 @@ lib_cluster = $(abs_builddir)/../libqpidcluster.la
 # Cluster tests.
 # 
 
+# ais_check runs ais if the conditions to run AIS tests
+# are met, otherwise it prints a warning.
 TESTS+=ais_check
 EXTRA_DIST+=ais_check
+AIS_TESTS=
 
-TESTS+=Cpg
+ais_check: ais_tests
+ais_tests:
+	echo $(AIS_TESTS) >$@
+
+AIS_TESTS+=Cpg
 check_PROGRAMS+=Cpg
 Cpg_SOURCES=Cpg.cpp
 Cpg_LDADD=$(lib_cluster) -lboost_unit_test_framework
 
 # TODO aconway 2007-07-26: Fix this test.
-#TESTS+=Cluster
+#AIS_TESTS+=Cluster
 check_PROGRAMS+=Cluster
 Cluster_SOURCES=Cluster.cpp Cluster.h
 Cluster_LDADD=$(lib_cluster) -lboost_unit_test_framework
@@ -31,7 +38,7 @@ Cluster_child_SOURCES=Cluster_child.cpp Cluster.h
 Cluster_child_LDADD=$(lib_cluster) -lboost_test_exec_monitor
 
 # TODO aconway 2007-07-03: In progress
-#TESTS+=cluster_client
+#AIS_TESTS+=cluster_client
 check_PROGRAMS+=cluster_client
 cluster_client_SOURCES=cluster_client.cpp
 cluster_client_LDADD=$(lib_client) -lboost_unit_test_framework
