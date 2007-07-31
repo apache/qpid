9a98a8c8915b67ad89104f8dbcd6feab11fe53ba
Rajith Muditha Attapattu <rajith@apache.org>
2007-07-31 15:37:39 +0000

    adding impl classes
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk@561362 13f79535-47bb-0310-9956-ffa450edef68

---
+++ qpid/java/client/src/main/java/org/apache/qpid/nclient/impl/ClientSession.java
@@ -0,0 +1,64 @@
+package org.apache.qpid.nclient.impl;
+
+import org.apache.qpid.nclient.api.Message;
+import org.apache.qpid.nclient.api.MessageReceiver;
+import org.apache.qpid.nclient.api.MessageSender;
+import org.apache.qpidity.Header;
+import org.apache.qpidity.Option;
+import org.apache.qpidity.QpidException;
+import org.apache.qpidity.Session;
+
+public class ClientSession extends Session implements org.apache.qpid.nclient.api.Session
+{	
+	/**
+	 * ---------------------------------------------------
+	 * Message methods
+	 * ---------------------------------------------------
+	 */
+	public MessageSender createSender(String queueName) throws QpidException
+	{
+		return null;
+	}
+
+	public MessageReceiver createReceiver(String queueName, Option... options) throws QpidException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setTransacted() throws QpidException, IllegalStateException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void messageBody(byte[] src) throws QpidException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void messageClose() throws QpidException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void messageHeaders(Header... headers) throws QpidException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void messageTransfer(String destination, Message msg) throws QpidException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void messageTransfer(Option... options) throws QpidException
+	{
+		// TODO Auto-generated method stub
+		
+	}	
+}
