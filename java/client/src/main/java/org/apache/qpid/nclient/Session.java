1548d94328342f17b95b538cb688f1a0ea907429
Arnaud Simon <arnaudsimon@apache.org>
2007-08-05 15:12:50 +0000

    Added message selector
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@562885 13f79535-47bb-0310-9956-ffa450edef68

--- java/client/src/main/java/org/apache/qpid/nclient/Session.java
+++ java/client/src/main/java/org/apache/qpid/nclient/Session.java
@@ -33,8 +33,16 @@ import org.apache.qpidity.api.Message;
  */
 public interface Session
 {
+    public static final short ACQUIRE_MODE_NO_ACQUIRE = 0;
+    public static final short ACQUIRE_MODE_PRE_ACQUIRE = 1;
+    public static final short CONFIRM_MODE_REQUIRED = 1;
+    public static final short CONFIRM_MODE_NOT_REQUIRED = 0;
+    public static final short MESSAGE_FLOW_MODE_CREDIT = 0;
+    public static final short MESSAGE_FLOW_MODE_WINDOW = 1;
+    public static final short MESSAGE_FLOW_UNIT_MESSAGE = 0;
+    public static final short MESSAGE_FLOW_UNIT_BYTE = 1;
 
-    //------------------------------------------------------ 
+    //------------------------------------------------------
     //                 Session housekeeping methods
     //------------------------------------------------------
     /**
@@ -154,19 +162,27 @@ public interface Session
      * <ul>
      * <li> NO_LOCAL
      * <li> EXCLUSIVE
-     * <li> NO_ACQUIRE
-     * <li> CONFIRM
      * </ul>
      * <p> In the absence of a particular option, defaul values are:
      * <ul>
      * <li> NO_LOCAL = false
      * <li> EXCLUSIVE = false
-     * <li> PRE-ACCQUIRE
-     * <li> CONFIRM = false
      * </ul>
      *
      * @param queue       The queue this receiver is receiving messages from.
      * @param destination The destination for the subscriber ,a.k.a the delivery tag.
+     * @param confirmMode <ul> </li>off (0): confirmation is not required, once a message has been transferred in pre-acquire
+     *                    mode (or once acquire has been sent in no-acquire mode) the message is considered
+     *                    transferred
+     *                    <p/>
+     *                    <li> on  (1): an acquired message (whether acquisition was implicit as in pre-acquire mode or
+     *                    explicit as in no-acquire mode) is not considered transferred until the original
+     *                    transfer is complete (signaled via execution.complete)
+     *                    </ul>
+     * @param acquireMode <ul> <li> no-acquire  (0): the message must be explicitly acquired
+     *                    <p/>
+     *                    <li> pre-acquire (1): the message is acquired when the transfer starts
+     *                    </ul>
      * @param listener    The listener for this destination. When big message are transfered then
      *                    it is recommended to use a {@link MessagePartListener}.
      * @param options     Set of Options.
@@ -174,8 +190,9 @@ public interface Session
      *                    on the providers implementation.
      * @throws QpidException If the session fails to create the receiver due to some error.
      */
-    public void messageSubscribe(String queue, String destination, MessagePartListener listener, Map<String, ?> filter,
-                                 Option... options) throws QpidException;
+    public void messageSubscribe(String queue, String destination, short confirmMode, short acquireMode,
+                                 MessagePartListener listener, Map<String, ?> filter, Option... options)
+            throws QpidException;
 
     /**
      * This method cancels a consumer. This does not affect already delivered messages, but it does
@@ -262,7 +279,7 @@ public interface Session
      * further credit is received.
      *
      * @param destination The destination to stop.
-     * @throws QpidException If stopping fails due to some error.     
+     * @throws QpidException If stopping fails due to some error.
      */
     public void messageStop(String destination) throws QpidException;
 
@@ -274,7 +291,7 @@ public interface Session
      * @param range Range of acknowledged messages.
      * @throws QpidException If the acknowledgement of the messages fails due to some error.
      */
-    public void messageAcknowledge(Range... range) throws QpidException;
+    public void messageAcknowledge(Range<Long>... range) throws QpidException;
 
     /**
      * Reject ranges of acquired messages.
@@ -284,7 +301,7 @@ public interface Session
      * @param range Range of rejected messages.
      * @throws QpidException If those messages cannot be rejected dus to some error
      */
-    public void messageReject(Range... range) throws QpidException;
+    public void messageReject(Range<Long>... range) throws QpidException;
 
     /**
      * Try to acquire ranges of messages hence releasing them form the queue.
@@ -298,7 +315,7 @@ public interface Session
      * @return Ranges of explicitly acquired messages.
      * @throws QpidException If this message cannot be acquired dus to some error
      */
-    public Range[] messageAcquire(Range... range) throws QpidException;
+    public Range<Long>[] messageAcquire(Range<Long>... range) throws QpidException;
 
     /**
      * Give up responsibility for processing ranges of messages.
@@ -307,7 +324,7 @@ public interface Session
      * @param range Ranges of messages to be released.
      * @throws QpidException If this message cannot be released dus to some error.
      */
-    public void messageRelease(Range... range) throws QpidException;
+    public void messageRelease(Range<Long>... range) throws QpidException;
 
     // -----------------------------------------------
     //            Local transaction methods
@@ -359,8 +376,8 @@ public interface Session
      * @throws QpidException If the session fails to declare the queue due to some error.
      * @see Option
      */
-    public void queueDeclare(String queueName, String alternateExchange, Map<String, ?> arguments,
-                             Option... options) throws QpidException;
+    public void queueDeclare(String queueName, String alternateExchange, Map<String, ?> arguments, Option... options)
+            throws QpidException;
 
     /**
      * Bind a queue with an exchange.
@@ -370,8 +387,8 @@ public interface Session
      * @param routingKey   The routing key.
      * @throws QpidException If the session fails to bind the queue due to some error.
      */
-    public void queueBind(String queueName, String exchangeName, String routingKey, Map<String, ?> arguments) throws
-                                                                                                              QpidException;
+    public void queueBind(String queueName, String exchangeName, String routingKey, Map<String, ?> arguments)
+            throws QpidException;
 
     /**
      * Unbind a queue from an exchange.
@@ -381,8 +398,8 @@ public interface Session
      * @param routingKey   The routing key.
      * @throws QpidException If the session fails to unbind the queue due to some error.
      */
-    public void queueUnbind(String queueName, String exchangeName, String routingKey, Map<String, ?> arguments) throws
-                                                                                                                QpidException;
+    public void queueUnbind(String queueName, String exchangeName, String routingKey, Map<String, ?> arguments)
+            throws QpidException;
 
     /**
      * Purge a queue. i.e. delete all enqueued messages
