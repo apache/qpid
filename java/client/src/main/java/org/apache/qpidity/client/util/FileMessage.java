dae146867b5794cc86a7b3d8c162dbabd7ab9d22
Rajith Muditha Attapattu <rajith@apache.org>
2007-08-13 16:41:34 +0000

    Added support for message handling. Sending Messages ------------------- ByteBufferMessage for small messages - data will be in memory. FileMessage and StreamingMessage for sending large messages.
    Receiving Messages
    -------------------
    For small messages u can use MessageListener and receive ByteBufferMessage.
    You need to use the MessageListener with the MessagePartListenerAdapter.
    
    For large messages it is recomended to use MessagePartListener.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@565407 13f79535-47bb-0310-9956-ffa450edef68

--- java/client/src/main/java/org/apache/qpidity/client/util/FileMessage.java
+++ java/client/src/main/java/org/apache/qpidity/client/util/FileMessage.java
@@ -1,5 +1,6 @@
 package org.apache.qpidity.client.util;
 
+import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -29,10 +30,8 @@ import org.apache.qpidity.api.Message;
  * and stream it.
  *
  */
-public class FileMessage implements Message
+public class FileMessage extends ReadOnlyMessage implements Message
 {
-    private MessageProperties _messageProperties;
-    private DeliveryProperties _deliveryProperties;
     private FileChannel _fileChannel;
     private int _chunkSize;
     private long _fileSize;
@@ -52,46 +51,24 @@ public class FileMessage implements Message
             _chunkSize = (int)_fileSize;
         }
     }
-    
-    public void appendData(byte[] src)
-    {
-        throw new UnsupportedOperationException("This Message is read only after the initial source");
-    }
-
-    public void appendData(ByteBuffer src)
-    {
-        throw new UnsupportedOperationException("This Message is read only after the initial source");
-    }
-
-    public DeliveryProperties getDeliveryProperties()
-    {
-        return _deliveryProperties;
-    }
-
-    public MessageProperties getMessageProperties()
-    {
-        return _messageProperties;
-    }
 
     public void readData(byte[] target) throws IOException
     {        
-        int readLen = target.length <= _chunkSize ? target.length : _chunkSize;
-        if (_pos + readLen > _fileSize)
-        {
-            readLen = (int)(_fileSize - _pos);
-        }
-        MappedByteBuffer bb = _fileChannel.map(FileChannel.MapMode.READ_ONLY, _pos, readLen);
-        _pos += readLen;
-        bb.get(target);
+        throw new UnsupportedOperationException();              
     }
     
     public ByteBuffer readData() throws IOException
     {
+        if (_pos == _fileSize)
+        {
+            throw new EOFException();
+        }
+        
         if (_pos + _chunkSize > _fileSize)
         {
             _chunkSize = (int)(_fileSize - _pos);
         }
-        MappedByteBuffer bb = _fileChannel.map(FileChannel.MapMode.READ_ONLY, _pos, _chunkSize);
+        MappedByteBuffer bb = _fileChannel.map(FileChannel.MapMode.READ_ONLY, _pos, _chunkSize);        
         _pos += _chunkSize;
         return bb;
     }
