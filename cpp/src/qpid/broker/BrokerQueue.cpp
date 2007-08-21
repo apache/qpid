70d2bc1d44e504bf0755b1c81924e022379e95c4
Carl C. Trieloff <cctrieloff@apache.org>
2007-08-21 19:11:07 +0000

    - header correction for buffer - added tests for one msg to multiple queue in async mode - added counter for async multiple msg
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@568236 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/broker/BrokerQueue.cpp
+++ cpp/src/qpid/broker/BrokerQueue.cpp
@@ -249,6 +249,7 @@ bool Queue::canAutoDelete() const{
 bool Queue::enqueue(TransactionContext* ctxt, Message::shared_ptr& msg)
 {
     if (msg->isPersistent() && store) {
+	msg->enqueueAsync(); //increment to async counter -- for message sent to more than one queue
         store->enqueue(ctxt, *msg.get(), *this);
 	return true;
     }
