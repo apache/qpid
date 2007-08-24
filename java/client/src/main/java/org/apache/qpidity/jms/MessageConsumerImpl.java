65f3a76fbfb0ca5d3178be80e35f378faad8ac62
Arnaud Simon <arnaudsimon@apache.org>
2007-08-24 15:59:22 +0000

    updated afterReceive
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@569431 13f79535-47bb-0310-9956-ffa450edef68

--- java/client/src/main/java/org/apache/qpidity/jms/MessageConsumerImpl.java
+++ java/client/src/main/java/org/apache/qpidity/jms/MessageConsumerImpl.java
@@ -393,7 +393,7 @@ public class MessageConsumerImpl extends MessageActor implements MessageConsumer
                         }
                         catch (InterruptedException e)
                         {
-                            // do nothing
+                            throw new RuntimeException(e);
                         }
                     }
                     if (_incomingMessage != null)
