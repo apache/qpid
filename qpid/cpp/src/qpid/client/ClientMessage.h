86cd3e1d42c223009f8b12123f480abea9860e94
Gordon Sim <gsim@apache.org>
2007-09-11 11:25:27 +0000

    Moved old ClientChannel class from using basic to using message for publish & consume. (Get and qos still use the basic class's defintions, that will be changed next)
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk@574551 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/qpid/client/ClientMessage.h
+++ qpid/cpp/src/qpid/client/ClientMessage.h
@@ -22,13 +22,7 @@
  *
  */
 #include <string>
-#include "qpid/framing/BasicHeaderProperties.h"
-#include "qpid/framing/FrameSet.h"
-#include "qpid/framing/MethodContent.h"
-
-#include "qpid/framing/BasicDeliverBody.h"
-#include "qpid/framing/BasicGetOkBody.h"
-#include "qpid/framing/MessageTransferBody.h"
+#include "qpid/framing/TransferContent.h"
 
 namespace qpid {
 namespace client {
@@ -39,49 +33,37 @@ namespace client {
  *
  * \ingroup clientapi
  */
-// FIXME aconway 2007-04-05: Should be based on MessageTransfer properties not
-// basic header properties.
-class Message : public framing::BasicHeaderProperties, public framing::MethodContent {
-  public:
-    Message(const std::string& data_=std::string()) : data(data_) {}
+class Message : public framing::TransferContent 
+{
+public:
+    Message(const std::string& data_=std::string()) : TransferContent(data_) {}
 
-    const std::string& getData() const { return data; }
-    void setData(const std::string& _data) { data = _data; }
-
-    std::string getDestination() const { return destination; }
-    void setDestination(const std::string& dest) { destination = dest; }
-
-    // TODO aconway 2007-03-22: only needed for Basic.deliver support.
-    uint64_t getDeliveryTag() const { return deliveryTag; }
-    void setDeliveryTag(uint64_t dt) { deliveryTag = dt; }
-
-    bool isRedelivered() const { return redelivered; }
-    void setRedelivered(bool _redelivered){  redelivered = _redelivered; }
-
-    framing::AMQHeaderBody getHeader() const
+    std::string getDestination() const 
     { 
-        framing::AMQHeaderBody header;
-        BasicHeaderProperties* properties = header.get<BasicHeaderProperties>(true);
-        BasicHeaderProperties::copy<BasicHeaderProperties, Message>(*properties, *this);
-        properties->setContentLength(data.size());
-        return header;
+        return destination; 
+    }
+    
+    void setDestination(const std::string& dest) 
+    { 
+        destination = dest; 
     }
 
-    //TODO: move this elsewhere (GRS 24/08/2007)
-    void populate(framing::FrameSet& frameset)
-    {
-        const BasicHeaderProperties* properties = frameset.getHeaders()->get<BasicHeaderProperties>();
-        if (properties) {
-            BasicHeaderProperties::copy<Message, BasicHeaderProperties>(*this, *properties);
-        }
-        frameset.getContent(data);
+    bool isRedelivered() const 
+    { 
+        return hasDeliveryProperties() && getDeliveryProperties().getRedelivered(); 
     }
 
-  private:
-    std::string data;
+    void setRedelivered(bool redelivered) { 
+        getDeliveryProperties().setRedelivered(redelivered); 
+    }
+
+    framing::FieldTable& getHeaders() 
+    { 
+        return getMessageProperties().getApplicationHeaders(); 
+    }
+
+private:
     std::string destination;
-    bool redelivered;
-    uint64_t deliveryTag;
 };
 
 }}
