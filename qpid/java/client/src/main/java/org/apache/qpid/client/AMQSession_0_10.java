f03b5e6c51b4d47f76a462e47512e9ef39c8a1bb
Arnaud Simon <arnaudsimon@apache.org>
2007-10-11 10:08:28 +0000

    Changed message confirm_mode when no ack is used
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk@583765 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/client/src/main/java/org/apache/qpid/client/AMQSession_0_10.java
+++ qpid/java/client/src/main/java/org/apache/qpid/client/AMQSession_0_10.java
@@ -339,7 +339,9 @@ public class AMQSession_0_10 extends AMQSession
         {
             throw new AMQException(AMQConstant.INTERNAL_ERROR, "problem when registering consumer", e);
         }
-        getQpidSession().messageSubscribe(queueName.toString(), tag.toString(), Session.TRANSFER_CONFIRM_MODE_REQUIRED,
+        getQpidSession().messageSubscribe(queueName.toString(), tag.toString(),
+                                          (consumer.getAcknowledgeMode() != org.apache.qpid.jms.Session.NO_ACKNOWLEDGE) ?
+                                                  Session.TRANSFER_CONFIRM_MODE_REQUIRED : Session.TRANSFER_CONFIRM_MODE_NOT_REQUIRED,
                                           preAcquire ? Session.TRANSFER_ACQUIRE_MODE_PRE_ACQUIRE : Session.TRANSFER_ACQUIRE_MODE_NO_ACQUIRE,
                                           new MessagePartListenerAdapter((BasicMessageConsumer_0_10) consumer), null,
                                           consumer.isNoLocal() ? Option.NO_LOCAL : Option.NO_OPTION,
