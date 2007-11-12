e1a965fda1327900b545577d36dcf1e66fda3884
Rajith Muditha Attapattu <rajith@apache.org>
2007-11-12 18:08:15 +0000

    Added a new set of performance tests which is more focused on getting the throughput. The producers and consumers can be run from separate machines. There is no pass/fail, it merely records message count, memory etc at periodic intervals and prints a summary at the the end of the test. Since there are periodic entries, you could identify the rough time time it crashed and memory consumption etc.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@594239 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/perftests/src/main/java/org/apache/qpid/client/perf/MessageProducerTest.java
@@ -0,0 +1,159 @@
+package org.apache.qpid.client.perf;
+
+import java.io.FileWriter;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+
+import org.apache.qpid.client.AMQConnection;
+import org.apache.qpid.client.AMQTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MessageProducerTest extends Options implements Runnable
+{
+    private static final Logger _logger = LoggerFactory.getLogger(MessageProducerTest.class);
+    private SimpleDateFormat df = new SimpleDateFormat("h:mm a");
+
+    private Map<Integer,JMSProducer> _producers = new ConcurrentHashMap<Integer,JMSProducer>();
+    private int _count;
+    String _logFileName;
+    long _startTime;
+    long totalMsgCount;
+
+    public void start() throws Exception
+    {
+       this.parseOptions();
+       boolean useSameDest = true;
+       _logFileName = _logFilePath + "/MessageProducerTest_" + System.currentTimeMillis();
+
+       // use each destination with a different producer
+       if (_producerCount == destArray.length)
+       {
+           useSameDest = false;
+       }
+       for (;_count < _producerCount;_count++)
+       {
+           createAndStartProducer(useSameDest?destArray[0]:destArray[_count]);
+       }
+    }
+
+    private void createAndStartProducer(String routingKey)throws Exception
+    {
+        AMQConnection con = ConnectionUtility.getInstance().getConnection();
+        con.start();
+        Destination dest = new AMQTopic(con,routingKey);
+        JMSProducer prod = new JMSProducer(String.valueOf(_count),(Connection)con, dest,_messageSize, _transacted);
+        Thread t = new Thread(prod);
+        t.setName("JMSProducer-"+_count);
+        t.start();
+        _producers.put(_count, prod);
+    }
+
+    private void startTimerThread()
+    {
+        Thread t = new Thread(this);
+        t.setName("MessageProducerTest-TimerThread");
+        t.start();
+    }
+
+    public void run()
+    {
+        boolean run = true;
+        _startTime = System.currentTimeMillis();
+        try
+        {
+            while(run)
+            {
+                Thread.sleep(_logDuration);
+                runReaper(false);
+
+                if(System.currentTimeMillis() - _startTime > _expiry )
+                {
+                    // time to stop the test.
+                    for (Integer id : _producers.keySet())
+                    {
+                        JMSProducer prod = _producers.get(id);
+                        prod.stopProducing();
+                    }
+                    runReaper(true);
+                    run = false;
+                }
+            }
+        }
+        catch (InterruptedException e)
+        {
+            _logger.error("The timer thread exited",e);
+        }
+    }
+
+    public void runReaper(boolean printSummary)
+    {
+        try
+        {
+            FileWriter _logFile = new FileWriter(_logFileName + ".csv",true);
+            for (Integer id : _producers.keySet())
+            {
+                JMSProducer prod = _producers.get(id);
+                StringBuffer buf = new StringBuffer("JMSProducer(").append(prod.getId()).append("),");
+                Date d = new Date(System.currentTimeMillis());
+                buf.append(df.format(d)).append(",");
+                buf.append(d.getTime()).append(",");
+                buf.append(prod.getCurrentMessageCount()).append("\n");
+                _logFile.write(buf.toString());
+                totalMsgCount = totalMsgCount + prod.getCurrentMessageCount();
+            }
+            _logFile.close();
+
+            FileWriter _memoryLog = new FileWriter(_logFileName + "_memory.csv",true);
+            StringBuffer buf = new StringBuffer("JMSProducer,");
+            Date d = new Date(System.currentTimeMillis());
+            buf.append(df.format(d)).append(",");
+            buf.append(d.getTime()).append(",");
+            buf.append(totalMsgCount).append(",");
+            buf.append(Runtime.getRuntime().totalMemory() -Runtime.getRuntime().freeMemory()).append("\n");
+            _memoryLog.write(buf.toString());
+            _memoryLog.close();
+            if (printSummary)
+            {
+                double totaltime = (d.getTime() - _startTime)*1000; // trying to get a per sec rate
+                double dCount = totalMsgCount;
+                double ratio = dCount/totaltime;
+                FileWriter _summaryLog = new FileWriter(_logFileName + "_Summary",true);
+                buf = new StringBuffer("MessageProducerTest \n Test started at : ");
+                buf.append(df.format(new Date(_startTime))).append("\n Test finished at : ");
+                d = new Date(System.currentTimeMillis());
+                buf.append(df.format(d)).append("\n Total Time taken (ms):");
+                buf.append(totaltime).append("\n Total messages sent:");
+                buf.append(totalMsgCount).append("\n Producer rate:");
+                buf.append(ratio).append("\n");
+                _summaryLog.write(buf.toString());
+                System.out.println("---------- Test Ended -------------");
+                _summaryLog.close();
+            }
+        }
+        catch(Exception e)
+        {
+            _logger.error("Error printing info to the log file",e);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            MessageProducerTest test = new MessageProducerTest();
+            test.start();
+            test.startTimerThread();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
