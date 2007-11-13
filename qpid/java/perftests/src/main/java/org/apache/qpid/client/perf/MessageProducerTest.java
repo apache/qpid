7b3e3578da19c5e57e26741311cfdf4d3cfcb2ca
Rajith Muditha Attapattu <rajith@apache.org>
2007-11-13 00:14:13 +0000

    modified to collect memory stats before the test.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk@594360 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/perftests/src/main/java/org/apache/qpid/client/perf/MessageProducerTest.java
+++ qpid/java/perftests/src/main/java/org/apache/qpid/client/perf/MessageProducerTest.java
@@ -65,6 +65,7 @@ public class MessageProducerTest extends Options implements Runnable
     {
         boolean run = true;
         _startTime = System.currentTimeMillis();
+        runReaper(false);
         try
         {
             while(run)
