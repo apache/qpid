613ceb29988f1c5d44ac41510a0fa4424fb04142
Arnaud Simon <arnaudsimon@apache.org>
2007-11-14 09:38:05 +0000

    Added distinction between sync and async consumers
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@594814 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/perftests/src/main/java/org/apache/qpid/client/perf/JMSConsumer.java
+++ qpid/java/perftests/src/main/java/org/apache/qpid/client/perf/JMSConsumer.java
@@ -1,102 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.qpid.client.perf;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import javax.jms.BytesMessage;
-import javax.jms.Connection;
-import javax.jms.Destination;
-import javax.jms.MessageConsumer;
-import javax.jms.Session;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class JMSConsumer implements Runnable
+/**
+ *
+ *
+ */
+public interface JMSConsumer
 {
-    private static final Logger _logger = LoggerFactory.getLogger(JMSConsumer.class);
-
-    private String _id;
-    private Connection _connection;
-    private Session _session;
-    private MessageConsumer _consumer;
-    private Destination _destination;
-    private boolean _transacted;
-    private int _ackMode = Session.AUTO_ACKNOWLEDGE;
-    private AtomicBoolean _run = new AtomicBoolean(true);
-    private long _currentMsgCount;
-
-    /* Not implementing transactions for first phase */
-    public JMSConsumer(String id,Connection connection, Destination destination,boolean transacted,int ackMode) throws Exception
-    {
-        _id = id;
-        _connection = connection;
-        _destination = destination;
-        _transacted = transacted;
-        _ackMode = ackMode;
-    }
-
-    public void run()
-    {
-        _run.set(true);
-
-        try
-        {
-            _session = _connection.createSession(_transacted, _ackMode);
-            _consumer = _session.createConsumer(_destination);
-        }
-        catch(Exception e)
-        {
-            _logger.error("Error Setting up JMSProducer:"+ _id, e);
-        }
-
-        while (_run.get())
-        {
-            try
-            {
-                BytesMessage msg = (BytesMessage)_consumer.receive();
-                if (msg != null)
-                {
-                    long msgId = Integer.parseInt(msg.getJMSCorrelationID());
-                    /*if (_currentMsgCount+1 != msgId)
-                    {
-                        _logger.error("Error : Message received out of order in JMSConsumer:" + _id + " message id was " + msgId + " expected: " + _currentMsgCount+1);
-                    }*/
-                    _currentMsgCount ++;
-                }
-            }
-            catch(Exception e)
-            {
-                _logger.error("Error Receiving message from JMSConsumer:" + _id, e);
-            }
-        }
-        try
-        {
-            _session.close();
-            _connection.close();
-        }
-        catch(Exception e)
-        {
-            _logger.error("Error Closing JMSConsumer:"+ _id, e);
-        }
-    }
-
-    public void stopConsuming()
-    {
-        _run.set(false);
-        System.out.println("Producer received notification to stop");
-    }
-
-    public String getId()
-    {
-        return _id;
-    }
-
-    /* Not worried about synchronizing as accuracy here is not that important.
-     * So if this method is invoked the count maybe off by a few digits.
-     * But when the test stops, this will always return the proper count.
-     */
-    public long getCurrentMessageCount()
-    {
-        return _currentMsgCount;
-    }
+     public String getId();
+     public void stopConsuming();
+        public long getCurrentMessageCount();
 }
