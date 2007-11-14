322be332721eb373c1780e53c1657797d4c207cc
Arnaud Simon <arnaudsimon@apache.org>
2007-11-14 09:38:05 +0000

    Added distinction between sync and async consumers
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@594814 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/perftests/src/main/java/org/apache/qpid/client/perf/JMSAsyncConsumer.java
@@ -0,0 +1,91 @@
+/* Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.qpid.client.perf;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ *
+ *
+ */
+public class JMSAsyncConsumer implements MessageListener, JMSConsumer
+{
+    private static final Logger _logger = LoggerFactory.getLogger(JMSSyncConsumer.class);
+
+    private String _id;
+    private Connection _connection;
+    private Session _session;
+    private MessageConsumer _consumer;
+    private Destination _destination;
+    private boolean _transacted;
+    private int _ackMode = Session.AUTO_ACKNOWLEDGE;
+    private AtomicBoolean _run = new AtomicBoolean(true);
+    private long _currentMsgCount;
+
+    /* Not implementing transactions for first phase */
+    public JMSAsyncConsumer(String id,Connection connection, Destination destination,boolean transacted,int ackMode) throws Exception
+    {
+        _id = id;
+        _connection = connection;
+        _destination = destination;
+        _transacted = transacted;
+        _ackMode = ackMode;
+        _session = _connection.createSession(_transacted, _ackMode);
+        _consumer = _session.createConsumer(_destination);
+        _consumer.setMessageListener(this);
+    }
+
+
+
+    public void onMessage(Message message)
+    {
+        _currentMsgCount ++;
+    }
+
+    public void stopConsuming()
+    {
+        System.out.println("Producer received notification to stop");
+        try
+        {
+            _session.close();
+            _connection.close();
+        }
+        catch(Exception e)
+        {
+            _logger.error("Error Closing JMSSyncConsumer:"+ _id, e);
+        }
+    }
+
+    public String getId()
+    {
+        return _id;
+    }
+
+    /* Not worried about synchronizing as accuracy here is not that important.
+     * So if this method is invoked the count maybe off by a few digits.
+     * But when the test stops, this will always return the proper count.
+     */
+    public long getCurrentMessageCount()
+    {
+        return _currentMsgCount;
+    }
+}
