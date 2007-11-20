df31334b15634d18e7de45f70f59cea50f262942
Arnaud Simon <arnaudsimon@apache.org>
2007-11-20 10:59:04 +0000

    Added new JMS examples
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@596610 13f79535-47bb-0310-9956-ffa450edef68

---
+++ qpid/java/client/example/src/main/java/org/apache/qpid/example/jmsexample/requestResponse/MessageMirror.java
@@ -0,0 +1,166 @@
+package org.apache.qpid.example.jmsexample.requestResponse;
+
+import org.redhat.mrg.messaging.examples.BaseExample;
+
+import javax.jms.*;
+
+/**
+ * The example creates a MessageConsumer on the specified
+ * Destination which is used to synchronously consume messages. If a
+ * received message has a ReplyTo header then a new response message is sent
+ * to that specified destination.
+ *
+ */
+public class MessageMirror extends BaseExample
+{
+    /* Used in log output. */
+    private static final String CLASS = "MessageMirror";
+
+    /* The destination type */
+    private String _destinationType;
+
+    /* The destination Name */
+    private String _destinationName;
+
+    /**
+     * Create a MessageMirror client.
+     * @param args Command line arguments.
+     */
+    public MessageMirror(String[] args)
+    {
+        super(CLASS, args);
+        _destinationType = _argProcessor.getStringArgument("-destinationType");
+        _destinationName =  _argProcessor.getStringArgument("-destinationName");
+    }
+
+    /**
+     * Run the message mirror example.
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args)
+    {
+        _options.put("-destinationType", "Destination Type: queue/topic");
+        _defaults.put("-destinationType", "queue");
+         _options.put("-destinationName", "Destination Name");
+        _defaults.put("-destinationName", "message_queue");
+        MessageMirror messageMirror = new MessageMirror(args);
+        messageMirror.runTest();
+    }
+
+    /**
+     * Start the example.
+     */
+    private void runTest()
+    {
+        try
+        {
+            Destination destination;
+
+            if (_destinationType.equals("queue"))
+            {
+                // Lookup the queue
+                System.out.println(CLASS + ": Looking up queue with name: " + _destinationName);
+                destination = (Queue) getInitialContext().lookup(_destinationName);
+            }
+            else
+            {
+                // Lookup the topic
+                System.out.println(CLASS + ": Looking up topic with name: " + _destinationName);
+                destination = (Topic) getInitialContext().lookup(_destinationName);
+            }
+
+            // Declare the connection
+            Connection connection = getConnection();
+
+            // As this application is using a MessageConsumer we need to set an ExceptionListener on the connection
+            // so that errors raised within the JMS client library can be reported to the application
+            System.out.println(CLASS + ": Setting an ExceptionListener on the connection as sample uses a MessageConsumer");
+
+            connection.setExceptionListener(new ExceptionListener()
+            {
+                public void onException(JMSException jmse)
+                {
+                    // The connection may have broken invoke reconnect code if available.
+                    // The connection may have broken invoke reconnect code if available.
+                    System.err.println(CLASS + ": The sample received an exception through the ExceptionListener");
+                    System.exit(0);
+                }
+            });
+
+            // Create a session on the connection
+            // This session is a default choice of non-transacted and uses
+            // the auto acknowledge feature of a session.
+            System.out.println(CLASS + ": Creating a non-transacted, auto-acknowledged session");
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create a MessageConsumer
+            System.out.println(CLASS + ": Creating a MessageConsumer");
+            MessageConsumer messageConsumer = session.createConsumer(destination);
+
+            /**
+             * Create a MessageProducer - note that although we create the
+             */
+            System.out.println(CLASS + ": Creating a MessageProducer");
+            MessageProducer messageProducer;
+
+            // Now the messageConsumer is set up we can start the connection
+            System.out.println(CLASS + ": Starting connection so MessageConsumer can receive messages");
+            connection.start();
+
+            // Cycle round until all the messages are consumed.
+            Message requestMessage;
+            TextMessage responseMessage;
+            boolean end = false;
+            while (!end)
+            {
+                System.out.println(CLASS + ": Receiving the message");
+
+                requestMessage = messageConsumer.receive();
+
+                // Print out the details of the just received message
+                System.out.println(CLASS + ": Message received:");
+                System.out.println("\tID=" + requestMessage.getJMSMessageID());
+                System.out.println("\tCorrelationID=" + requestMessage.getJMSCorrelationID());
+
+                if (requestMessage instanceof TextMessage)
+                {
+                       if (((TextMessage) requestMessage).getText().equals("That's all, folks!"))
+                    {
+                        System.out.println("Received final message for " + destination);
+                        end = true;
+                    }
+                    System.out.println("\tContents = " + ((TextMessage)requestMessage).getText());
+                }
+
+                // Now bounce the message if a ReplyTo header was set.
+                if (requestMessage.getJMSReplyTo() != null)
+                {
+                     System.out.println("Activating response queue listener for: " + destination);
+                    responseMessage = session.createTextMessage("Activating response queue listener for: " + destination);
+                    String correlationID = requestMessage.getJMSCorrelationID();
+                    if (correlationID != null)
+                    {
+                        responseMessage.setJMSCorrelationID(correlationID);
+                    }
+                    messageProducer = session.createProducer(requestMessage.getJMSReplyTo()) ;
+                    messageProducer.send(responseMessage);
+                }
+                System.out.println();
+            }
+
+            // Close the connection to the server
+            System.out.println(CLASS + ": Closing connection");
+            connection.close();
+
+            // Close the JNDI reference
+            System.out.println(CLASS + ": Closing JNDI context");
+            getInitialContext().close();
+        }
+        catch (Exception exp)
+        {
+            exp.printStackTrace();
+            System.err.println(CLASS + ": Caught an Exception: " + exp);
+        }
+    }
+}
---
+++ qpid/java/client/example/src/main/java/org/apache/qpid/example/jmsexample/requestResponse/P2PRequestor.java
@@ -0,0 +1,147 @@
+package org.apache.qpid.example.jmsexample.requestResponse;
+
+import org.redhat.mrg.messaging.examples.BaseExample;
+
+import javax.jms.*;
+
+/**
+ * This example illustrates the use of the JMS utility class <code>QueueRequestor</code>
+ * which provides a synchronous RPC-like abstraction using temporary destinations
+ * to deliver responses back to the client.
+ *
+ * <p>Run with <code>-help</code> argument for a description of command line arguments.
+ *
+ */
+public class P2PRequestor extends BaseExample
+{
+    /* Used in log output. */
+    private static final String CLASS = "P2PRequestor";
+
+    /* The queue name  */
+    private String _queueName;
+
+    /**
+     * Create a P2PRequestor client.
+     * @param args Command line arguments.
+     */
+    public P2PRequestor(String[] args)
+    {
+        super(CLASS, args);
+        _queueName = _argProcessor.getStringArgument("-queueName");
+    }
+
+    /**
+     * Run the message requestor example.
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args)
+    {
+         _options.put("-queueName", "The queue name");
+        _defaults.put("-queueName", "message_queue");
+        P2PRequestor requestor = new P2PRequestor(args);
+        requestor.runTest();
+    }
+
+    /**
+     * Start the example.
+     */
+    private void runTest()
+    {
+        try
+        {
+            // Declare the connection
+            QueueConnection connection = (QueueConnection) getConnection();
+
+            // As this application is using a MessageConsumer we need to set an ExceptionListener on the connection
+            // so that errors raised within the JMS client library can be reported to the application
+            System.out.println(CLASS + ": Setting an ExceptionListener on the connection as sample uses a MessageConsumer");
+
+            connection.setExceptionListener(new ExceptionListener()
+            {
+                public void onException(JMSException jmse)
+                {
+                    // The connection may have broken invoke reconnect code if available.
+                    // The connection may have broken invoke reconnect code if available.
+                    System.err.println(CLASS + ": The sample received an exception through the ExceptionListener");
+                    System.exit(0);
+                }
+            });
+
+            // Create a session on the connection.
+            System.out.println(CLASS + ": Creating a non-transacted, auto-acknowledged session");
+            QueueSession session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Lookup the destination
+            System.out.println(CLASS + ": Looking up queue with name: " + _queueName);
+            Queue destination = (Queue) getInitialContext().lookup(_queueName);
+
+            // Create a QueueRequestor
+            System.out.println(CLASS + ": Creating a QueueRequestor");
+
+            QueueRequestor requestor = new QueueRequestor(session, destination);
+
+            // Now start the connection
+            System.out.println(CLASS + ": Starting connection");
+            connection.start();
+
+            // Create a message to send as a request for service
+            TextMessage request;
+
+            request = session.createTextMessage("\"Twas brillig, and the slithy toves\",\n" +
+                    "\t\t\"Did gire and gymble in the wabe.\",\n" +
+                    "\t\t\"All mimsy were the borogroves,\",\n" +
+                    "\t\t\"And the mome raths outgrabe.\"");
+
+            // Declare a message to be used for receiving any response
+           Message response;
+
+            // Get the number of times that this sample should request service
+            for (int i = 0; i < getNumberMessages(); i++)
+            {
+                /**
+                 * Set a message correlation value. This is not strictly required it is
+                 * just an example of how messages requests can be tied together.
+                 */
+                request.setJMSCorrelationID("Message " + i);
+                System.out.println(CLASS + ": Sending request " + i);
+
+                response = requestor.request(request);
+
+                // Print out the details of the message sent
+                System.out.println(CLASS + ": Message details of request");
+                System.out.println("\tID = " + request.getJMSMessageID());
+                System.out.println("\tCorrelationID = " + request.getJMSCorrelationID());
+                 System.out.println("\tContents= " + ((TextMessage)request).getText());
+
+                // Print out the details of the response received
+                System.out.println(CLASS + ": Message details of response");
+                System.out.println("\tID = " + response.getJMSMessageID());
+                System.out.println("\tCorrelationID = " + response.getJMSCorrelationID());
+                if (response instanceof TextMessage)
+                {
+                    System.out.println("\tContents= " + ((TextMessage) response).getText());
+                }
+
+                System.out.println();
+            }
+
+            //send the final message  for ending the mirror
+            // And send a final message to indicate termination.
+            request.setText("That's all, folks!");
+            MessageProducer messageProducer = session.createProducer(destination);
+           messageProducer.send(request, getDeliveryMode(), Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
+                                  
+            // Close the connection to the server
+            System.out.println(CLASS + ": Closing connection");
+            connection.close();
+
+            // Close the JNDI reference
+            System.out.println(CLASS + ": Closing JNDI context");
+            getInitialContext().close();
+        }
+        catch (Exception exp)
+        {
+            System.err.println(CLASS + ": Caught an Exception: " + exp);
+        }
+    }
+}
