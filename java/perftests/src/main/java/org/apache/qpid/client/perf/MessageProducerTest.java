e80b169599c6ec09343054b06d9f4162dc0f7ad4
Rajith Muditha Attapattu <rajith@apache.org>
2007-11-29 22:46:28 +0000

    Removed all the fancy threading with the test cases as these interfer with the test cases
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@599611 13f79535-47bb-0310-9956-ffa450edef68

--- java/perftests/src/main/java/org/apache/qpid/client/perf/MessageProducerTest.java
+++ java/perftests/src/main/java/org/apache/qpid/client/perf/MessageProducerTest.java
@@ -7,119 +7,97 @@ import java.text.SimpleDateFormat;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import javax.jms.BytesMessage;
 import javax.jms.Connection;
 import javax.jms.Destination;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
 
 import org.apache.qpid.client.AMQConnection;
 import org.apache.qpid.client.AMQQueue;
 import org.apache.qpid.client.AMQTopic;
+import org.apache.qpid.client.message.TestMessageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MessageProducerTest extends Options implements Runnable
+public class MessageProducerTest extends Options
 {
     private static final Logger _logger = LoggerFactory.getLogger(MessageProducerTest.class);
     private SimpleDateFormat df = new SimpleDateFormat("h:mm a");
 
-    private Map<Integer,JMSProducer> _producers = new ConcurrentHashMap<Integer,JMSProducer>();
-    private int _count;
     String _logFileName;
     long _startTime;
     long _totalMsgCount;
+    long _intervalCount;
 
-    public void start() throws Exception
+    private Connection _connection;
+    private Session _session;
+    private BytesMessage _payload;
+    private MessageProducer _producer;
+
+    public void init() throws Exception
     {
        this.parseOptions();
-       boolean useSameDest = true;
        _logFileName = _logFilePath + "/MessageProducerTest_" + System.currentTimeMillis();
 
-       // use each destination with a different producer
-       if (_producerCount == destArray.length)
-       {
-           useSameDest = false;
-       }
-       for (;_count < _producerCount;_count++)
-       {
-           createAndStartProducer(useSameDest?destArray[0]:destArray[_count]);
-       }
-    }
-
-    private void createAndStartProducer(String routingKey)throws Exception
-    {
-        AMQConnection con = ConnectionUtility.getInstance().getConnection();
-        con.start();
-        Destination dest = Boolean.getBoolean("useQueue")? new AMQQueue(con,routingKey) : new AMQTopic(con,routingKey);
-        JMSProducer prod = new JMSProducer(String.valueOf(_count),(Connection)con, dest,_messageSize, _transacted);
-        Thread t = new Thread(prod);
-        t.setName("JMSProducer-"+_count);
-        t.start();
-        _producers.put(_count, prod);
-    }
-
-    private void startTimerThread()
-    {
-        _startTime = System.currentTimeMillis();
-        if(Boolean.getBoolean("collect_stats"))
-        {
-            Thread t = new Thread(this);
-            t.setName("MessageProducerTest-TimerThread");
-            t.start();
-        }
-        try
-        {
-            printSummary();
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
+       AMQConnection _connection = ConnectionUtility.getInstance().getConnection();
+       _connection.start();
+       Destination dest = Boolean.getBoolean("useQueue")? new AMQQueue(_connection,_destination) : new AMQTopic(_connection,_destination);
+       _session = _connection.createSession(_transacted, Session.AUTO_ACKNOWLEDGE);
+       _payload = TestMessageFactory.newBytesMessage(_session, _messageSize);
+       _producer = _session.createProducer(dest);
+       // this should speedup the message producer
+       _producer.setDisableMessageTimestamp(true);
     }
 
     public void run()
     {
+        _startTime = System.currentTimeMillis();
         boolean run = true;
-        printHeading();
-        runReaper();
+        if(Boolean.getBoolean("collect_stats"))
+        {
+            printHeading();
+            runReaper();
+        }
+
         try
         {
             while (run)
             {
-                Thread.sleep(_logDuration);
-                runReaper();
+                _payload.setJMSMessageID(String.valueOf(_totalMsgCount+1));
+                _producer.send(_payload);
+                _totalMsgCount ++;
+                _intervalCount ++;
 
-                if (System.currentTimeMillis() - _startTime > _expiry)
+                // check every x messages to see if times up
+                if(_intervalCount >= _logFrequency)
                 {
-                    // time to stop the test.
-                    for (Integer id : _producers.keySet())
+                    _intervalCount = 0;
+                    if (Boolean.getBoolean("collect_stats"))
                     {
-                        JMSProducer producer = _producers.get(id);
-                        producer.stopProducing();
+                        runReaper();
                     }
-                    runReaper();
-                    run = false;
+                    if (System.currentTimeMillis() - _startTime >= _expiry)
+                    {
+                        // time to stop the test.
+                        _session.close();
+                        _connection.stop();
+                        run = false;
+                    }
                 }
             }
         }
-        catch (InterruptedException e)
+        catch (Exception e)
         {
             _logger.error("The timer thread exited", e);
         }
+        printSummary();
     }
 
     public void runReaper()
     {
         try
         {
-            long totalMsgCountThisInterval = 0;
-
-            for (Integer id : _producers.keySet())
-            {
-                JMSProducer producer = _producers.get(id);
-                totalMsgCountThisInterval = totalMsgCountThisInterval + producer.getCurrentMessageCount();
-
-            }
-            _totalMsgCount = _totalMsgCount + totalMsgCountThisInterval;
-
             FileWriter _memoryLog = new FileWriter(_logFileName + ".csv",true);
             StringBuffer buf = new StringBuffer();
             Date d = new Date(System.currentTimeMillis());
@@ -128,8 +106,6 @@ public class MessageProducerTest extends Options implements Runnable
             buf.append(d.getTime()).append(",");
             buf.append(_totalMsgCount).append(",");
             buf.append(_totalMsgCount*1000 /totaltime).append(",");
-            buf.append(totalMsgCountThisInterval).append(",");
-            buf.append(totalMsgCountThisInterval*1000/_logDuration).append(",");
             buf.append(Runtime.getRuntime().totalMemory() -Runtime.getRuntime().freeMemory()).append("\n");
             buf.append("\n");
             _memoryLog.write(buf.toString());
@@ -147,7 +123,7 @@ public class MessageProducerTest extends Options implements Runnable
         try
         {
             FileWriter _memoryLog = new FileWriter(_logFileName + ".csv",true);
-            String s = "Date/Time,Time (ms),total msg count,total rate (msg/sec),interval count,interval rate (msg/sec),memory";
+            String s = "Date/Time,Time (ms),total msg count,total rate (msg/sec),memory";
             _memoryLog.write(s);
             _memoryLog.close();
         }
@@ -157,33 +133,31 @@ public class MessageProducerTest extends Options implements Runnable
         }
     }
 
-    private void printSummary() throws Exception
+    private void printSummary()
     {
-        if (Boolean.getBoolean("collect_stats"))
+        try
         {
-            for (Integer id : _producers.keySet())
-            {
-                JMSProducer producer = _producers.get(id);
-                _totalMsgCount = _totalMsgCount + producer.getCurrentMessageCount();
 
-            }
+            long current = System.currentTimeMillis();
+            double time = current - _startTime;
+            double ratio = _totalMsgCount*1000/time;
+            FileWriter _summaryLog = new FileWriter(_logFileName + "_Summary",true);
+
+            StringBuffer buf = new StringBuffer("MessageProducerTest \n Test started at : ");
+            buf.append(df.format(new Date(_startTime))).append("\n Test finished at : ");
+            Date d = new Date(current);
+            buf.append(df.format(d)).append("\n Total Time taken (ms):");
+            buf.append(time).append("\n Total messages sent:");
+            buf.append(_totalMsgCount).append("\n producer rate:");
+            buf.append(ratio).append("\n");
+            _summaryLog.write(buf.toString());
+            System.out.println("---------- Test Ended -------------");
+            _summaryLog.close();
         }
-
-        long current = System.currentTimeMillis();
-        double time = current - _startTime;
-        double ratio = _totalMsgCount*1000/time;
-        FileWriter _summaryLog = new FileWriter(_logFileName + "_Summary",true);
-
-        StringBuffer buf = new StringBuffer("MessageProducerTest \n Test started at : ");
-        buf.append(df.format(new Date(_startTime))).append("\n Test finished at : ");
-        Date d = new Date(current);
-        buf.append(df.format(d)).append("\n Total Time taken (ms):");
-        buf.append(time).append("\n Total messages sent:");
-        buf.append(_totalMsgCount).append("\n producer rate:");
-        buf.append(ratio).append("\n");
-        _summaryLog.write(buf.toString());
-        System.out.println("---------- Test Ended -------------");
-        _summaryLog.close();
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args)
@@ -191,8 +165,8 @@ public class MessageProducerTest extends Options implements Runnable
         try
         {
             MessageProducerTest test = new MessageProducerTest();
-            test.start();
-            test.startTimerThread();
+            test.init();
+            test.run();
         }
         catch(Exception e)
         {
