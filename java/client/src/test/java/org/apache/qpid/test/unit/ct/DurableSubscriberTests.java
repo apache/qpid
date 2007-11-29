3022b6bb226f1d36c0ed10d5339e674e7ff3de6b
Rajith Muditha Attapattu <rajith@apache.org>
2007-11-29 00:08:07 +0000

    Enabled the broker module again, and Commented out the offending class that had compilation errors due missing classes from the new mina version
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@599216 13f79535-47bb-0310-9956-ffa450edef68

--- java/client/src/test/java/org/apache/qpid/test/unit/ct/DurableSubscriberTests.java
+++ java/client/src/test/java/org/apache/qpid/test/unit/ct/DurableSubscriberTests.java
@@ -5,9 +5,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -15,11 +15,11 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.qpid.test.unit.cr;
-
-import org.apache.qpid.testutil.QpidTestCase;
+package org.apache.qpid.test.unit.ct;
 
 import javax.jms.*;
+
+import org.apache.qpid.testutil.QpidTestCase;
 
 /**
  *   Crash Recovery tests for durable subscription
