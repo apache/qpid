cbe6690289988a6e5fda4d562352394e9b1a72c7
Alan Conway <aconway@apache.org>
2007-11-30 17:11:47 +0000

    Python examples
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@599876 13f79535-47bb-0310-9956-ffa450edef68

---
+++ qpid/python/examples/request-response/server.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+"""
+ server.py
+
+ Server for a client/server example
+"""
+
+import qpid
+from qpid.client import Client
+from qpid.content import Content
+from qpid.queue import Empty
+
+#----- Functions -------------------------------------------
+
+def respond(session, request):
+
+    # The routing key for the response is the request's reply-to
+    # property.  The body for the response is the request's body,
+    # converted to upper case.
+    
+    response=Content(request.body.upper())
+    response["routing_key"] = request["reply_to"]["routing_key"]
+
+    session.message_transfer(destination=request["reply_to"]["exchange_name"], content=response)
+
+#----- Initialization --------------------------------------
+
+#  Set parameters for login
+
+host="127.0.0.1"
+port=5672
+amqp_spec="/usr/share/amqp/amqp.0-10-preview.xml"
+user="guest"
+password="guest"
+
+#  Create a client and log in to it.
+
+client = Client(host, port, qpid.spec.load(amqp_spec))
+client.start({"LOGIN": user, "PASSWORD": password})
+
+# Open Channel 1 so we can use it to manage our queue.
+
+session = client.session()
+session.session_open()  # keep the session object, we'll need the session id
+
+#----- Main Body -- ----------------------------------------
+
+# Create a request queue and subscribe to it
+
+session.queue_declare(queue="request", exclusive=True)
+session.queue_bind(exchange="amq.direct", queue="request", routing_key="request")
+
+dest = "request_destination"
+
+session.message_subscribe(queue="request", destination=dest)
+session.message_flow(dest, 0, 0xFFFFFFFF)
+session.message_flow(dest, 1, 0xFFFFFFFF)
+
+
+# Remind the user to start the client program
+
+print "Request server running - run your client now."
+print "(Times out after 100 seconds ...)"
+
+# Respond to each request
+
+queue = client.queue(dest)
+
+# If we get a message, send it back to the user (as indicated in the
+# ReplyTo property)
+
+while True:
+  try:
+    request = queue.get(timeout=100)
+    respond(session, request.content)
+    request.complete()
+  except Empty:
+    print "No more messages!"
+    break;
+
+
+#----- Cleanup ------------------------------------------------
+
+# Clean up before exiting so there are no open threads.
+#
+# Close Channel 1.
+
+session.session_close()
+
