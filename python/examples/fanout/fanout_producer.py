854f0fecc29299c3abbd9a95331acc63fa7ada62
Alan Conway <aconway@apache.org>
2007-11-30 17:11:47 +0000

    Python examples
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@599876 13f79535-47bb-0310-9956-ffa450edef68

---
+++ python/examples/fanout/fanout_producer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+"""
+ direct_producer.py
+
+ Publishes messages to an AMQP direct exchange, using
+ the routing key "routing_key"
+"""
+
+import qpid
+from qpid.client import Client
+from qpid.content import Content
+from qpid.queue import Empty
+
+#----- Initialization -----------------------------------
+
+#  Set parameters for login
+
+host="127.0.0.1"
+port=5672
+amqp_spec="/usr/share/amqp/amqp.0-10-preview.xml"
+user="guest"
+password="guest"
+
+#  Create a client and log in to it.
+
+client = Client(host, port, qpid.spec.load(amqp_spec))
+client.start({"LOGIN": user, "PASSWORD": password})
+
+session = client.session()
+session.session_open()
+
+#----- Publish some messages ------------------------------
+
+# Create some messages and put them on the broker.
+
+for i in range(10):
+  message = Content(body="message " + str(i))
+  session.message_transfer(destination="amq.fanout", content=message)
+
+final="That's all, folks!"
+message=Content(final)
+session.message_transfer(destination="amq.fanout", content=message)
+
+#----- Cleanup --------------------------------------------
+
+# Clean up before exiting so there are no open threads.
+
+session.session_close()
+
