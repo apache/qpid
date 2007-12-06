1d1fdb7b96251a150955a7536821a2da71efa964
Arnaud Simon <arnaudsimon@apache.org>
2007-12-06 18:18:27 +0000

    run script for compiling and running the samples
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@601800 13f79535-47bb-0310-9956-ffa450edef68

---
+++ qpid/java/client/example/runSample.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Work out the CLASSPATH divider
+UNAME=`uname -s`
+case $UNAME in 
+    CYGWIN*)
+	DIVIDER=";"
+    ;;
+    *)
+	DIVIDER=":"
+;;
+esac
+
+if test "'x$QPID_HOME'" != "'x'"
+then
+    QPID_HOME=$QPID_HOME
+else
+    QPID_HOME="/usr/share/java/"
+fi
+echo "Using QPID_HOME: $QPID_HOME"
+
+if test "'x$QPID_SAMPLE'" != "'x'"
+then
+    QPID_SAMPLE=$QPID_SAMPLE
+else
+    QPID_SAMPLE="/usr/share/rhm-docs-0.2/java"
+fi
+echo "Using QPID_SAMPLE: $QPID_SAMPLE"
+
+
+# set the CLASSPATH
+CLASSPATH=`find "$QPID_HOME" -name '*.jar' | tr '\n' "$DIVIDER"`
+
+#create output dir
+mkdir $QPID_SAMPLE/classes
+
+# compile the samples 
+javac -cp  "$CLASSPATH" -sourcepath "$QPID_SAMPLE" -d "$QPID_SAMPLE/classes/" `find $QPID_SAMPLE -name '*.java'`
+
+# Add output classes to CLASSPATH
+CLASSPATH="$CLASSPATH$DIVIDER$QPID_SAMPLE/classes"
+
+
+# Check if the user supplied a sample classname
+if test "'x$1'" = "'x'"
+then
+    echo "No sample classname specified"
+    exit;
+else
+    java -cp $CLASSPATH $*
+fi
