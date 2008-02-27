b0c194956862fd21c45bdfa7c552d18f16932491
Rajith Muditha Attapattu <rajith@apache.org>
2008-02-27 05:17:07 +0000

    Added a new parser for the BindingURL. This allows adding multiple binding keys, using # and * in the URLs. This is tracked via QPID-814
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@631489 13f79535-47bb-0310-9956-ffa450edef68

--- java/common/src/main/java/org/apache/qpid/url/BindingURL.java
+++ java/common/src/main/java/org/apache/qpid/url/BindingURL.java
@@ -7,9 +7,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -34,6 +34,7 @@ public interface BindingURL
     public static final String OPTION_CLIENTID = "clientid";
     public static final String OPTION_SUBSCRIPTION = "subscription";
     public static final String OPTION_ROUTING_KEY = "routingkey";
+    public static final String OPTION_BINDING_KEY = "bindingKey";
 
 
     String getURL();
@@ -51,6 +52,8 @@ public interface BindingURL
     boolean containsOption(String key);
 
     AMQShortString getRoutingKey();
+
+    AMQShortString[] getBindingKeys();
 
     String toString();
 }
