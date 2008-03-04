c19ba3eb15620becdbc157005dcc5a465b3ac064
Rafael H. Schloming <rhs@apache.org>
2008-03-04 20:03:09 +0000

    import of in-process 0-10 final python client
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@633610 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/python/qpid/util.py
+++ qpid/python/qpid/util.py
@@ -1,5 +1,3 @@
-# Do not delete - marks this directory as a python package.
-
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
@@ -8,9 +6,9 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -19,11 +17,23 @@
 # under the License.
 #
 
-from codec import *
-from queue import *
-from spec import *
-from framer import *
-from assembler import *
-from datatypes import *
-from connection010 import *
-from spec010 import *
+import socket
+
+def connect(host, port):
+  sock = socket.socket()
+  sock.connect((host, port))
+  sock.setblocking(1)
+  # XXX: we could use this on read, but we'd have to put write in a
+  # loop as well
+  # sock.settimeout(1)
+  return sock
+
+def listen(host, port, predicate = lambda: True, bound = lambda: None):
+  sock = socket.socket()
+  sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  sock.bind((host, port))
+  bound()
+  sock.listen(5)
+  while predicate():
+    s, a = sock.accept()
+    yield s
