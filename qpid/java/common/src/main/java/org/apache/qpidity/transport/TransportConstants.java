505150f4ff07c7714411dd5b2160e32c643833b6
Rajith Muditha Attapattu <rajith@apache.org>
2008-03-13 16:04:46 +0000

    Added constant to represent the AMQP versions, as previously it was hard-coded. Modified the ConnectionDelegate to use the Constants for AMQP version. Also the version cosntants were changed to 99-0 to work with the c++ broker until the 0-10 framing gets completed.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@636791 13f79535-47bb-0310-9956-ffa450edef68

---
+++ qpid/java/common/src/main/java/org/apache/qpidity/transport/TransportConstants.java
@@ -0,0 +1,7 @@
+package org.apache.qpidity.transport;
+
+public class TransportConstants
+{
+    public static final byte CONNECTION_VERSION_MAJOR = 99;
+    public static final byte CONNECTION_VERSION_MINOR = 0;
+}
