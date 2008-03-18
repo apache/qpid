762565a83a480183fe84a832631e01931ed01994
Arnaud Simon <arnaudsimon@apache.org>
2008-03-18 12:20:49 +0000

    Revision 636791 says: "Added constant to represent the AMQP versions, as previously it was hard-coded." Those constants must be used when the connection is established.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@638344 13f79535-47bb-0310-9956-ffa450edef68

--- java/common/src/main/java/org/apache/qpidity/ToyClient.java
+++ java/common/src/main/java/org/apache/qpidity/ToyClient.java
@@ -75,7 +75,7 @@ class ToyClient extends SessionDelegate
                                                   }
                                                   public void closed() {}
                                               });
-        conn.send(new ConnectionEvent(0, new ProtocolHeader(1, 0, 10)));
+        conn.send(new ConnectionEvent(0, new ProtocolHeader(1, TransportConstants.CONNECTION_VERSION_MAJOR, TransportConstants.CONNECTION_VERSION_MINOR)));
 
         Channel ch = conn.getChannel(0);
         Session ssn = new Session();
