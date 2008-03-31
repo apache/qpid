3fe72e8401c92084965e888da9d63decad7de082
Aidan Skinner <aidan@apache.org>
2008-03-31 21:19:21 +0000

    Created prematurely, will recreate from release branch
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/tags/M2.1@643154 13f79535-47bb-0310-9956-ffa450edef68

--- RC1/java/perftests/etc/scripts/Reliability.sh
+++
@@ -1,1 +0,0 @@
- find . -regex '.*R-Qpid-0[1-2].*\.sh' -exec  {} -o results-Reliability/ --csv \; && for i in `seq 1 6` ; do find . -regex '.*R-Qpid-0[3-7].*\.sh' -exec  {} -o results-Reliability/ --csv  \; ; done
--- RC1/java/perftests/etc/scripts/Throughput.sh
+++
@@ -1,1 +0,0 @@
- find . -regex '.*T.BT-.*\.sh' -exec  {} -o 2110rc3-throughput/ --csv $* \; 
