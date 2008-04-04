f7204bd30d2efb2a7290e6f231beb3122fdaba7b
Kim van der Riet <kpvdr@apache.org>
2008-04-04 18:25:08 +0000

    Additional files for Ted Ross's checkin
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@644812 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cpp/managementgen/templates/Package.h
@@ -0,0 +1,41 @@
+/*MGEN:commentPrefix=//*/
+#ifndef _MANAGEMENT_PACKAGE_/*MGEN:Schema.PackageNameUpper*/_
+#define _MANAGEMENT_PACKAGE_/*MGEN:Schema.PackageNameUpper*/_
+
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+/*MGEN:Root.Disclaimer*/
+
+#include "qpid/management/ManagementAgent.h"
+
+namespace qpid {
+namespace management {
+
+class Package/*MGEN:Schema.PackageNameCap*/
+{
+  public:
+    Package/*MGEN:Schema.PackageNameCap*/ (ManagementAgent::shared_ptr agent);
+    ~Package/*MGEN:Schema.PackageNameCap*/ () {}
+};
+
+}}
+            
+
+#endif  /*!_MANAGEMENT_PACKAGE_/*MGEN:Schema.PackageNameUpper*/_*/
