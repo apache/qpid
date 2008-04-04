e4d5fa23c36700d085109c03a232eef3acaa1df1
Alan Conway <aconway@apache.org>
2008-04-04 21:00:40 +0000

    src/qpid/amqp_0_10/Exception.h  - base classes for 0-10 exceptions rubygen/0-10/exceptions.rb  - generated 0-10 exceptions
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@644917 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/rubygen/0-10/exceptions.rb
+++ qpid/cpp/rubygen/0-10/exceptions.rb
@@ -10,17 +10,37 @@ class GenExceptions < CppGen
     @dir="qpid/amqp_#{@amqp.version.bars}"
   end
 
+  def exceptions_for_enum(enum, base, ns, suffix="")
+    enum.choices.each { |c|
+      name=c.name.typename+suffix+"Exception"
+      genl
+      doxygen_comment { genl c.doc }
+      struct(name, "public #{base}") {
+        genl "#{name}(const std::string& msg=std::string())"
+        genl "    : #{base}(#{ns}::#{c.name.shout}, msg) {}"
+        protected
+        genl "std::string getPrefix() const { return \"#{name}\"; }"
+      }
+    }
+  end
+  
   def gen_exceptions()
     h_file("#{@dir}/exceptions") { 
-      include "qpid/Exception"
-      include "specification.h"
+      include "qpid/amqp_0_10/Exception"
       namespace("#{@ns}") { 
-        @amqp.class_("execution").domain("error-code").enum.choices.each { |c|
-          name=c.name.typename+"Exception"
-          struct(name, "public SessionException") {
-            genl "#{name}(const std::string& msg=std::string()) : SessionException(execution::#{c.name.shout}, msg) {}"
-          }
-        }
+        error_code = @amqp.class_("execution").domain("error-code").enum
+        exceptions_for_enum(error_code, "SessionAbortedException", "execution")
+        genl
+
+        detach_code = @amqp.class_("session").domain("detach-code").enum
+        exceptions_for_enum(detach_code, "SessionDetachedException", "session", "Detached")
+
+        genl
+        exceptions_for_enum(detach_code, "SessionExpiredException", "session", "Expired")
+        genl
+
+        close_code =  @amqp.class_("connection").domain("close-code").enum
+        exceptions_for_enum(close_code, "ConnectionException", "connection")
       }
     }
   end
