1193b93eac4b34e540c4995539b10c1d6b65ce02
Alan Conway <aconway@apache.org>
2008-04-21 17:34:49 +0000

    Fix compile error on rhel5.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@650210 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/tests/RangeSet.cpp
+++ cpp/src/tests/RangeSet.cpp
@@ -26,46 +26,46 @@ using namespace qpid;
 
 QPID_AUTO_TEST_SUITE(RangeSetTestSuite)
 
-typedef qpid::Range<int> Range;
-typedef qpid::RangeSet<int> RangeSet;
+typedef qpid::Range<int> TestRange;
+typedef qpid::RangeSet<int> TestRangeSet;
 
 QPID_AUTO_TEST_CASE(testEmptyRange) {
-    Range r;
+    TestRange r;
     BOOST_CHECK(r.empty());
     BOOST_CHECK(!r.contains(0));
     //    BOOST_CHECK(r.contiguous(0));
 }
 
 QPID_AUTO_TEST_CASE(testRangeSetAddPoint) {
-    RangeSet r;
+    TestRangeSet r;
     BOOST_CHECK(r.empty());
     r += 3;
     BOOST_CHECK_MESSAGE(r.contains(3), r);
-    BOOST_CHECK_MESSAGE(r.contains(Range(3,4)), r);
+    BOOST_CHECK_MESSAGE(r.contains(TestRange(3,4)), r);
     BOOST_CHECK(!r.empty());
     r += 5;
     BOOST_CHECK_MESSAGE(r.contains(5), r);        
-    BOOST_CHECK_MESSAGE(r.contains(Range(5,6)), r);        
-    BOOST_CHECK_MESSAGE(!r.contains(Range(3,6)), r);
+    BOOST_CHECK_MESSAGE(r.contains(TestRange(5,6)), r);        
+    BOOST_CHECK_MESSAGE(!r.contains(TestRange(3,6)), r);
     r += 4;
-    BOOST_CHECK_MESSAGE(r.contains(Range(3,6)), r);
+    BOOST_CHECK_MESSAGE(r.contains(TestRange(3,6)), r);
 }
 
 QPID_AUTO_TEST_CASE(testRangeSetAddRange) {
-    RangeSet r;
-    r += Range(0,3);
-    BOOST_CHECK(r.contains(Range(0,3)));
-    r += Range(4,6);
-    BOOST_CHECK_MESSAGE(r.contains(Range(4,6)), r);
+    TestRangeSet r;
+    r += TestRange(0,3);
+    BOOST_CHECK(r.contains(TestRange(0,3)));
+    r += TestRange(4,6);
+    BOOST_CHECK_MESSAGE(r.contains(TestRange(4,6)), r);
     r += 3;
-    BOOST_CHECK_MESSAGE(r.contains(Range(0,6)), r);
+    BOOST_CHECK_MESSAGE(r.contains(TestRange(0,6)), r);
     BOOST_CHECK(r.front() == 0);
     BOOST_CHECK(r.back() == 6);
 }
 
 QPID_AUTO_TEST_CASE(testRangeSetIterate) {
-    RangeSet r;
-    (((r += 1) += 10) += Range(4,7)) += 2;
+    TestRangeSet r;
+    (((r += 1) += 10) += TestRange(4,7)) += 2;
     BOOST_MESSAGE(r);
     std::vector<int> actual;
     std::copy(r.begin(), r.end(), std::back_inserter(actual));
@@ -74,44 +74,44 @@ QPID_AUTO_TEST_CASE(testRangeSetIterate) {
 }
 
 QPID_AUTO_TEST_CASE(testRangeSetRemove) {
-    BOOST_CHECK_EQUAL(RangeSet(0,5)-3, RangeSet(0,3)+Range(4,5));
-    BOOST_CHECK_EQUAL(RangeSet(1,5)-5, RangeSet(1,5));
-    BOOST_CHECK_EQUAL(RangeSet(1,5)-0, RangeSet(1,5));
+    BOOST_CHECK_EQUAL(TestRangeSet(0,5)-3, TestRangeSet(0,3)+TestRange(4,5));
+    BOOST_CHECK_EQUAL(TestRangeSet(1,5)-5, TestRangeSet(1,5));
+    BOOST_CHECK_EQUAL(TestRangeSet(1,5)-0, TestRangeSet(1,5));
 
-    RangeSet r(RangeSet(0,5)+Range(10,15)+Range(20,25));
+    TestRangeSet r(TestRangeSet(0,5)+TestRange(10,15)+TestRange(20,25));
 
-    BOOST_CHECK_EQUAL(r-Range(0,5), RangeSet(10,15)+Range(20,25));
-    BOOST_CHECK_EQUAL(r-Range(10,15), RangeSet(0,5)+Range(20,25));
-    BOOST_CHECK_EQUAL(r-Range(20,25), RangeSet(0,5)+Range(10,15));
+    BOOST_CHECK_EQUAL(r-TestRange(0,5), TestRangeSet(10,15)+TestRange(20,25));
+    BOOST_CHECK_EQUAL(r-TestRange(10,15), TestRangeSet(0,5)+TestRange(20,25));
+    BOOST_CHECK_EQUAL(r-TestRange(20,25), TestRangeSet(0,5)+TestRange(10,15));
 
-    BOOST_CHECK_EQUAL(r-Range(-5, 30), RangeSet());
+    BOOST_CHECK_EQUAL(r-TestRange(-5, 30), TestRangeSet());
 
-    BOOST_CHECK_EQUAL(r-Range(-5, 7), RangeSet(10,15)+Range(20,25));
-    BOOST_CHECK_EQUAL(r-Range(8,19), RangeSet(0,5)+Range(20,25));
-    BOOST_CHECK_EQUAL(r-Range(17,30), RangeSet(0,5)+Range(10,15));
+    BOOST_CHECK_EQUAL(r-TestRange(-5, 7), TestRangeSet(10,15)+TestRange(20,25));
+    BOOST_CHECK_EQUAL(r-TestRange(8,19), TestRangeSet(0,5)+TestRange(20,25));
+    BOOST_CHECK_EQUAL(r-TestRange(17,30), TestRangeSet(0,5)+TestRange(10,15));
 
-    BOOST_CHECK_EQUAL(r-Range(-5, 5), RangeSet(10,15)+Range(20,25));
-    BOOST_CHECK_EQUAL(r-Range(10,19), RangeSet(0,5)+Range(20,25));
-    BOOST_CHECK_EQUAL(r-Range(18,25), RangeSet(0,5)+Range(10,15));
+    BOOST_CHECK_EQUAL(r-TestRange(-5, 5), TestRangeSet(10,15)+TestRange(20,25));
+    BOOST_CHECK_EQUAL(r-TestRange(10,19), TestRangeSet(0,5)+TestRange(20,25));
+    BOOST_CHECK_EQUAL(r-TestRange(18,25), TestRangeSet(0,5)+TestRange(10,15));
 
-    BOOST_CHECK_EQUAL(r-Range(-3, 3), RangeSet(3,5)+Range(10,15)+Range(20,25));
-    BOOST_CHECK_EQUAL(r-Range(3, 7), RangeSet(0,2)+Range(10,15)+Range(20,25));
-    BOOST_CHECK_EQUAL(r-Range(3, 12), RangeSet(0,3)+Range(12,15)+Range(20,25));
-    BOOST_CHECK_EQUAL(r-Range(3, 22), RangeSet(12,15)+Range(22,25));
-    BOOST_CHECK_EQUAL(r-Range(12, 22), RangeSet(0,5)+Range(10,11)+Range(22,25));
+    BOOST_CHECK_EQUAL(r-TestRange(-3, 3), TestRangeSet(3,5)+TestRange(10,15)+TestRange(20,25));
+    BOOST_CHECK_EQUAL(r-TestRange(3, 7), TestRangeSet(0,2)+TestRange(10,15)+TestRange(20,25));
+    BOOST_CHECK_EQUAL(r-TestRange(3, 12), TestRangeSet(0,3)+TestRange(12,15)+TestRange(20,25));
+    BOOST_CHECK_EQUAL(r-TestRange(3, 22), TestRangeSet(12,15)+TestRange(22,25));
+    BOOST_CHECK_EQUAL(r-TestRange(12, 22), TestRangeSet(0,5)+TestRange(10,11)+TestRange(22,25));
 }
 
 QPID_AUTO_TEST_CASE(testRangeContaining) {
-    RangeSet r;
-    (((r += 1) += Range(3,5)) += 7);
-    BOOST_CHECK_EQUAL(r.rangeContaining(0), Range(0,0));
-    BOOST_CHECK_EQUAL(r.rangeContaining(1), Range(1,2));
-    BOOST_CHECK_EQUAL(r.rangeContaining(2), Range(2,2));
-    BOOST_CHECK_EQUAL(r.rangeContaining(3), Range(3,5));
-    BOOST_CHECK_EQUAL(r.rangeContaining(4), Range(3,5));
-    BOOST_CHECK_EQUAL(r.rangeContaining(5), Range(5,5));
-    BOOST_CHECK_EQUAL(r.rangeContaining(6), Range(6,6));
-    BOOST_CHECK_EQUAL(r.rangeContaining(7), Range(7,8));
+    TestRangeSet r;
+    (((r += 1) += TestRange(3,5)) += 7);
+    BOOST_CHECK_EQUAL(r.rangeContaining(0), TestRange(0,0));
+    BOOST_CHECK_EQUAL(r.rangeContaining(1), TestRange(1,2));
+    BOOST_CHECK_EQUAL(r.rangeContaining(2), TestRange(2,2));
+    BOOST_CHECK_EQUAL(r.rangeContaining(3), TestRange(3,5));
+    BOOST_CHECK_EQUAL(r.rangeContaining(4), TestRange(3,5));
+    BOOST_CHECK_EQUAL(r.rangeContaining(5), TestRange(5,5));
+    BOOST_CHECK_EQUAL(r.rangeContaining(6), TestRange(6,6));
+    BOOST_CHECK_EQUAL(r.rangeContaining(7), TestRange(7,8));
 }
 
 QPID_AUTO_TEST_SUITE_END()
