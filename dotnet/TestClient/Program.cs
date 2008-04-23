5ce68425b8967bab283b5151568242163c2e1856
Aidan Skinner <aidan@apache.org>
2008-04-23 23:56:38 +0000

    QPID-832 sync from M2.x
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@651113 13f79535-47bb-0310-9956-ffa450edef68

---
+++ dotnet/TestClient/Program.cs
@@ -0,0 +1,10 @@
+namespace TopicListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apache.Qpid.Integration.Tests.interop.TestClient.Main(args);
+        }
+    }
+}
