d91e5679173a3cefafb9b3b6f61fea52f408704c
Martin Ritchie <ritchiem@apache.org>
2008-05-09 12:40:54 +0000

    Moved all references to M2.1 to M2.1.x-SNAPSHOT
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/branches/M2.1.x@654785 13f79535-47bb-0310-9956-ffa450edef68

--- java/perftests/etc/scripts/Test-SwiftMQ.sh
+++ java/perftests/etc/scripts/Test-SwiftMQ.sh
@@ -27,4 +27,4 @@ for arg in "$@"; do
     fi
 done
 
-java -Xms256m -Dlog4j.configuration=perftests.log4j -Xmx1024m -Dbadger.level=warn -Damqj.test.logging.level=info -Damqj.logging.level=warn ${JAVA_OPTS} -cp "qpid-perftests-1.0-incubating-M2.1-SNAPSHOT.jar;swiftmqjars/swiftmq.jar" org.apache.qpid.junit.extensions.TKTestRunner -n Test-SwiftMQ -s[1] -r 1 -t testAsyncPingOk -o . org.apache.qpid.ping.PingAsyncTestPerf properties=swiftmq.properties factoryName=ConnectionFactory queueNamePostfix=@router1 overrideClientId=true ${ARGS}
+java -Xms256m -Dlog4j.configuration=perftests.log4j -Xmx1024m -Dbadger.level=warn -Damqj.test.logging.level=info -Damqj.logging.level=warn ${JAVA_OPTS} -cp "qpid-perftests-1.0-incubating-M2.1.x-SNAPSHOT.jar;swiftmqjars/swiftmq.jar" org.apache.qpid.junit.extensions.TKTestRunner -n Test-SwiftMQ -s[1] -r 1 -t testAsyncPingOk -o . org.apache.qpid.ping.PingAsyncTestPerf properties=swiftmq.properties factoryName=ConnectionFactory queueNamePostfix=@router1 overrideClientId=true ${ARGS}
