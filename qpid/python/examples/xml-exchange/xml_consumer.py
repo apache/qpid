4bed8993507c1a9e89a7cc04547faf409356e347
Alan Conway <aconway@apache.org>
2008-05-13 20:38:21 +0000

    From Jonathan Robie:
    https://issues.apache.org/jira/browse/QPID-1056: 
    
    Python examples for the xml exchange.
    
    https://issues.apache.org/jira/browse/QPID-1057
    
    Fixes to the XmlExchange.cpp that prevent it from crashing the broker
    when used with python clients that don't send application header
    properties
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@656005 13f79535-47bb-0310-9956-ffa450edef68

---
+++ qpid/python/examples/xml-exchange/xml_consumer.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+"""
+ direct_consumer.py
+
+ This AMQP client reads messages from a message
+ queue named "message_queue".
+"""
+
+import qpid
+import sys
+import os
+from random import randint
+from qpid.util import connect
+from qpid.connection import Connection
+from qpid.datatypes import Message, RangedSet, uuid4
+from qpid.queue import Empty
+
+
+#----- Initialization --------------------------------------
+
+#  Set parameters for login
+
+host="127.0.0.1"
+port=5672
+user="guest"
+password="guest"
+amqp_spec="/usr/share/amqp/amqp.0-10.xml"     
+
+# If an alternate host or port has been specified, use that instead
+# (this is used in our unit tests)
+#
+# If AMQP_SPEC is defined, use it to locate the spec file instead of
+# looking for it in the default location.
+
+if len(sys.argv) > 1 :
+  host=sys.argv[1]
+if len(sys.argv) > 2 :
+  port=int(sys.argv[2])
+
+try:
+     amqp_spec = os.environ["AMQP_SPEC"]
+except KeyError:
+     amqp_spec="/usr/share/amqp/amqp.0-10.xml"
+
+#  Create a connection.
+socket = connect(host, port)
+connection = Connection (sock=socket, spec=qpid.spec.load(amqp_spec))
+connection.start()
+session = connection.session(str(uuid4()))
+
+
+#----- Read from queue --------------------------------------------
+
+# Now let's create a local client queue and tell it to read
+# incoming messages.
+
+# The consumer tag identifies the client-side queue.
+
+local_queue_name = "local_queue"
+local_queue = session.incoming(local_queue_name)
+
+# Call message_consume() to tell the broker to deliver messages
+# from the AMQP queue to this local client queue. The broker will
+# start delivering messages as soon as message_consume() is called.
+
+session.message_subscribe(queue="message_queue", destination=local_queue_name)
+session.message_flow(local_queue_name,  session.credit_unit.message, 0xFFFFFFFF)
+session.message_flow(local_queue_name, session.credit_unit.byte, 0xFFFFFFFF)
+
+#  Initialize 'final' and 'content', variables used to identify the last message.
+
+message = None
+while True:
+   try:
+	message = local_queue.get(timeout=10)
+        session.message_accept(RangedSet(message.id))
+	content = message.body
+	print content
+   except Empty:
+        print "No more messages!"
+        break
+
+
+#----- Cleanup ------------------------------------------------
+
+# Clean up before exiting so there are no open threads.
+#
+
+session.close()
