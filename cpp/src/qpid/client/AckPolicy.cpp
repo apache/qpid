094ea0a23bee7f9ec3b8e239ae8945495e585022
Gordon Sim <gsim@apache.org>
2008-05-29 12:03:26 +0000

    Move AckPolicy impl from header to .cpp; ensure that completion is marked even when auto-acking is turned off.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@661309 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/client/AckPolicy.cpp
+++ cpp/src/qpid/client/AckPolicy.cpp
@@ -1,7 +1,5 @@
-#ifndef QPID_CLIENT_ACKPOLICY_H
-#define QPID_CLIENT_ACKPOLICY_H
-
 /*
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -9,9 +7,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * 
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -20,38 +18,33 @@
  * under the License.
  *
  */
-
-#include "qpid/framing/SequenceSet.h"
-#include "qpid/client/AsyncSession.h"
-#include "qpid/client/Message.h"
+#include "AckPolicy.h"
 
 namespace qpid {
 namespace client {
 
-/**
- * Policy for automatic acknowledgement of messages.
- *
- * \ingroup clientapi
- */
-class AckPolicy
-{
-    framing::SequenceSet accepted;
-    size_t interval;
-    size_t count;
+AckPolicy::AckPolicy(size_t n) : interval(n), count(n) {}
 
-  public:
-    /**
-     * Sends accepts and marks completion of received transfers.
-     * 
-     *@param n: acknowledge every n messages.
-     *n==0 means no automatic acknowledgement.
-     */
-    AckPolicy(size_t n=1);
-    void ack(const Message& msg, AsyncSession session);
-    void ackOutstanding(AsyncSession session);};
+void AckPolicy::ack(const Message& msg, AsyncSession session) 
+{
+    accepted.add(msg.getId());
+    if (interval && --count==0) {
+        session.markCompleted(msg.getId(), false, true);        
+        session.messageAccept(accepted);
+        accepted.clear();
+        count = interval;
+    } else {
+        session.markCompleted(msg.getId(), false, false);        
+    }
+}
+
+void AckPolicy::ackOutstanding(AsyncSession session) 
+{
+    if (!accepted.empty()) {
+        session.messageAccept(accepted);
+        accepted.clear();
+        session.sendCompletion();
+    }
+}
 
 }} // namespace qpid::client
-
-
-
-#endif  /*!QPID_CLIENT_ACKPOLICY_H*/
