3d57ef863d0a5974d3478972c5aa9c7b5670bd65
Martin Ritchie <ritchiem@apache.org>
2008-06-03 12:38:18 +0000

    Merged revisions 662770 via svnmerge from https://svn.apache.org/repos/asf/incubator/qpid/branches/M2.x
    ........
      r662770 | ritchiem | 2008-06-03 13:32:47 +0100 (Tue, 03 Jun 2008) | 3 lines
      
      QPID-1092 : Changed toString to be String.valueOf(getObject())
      Added MessageToStringTest, tests performing toString on Message before calling getObject().
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/branches/M2.1.x@662773 13f79535-47bb-0310-9956-ffa450edef68

--- java/client/src/main/java/org/apache/qpid/client/message/JMSObjectMessage.java
+++ java/client/src/main/java/org/apache/qpid/client/message/JMSObjectMessage.java
@@ -88,7 +88,7 @@ public class JMSObjectMessage extends AbstractJMSMessage implements ObjectMessag
 
     public String toBodyString() throws JMSException
     {
-        return toString(_data);
+        return String.valueOf(getObject());
     }
 
     public AMQShortString getMimeTypeAsShortString()
@@ -171,27 +171,5 @@ public class JMSObjectMessage extends AbstractJMSMessage implements ObjectMessag
         }
         catch (IOException ignore)
         { }
-    }
-
-    private static String toString(ByteBuffer data)
-    {
-        if (data == null)
-        {
-            return null;
-        }
-
-        int pos = data.position();
-        try
-        {
-            return data.getString(Charset.forName("UTF8").newDecoder());
-        }
-        catch (CharacterCodingException e)
-        {
-            return null;
-        }
-        finally
-        {
-            data.position(pos);
-        }
     }
 }
