3761e1aa8c6538e43a3a853ec965aa76283935bd
Martin Ritchie <ritchiem@apache.org>
2008-06-04 14:32:49 +0000

    QPID-1119 : M2x commit : Addition of a System property to AMQProtocolHandler.java to allow the syncWait default to be changed. To perform this a new SlowMessageStore has been added to the systest package. This allows all MessageStore methods to have a pre and/or post delay applied. This delay can be configured dynamically if you have a handle to the Store or via the XML configuration. The SlowMessageStore can also be used to wrap any existing MessageStore (Testing only carried out with the default MemoryMessageStore) To make testing easier on M2x VMTestCase has been modified to allow the test to simply configure logging levels and systemProperties. These are then reverted after the test has completed.
    These changes will naturally need more work to before they are merged to trunk which uses totally different methods for ClientProperties and for running tests.
    
    systests/pom.xml didn't have amqj.logging.level as a systemProperty hence setting it did nothing for the tests.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/branches/M2.x@663125 13f79535-47bb-0310-9956-ffa450edef68

--- java/client/src/main/java/org/apache/qpid/client/protocol/AMQProtocolHandler.java
+++ java/client/src/main/java/org/apache/qpid/client/protocol/AMQProtocolHandler.java
@@ -158,7 +158,7 @@ public class AMQProtocolHandler extends IoHandlerAdapter
     private FailoverException _lastFailoverException;
 
     /** Defines the default timeout to use for synchronous protocol commands. */
-    private final long DEFAULT_SYNC_TIMEOUT = 1000 * 30;
+    private final long DEFAULT_SYNC_TIMEOUT = Long.getLong("amqj.default_syncwrite_timeout", 1000 * 30);
 
     /** Default buffer size for pending messages reads */
     private static final String DEFAULT_READ_BUFFER_LIMIT = "262144";
