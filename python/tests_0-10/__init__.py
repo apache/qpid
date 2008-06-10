83ab3794444685c5f0e0f659da2ead70d396817d
Gordon Sim <gsim@apache.org>
2008-06-10 14:47:54 +0000

    Removed import of deleted test modules.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@666138 13f79535-47bb-0310-9956-ffa450edef68

--- python/tests_0-10/__init__.py
+++ python/tests_0-10/__init__.py
@@ -24,9 +24,7 @@ from broker import *
 from dtx import *
 from example import *
 from exchange import *
-from execution import *
 from message import *
 from query import *
 from queue import *
-from testlib import *
 from tx import *
