830943be4ed6ae90edd2e2655720c0dcc721171d
Alan Conway <aconway@apache.org>
2008-06-25 16:54:22 +0000

    - use flock to lock data dir rather than a lock file.  - removed troublesome global constructor in Mutex initialization.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@671604 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/sys/posix/Mutex.cpp
+++ cpp/src/qpid/sys/posix/Mutex.cpp
@@ -1,7 +1,5 @@
-#ifndef QPID_DATADIR_H
-#define QPID_DATADIR_H
-
 /*
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -9,9 +7,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * 
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -20,29 +18,29 @@
  * under the License.
  *
  */
-
-#include <string>
+#include "qpid/sys/Mutex.h"
 
 namespace qpid {
+namespace sys {
 
 /**
- * DataDir class.
+ * Initialise a recursive mutex attr for use in creating mutexes later
+ * (we use pthread_once to make sure it is initialised exactly once)
  */
-class DataDir
-{
-    const bool        enabled;
-    const std::string dirPath;
-    int dirFd;
 
-  public:
+namespace {
+pthread_once_t  onceControl = PTHREAD_ONCE_INIT;
+pthread_mutexattr_t mutexattr;
+	
+void initMutexattr()  {
+    pthread_mutexattr_init(&mutexattr);
+    pthread_mutexattr_settype(&mutexattr, PTHREAD_MUTEX_RECURSIVE);
+}
+}
 
-    DataDir (std::string path);
-    ~DataDir ();
+const pthread_mutexattr_t* Mutex::getAttribute() {
+    pthread_once(&onceControl, initMutexattr);
+    return &mutexattr;
+}
 
-    bool        isEnabled () { return enabled; }
-    std::string getPath   () { return dirPath; }
-};
- 
-} // namespace qpid
-
-#endif  /*!QPID_DATADIR_H*/
+}} // namespace qpid::sys
