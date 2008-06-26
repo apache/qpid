9d3e7a5194485568a1199a94a0faf10d558c3cc0
Arnaud Simon <arnaudsimon@apache.org>
2008-06-26 13:00:10 +0000

    QPID-1112: Changed addDeliveredMessage so to avoid division by 0 error when max pre-fetch=1
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@671887 13f79535-47bb-0310-9956-ffa450edef68

--- java/client/src/main/java/org/apache/qpid/client/AMQSession_0_10.java
+++ java/client/src/main/java/org/apache/qpid/client/AMQSession_0_10.java
@@ -758,10 +758,11 @@ public class AMQSession_0_10 extends AMQSession
         _txRangeSet.add((int) id);
         _txSize++;
         // this is a heuristic, we may want to have that configurable 
-        if( _connection.getMaxPrefetch() != 0 && _txSize % (_connection.getMaxPrefetch() / 2) == 0 )
+        if (_connection.getMaxPrefetch() == 1 ||
+                _connection.getMaxPrefetch() != 0 && _txSize % (_connection.getMaxPrefetch() / 2) == 0)
         {
-           // send completed so consumer credits don't dry up
-           getQpidSession().messageAcknowledge(_txRangeSet, false);
+            // send completed so consumer credits don't dry up
+            getQpidSession().messageAcknowledge(_txRangeSet, false);
         }
     }
 
