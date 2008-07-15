e62d0672d487bf7c7b766ecfd8704ff9fb884c0a
Alan Conway <aconway@apache.org>
2008-07-15 16:49:25 +0000

    Fix "ignoring return value" warning from LockFile.h.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk@676963 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/qpid/sys/posix/LockFile.h
+++ qpid/cpp/src/qpid/sys/posix/LockFile.h
@@ -43,7 +43,7 @@ public:
 
     ~LockFile() {
         if (fd >= 0) {
-            ::lockf(fd, F_ULOCK, 0);
+            (void) ::lockf(fd, F_ULOCK, 0); // Suppress warnings about ignoring return value.
             ::close(fd);
         }
     }
