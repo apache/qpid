f3c0cdf6f1d482127323eb6b505fe1ca0ed500f1
Martin Ritchie <ritchiem@apache.org>
2008-07-15 16:58:26 +0000

    QPID-1079 : Based on Code Review : Remvoed AutoCreateVMBroker code from QpidTestCase. Removed VMTestCase and all references to it, it was only used in JUnit4 testSuite wrappers. Rather than move QpidTestCase to a new module all client tests have that require this class have been moved to systests.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@676969 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/client/src/test/java/org/apache/qpid/test/unit/client/message/ObjectMessageTest.java
+++ qpid/java/client/src/test/java/org/apache/qpid/test/unit/client/message/ObjectMessageTest.java
@@ -25,7 +25,7 @@ import org.apache.qpid.client.AMQDestination;
 import org.apache.qpid.client.AMQQueue;
 import org.apache.qpid.client.AMQSession;
 import org.apache.qpid.testutil.VMBrokerSetup;
-import org.apache.qpid.testutil.QpidTestCase;
+import org.apache.qpid.test.utils.QpidTestCase;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
