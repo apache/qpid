640aaa76aac9497ebb7340cc6bb4f89264748794
Aidan Skinner <aidan@apache.org>
2008-07-29 16:20:06 +0000

    QPID-1203: Add 08ExcludeList for external Java broker and make the profile use that.
    Make AMQConnectionFactory take an optional clientid and use that if specified.
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk@680752 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/systests/src/main/java/org/apache/qpid/test/unit/topic/TopicSessionTest.java
+++ qpid/java/systests/src/main/java/org/apache/qpid/test/unit/topic/TopicSessionTest.java
@@ -138,13 +138,13 @@ public class TopicSessionTest extends QpidTestCase
 
     public void testUnsubscriptionAfterConnectionClose() throws Exception
     {
-        AMQConnection con1 = (AMQConnection) getConnection("guest", "guest");
+        AMQConnection con1 = (AMQConnection) getConnection("guest", "guest", "clientid");
         AMQTopic topic = new AMQTopic(con1, "MyTopic3");
 
         TopicSession session1 = con1.createTopicSession(false, AMQSession.AUTO_ACKNOWLEDGE);
         TopicPublisher publisher = session1.createPublisher(topic);
 
-        AMQConnection con2 = (AMQConnection) getConnection("guest", "guest");
+        AMQConnection con2 = (AMQConnection) getConnection("guest", "guest", "clientid");
         TopicSession session2 = con2.createTopicSession(false, AMQSession.AUTO_ACKNOWLEDGE);
         TopicSubscriber sub = session2.createDurableSubscriber(topic, "subscription0");
 
@@ -155,7 +155,7 @@ public class TopicSessionTest extends QpidTestCase
         assertNotNull(tm);
         con2.close();
         publisher.publish(session1.createTextMessage("Hello2"));
-        con2 =  (AMQConnection) getConnection("guest", "guest");
+        con2 =  (AMQConnection) getConnection("guest", "guest", "clientid");
         session2 = con2.createTopicSession(false, AMQSession.NO_ACKNOWLEDGE);
         sub = session2.createDurableSubscriber(topic, "subscription0");
         con2.start();
