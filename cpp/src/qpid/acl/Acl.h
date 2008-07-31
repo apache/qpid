66df1a35341eaef9eb705e6d6172d933ff9ede50
Carl C. Trieloff <cctrieloff@apache.org>
2008-07-31 20:45:44 +0000

    - Implementation of ACL plugin - Apply ACL to Exchange, Queue, Binding, Subscribe - Follow Java ACL types, few added
    To complete the implementation of ACL the following items are remaining.
    - ACL on message transfer
    - ACL on MGNT commands
    - Reading ACL from File, into auth map.
    
    
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk/qpid@681479 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cpp/src/qpid/acl/Acl.h
@@ -0,0 +1,75 @@
+#ifndef QPID_ACL_ACL_H
+#define QPID_ACL_ACL_H
+
+
+/*
+ *
+ * Copyright (c) 2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+
+#include "qpid/shared_ptr.h"
+#include "qpid/RefCounted.h"
+#include "qpid/broker/AclModule.h"
+#include <map>
+#include <string>
+
+
+namespace qpid {
+namespace broker {
+class Broker;
+}
+
+namespace acl {
+
+struct AclValues {
+    public:
+	bool noEnforce;
+    std::string aclFile;
+
+    AclValues() {noEnforce = false; aclFile = "policy.acl"; }
+};
+
+
+class Acl : public broker::AclModule, public RefCounted 
+{
+
+public:
+   Acl (AclValues& av, broker::Broker& b);
+
+   void initialize();
+   
+   virtual bool authorise(std::string id, acl::Action action, acl::ObjectType objType, std::string name, std::map<std::string, std::string>* params);
+   // create specilied authorise methods for cases that need faster matching as needed.
+
+   virtual ~Acl();
+private:
+   std::string printAction(acl::Action action);
+   std::string printObjType(acl::ObjectType objType);
+
+   acl::AclValues aclValues;
+   broker::Broker* broker;
+   
+   bool readAclFile();
+      
+};
+
+
+    
+}} // namespace qpid::acl
+
+#endif // QPID_ACL_ACL_H
