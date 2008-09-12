d0e559a1b6cf8399306c0960aeb5d207810d0806
Carl C. Trieloff <cctrieloff@apache.org>
2008-09-12 19:07:13 +0000

    Corrected warning for unsigned
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk@694776 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/tests/ClusterMapTest.cpp
+++ qpid/cpp/src/tests/ClusterMapTest.cpp
@@ -41,40 +41,40 @@ QPID_AUTO_TEST_CASE(testNotice) {
     BOOST_CHECK(!m.urlNotice(id(0), url("0-ready"))); // id(0) member, no dump.
     BOOST_CHECK(m.isMember(id(0)));
     BOOST_CHECK_EQUAL(m.dumps(id(0)), 0);
-    BOOST_CHECK_EQUAL(m.memberCount(), 1);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 0);
+    BOOST_CHECK_EQUAL(m.memberCount(), (unsigned)1);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)0);
 
     BOOST_CHECK_EQUAL(id(0), m.urlNotice(id(1), url("1-dump"))); // Newbie, needs dump
     BOOST_CHECK(m.isMember(id(0)));
     BOOST_CHECK(m.isDumpee(id(1)));
     BOOST_CHECK_EQUAL(m.dumps(id(0)), 1);
     BOOST_CHECK_EQUAL(m.dumps(id(1)), 0);
-    BOOST_CHECK_EQUAL(m.memberCount(), 1);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 1);
+    BOOST_CHECK_EQUAL(m.memberCount(), (unsigned)1);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)1);
 
     BOOST_CHECK(!m.urlNotice(id(1), url("1-ready"))); // id(1) is ready.
     BOOST_CHECK(m.isMember(id(0)));
     BOOST_CHECK(m.isMember(id(1)));
     BOOST_CHECK_EQUAL(m.dumps(id(0)), 0);
     BOOST_CHECK_EQUAL(m.dumps(id(1)), 0);
-    BOOST_CHECK_EQUAL(m.memberCount(), 2);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 0);
+    BOOST_CHECK_EQUAL(m.memberCount(), (unsigned)2);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)0);
 
     BOOST_CHECK_EQUAL(id(0), m.urlNotice(id(2), url("2-dump"))); // id(2) needs dump
     BOOST_CHECK(m.isDumpee(id(2)));
     BOOST_CHECK_EQUAL(m.dumps(id(0)), 1);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 1);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)1);
 
     BOOST_CHECK_EQUAL(id(1), m.urlNotice(id(3), url("3-dump"))); // 0 busy, dump to id(1).
     BOOST_CHECK(m.isDumpee(id(3)));    
     BOOST_CHECK_EQUAL(m.dumps(id(0)), 1);
     BOOST_CHECK_EQUAL(m.dumps(id(1)), 1);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 2);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)2);
 
     BOOST_CHECK_EQUAL(id(0), m.urlNotice(id(4), url("4-dump"))); // Equally busy, 0 is first on list.
     BOOST_CHECK_EQUAL(m.dumps(id(0)), 2);
     BOOST_CHECK_EQUAL(m.dumps(id(1)), 1);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 3);    
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)3);    
 
     // My dumpees both complete
     BOOST_CHECK(!m.urlNotice(id(2), url("2-ready"))); 
@@ -83,14 +83,14 @@ QPID_AUTO_TEST_CASE(testNotice) {
     BOOST_CHECK(m.isMember(id(4)));
     BOOST_CHECK_EQUAL(m.dumps(id(0)), 0);    
     BOOST_CHECK_EQUAL(m.dumps(id(1)), 1);    
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 1);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)1);
 
     // Final dumpee completes.
     BOOST_CHECK(!m.urlNotice(id(3), url("3-ready")));
     BOOST_CHECK(m.isMember(id(3)));
     BOOST_CHECK_EQUAL(m.dumps(id(0)), 0);    
     BOOST_CHECK_EQUAL(m.dumps(id(1)), 0);    
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 0);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)0);
 
 }
 
@@ -101,48 +101,48 @@ QPID_AUTO_TEST_CASE(testLeave) {
     BOOST_CHECK(!m.urlNotice(id(1), url("1-ready")));
     BOOST_CHECK_EQUAL(id(0), m.urlNotice(id(2), url("2-dump")));
     BOOST_CHECK(!m.urlNotice(id(2), url("2-ready")));
-    BOOST_CHECK_EQUAL(m.memberCount(), 3);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 0);
+    BOOST_CHECK_EQUAL(m.memberCount(), (unsigned)3);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)0);
     
     m.leave(id(1));
-    BOOST_CHECK_EQUAL(m.memberCount(), 2);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 0);
+    BOOST_CHECK_EQUAL(m.memberCount(), (unsigned)2);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)0);
     BOOST_CHECK(m.isMember(id(0)));
     BOOST_CHECK(m.isMember(id(2)));
 
     BOOST_CHECK_EQUAL(id(0), m.urlNotice(id(4), url("4-dump")));
     BOOST_CHECK_EQUAL(m.dumps(id(0)), 1);
     BOOST_CHECK(m.isDumpee(id(4)));
-    BOOST_CHECK_EQUAL(m.memberCount(), 2);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 1);
+    BOOST_CHECK_EQUAL(m.memberCount(), (unsigned)2);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)1);
 
     m.dumpFailed(id(4));        // Dumper detected a failure.
     BOOST_CHECK_EQUAL(m.dumps(id(0)), 0);
     BOOST_CHECK(!m.isDumpee(id(4)));
     BOOST_CHECK(!m.isMember(id(4)));
-    BOOST_CHECK_EQUAL(m.memberCount(), 2);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 0);
+    BOOST_CHECK_EQUAL(m.memberCount(), (unsigned)2);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)0);
 
     m.leave(id(4));             // Dumpee leaves, no-op since we already know it failed.
-    BOOST_CHECK_EQUAL(m.memberCount(), 2);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 0);
+    BOOST_CHECK_EQUAL(m.memberCount(), (unsigned)2);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)0);
 
     BOOST_CHECK_EQUAL(id(0), m.urlNotice(id(5), url("5-dump")));
     BOOST_CHECK_EQUAL(m.dumps(id(0)), 1);
     BOOST_CHECK(m.isDumpee(id(5)));
-    BOOST_CHECK_EQUAL(m.memberCount(), 2);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 1);
+    BOOST_CHECK_EQUAL(m.memberCount(), (unsigned)2);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)1);
 
     m.leave(id(5));             // Dumpee detects failure and leaves cluster.
     BOOST_CHECK_EQUAL(m.dumps(id(0)), 0);
     BOOST_CHECK(!m.isDumpee(id(5)));
     BOOST_CHECK(!m.isMember(id(5)));
-    BOOST_CHECK_EQUAL(m.memberCount(), 2);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 0);
+    BOOST_CHECK_EQUAL(m.memberCount(), (unsigned)2);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)0);
 
     m.dumpFailed(id(5));        // Dumper reports failure - no op, we already know.
-    BOOST_CHECK_EQUAL(m.memberCount(), 2);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 0);
+    BOOST_CHECK_EQUAL(m.memberCount(), (unsigned)2);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)0);
 }
 
 QPID_AUTO_TEST_CASE(testToControl) {
@@ -154,8 +154,8 @@ QPID_AUTO_TEST_CASE(testToControl) {
     m.urlNotice(id(3), url("3dump"));
     m.urlNotice(id(4), url("4dump"));
 
-    BOOST_CHECK_EQUAL(m.memberCount(), 2);
-    BOOST_CHECK_EQUAL(m.dumpeeCount(), 3);
+    BOOST_CHECK_EQUAL(m.memberCount(), (unsigned)2);
+    BOOST_CHECK_EQUAL(m.dumpeeCount(), (unsigned)3);
 
     ClusterMapBody b = m.toControl();
 
