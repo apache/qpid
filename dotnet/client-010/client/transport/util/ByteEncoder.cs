995ef780845afca4eda7996117cab49a7b7b9d6b
Arnaud Simon <arnaudsimon@apache.org>
2008-10-02 15:38:43 +0000

    qpid-1277: added unit tests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid@701160 13f79535-47bb-0310-9956-ffa450edef68

--- dotnet/client-010/client/transport/util/ByteEncoder.cs
+++ dotnet/client-010/client/transport/util/ByteEncoder.cs
@@ -1,6 +1,24 @@
+/*
+*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*
+*/
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace org.apache.qpid.transport.util
 {
@@ -18,10 +36,7 @@ namespace org.apache.qpid.transport.util
             {
                 return swapByteOrder(value);
             }
-            else
-            {
-                return value;
-            }
+            return value;
         }
 
         /// <summary>
@@ -35,10 +50,7 @@ namespace org.apache.qpid.transport.util
             {
                 return swapByteOrder(value);
             }
-            else
-            {
-                return value;
-            }
+            return value;
         }
 
         /// <summary>
@@ -52,10 +64,7 @@ namespace org.apache.qpid.transport.util
             {
                 return swapByteOrder(value);
             }
-            else
-            {
-                return value;
-            }
+            return value;
         }
 
         /// <summary>
@@ -69,10 +78,7 @@ namespace org.apache.qpid.transport.util
             {
                 return swapByteOrder(value);
             }
-            else
-            {
-                return value;
-            }
+            return value;
         }
 
         /// <summary>
@@ -86,10 +92,7 @@ namespace org.apache.qpid.transport.util
             {
                 return value;
             }
-            else
-            {
-                return swapByteOrder(value);
-            }
+            return swapByteOrder(value);
         }
 
         /// <summary>
@@ -103,10 +106,7 @@ namespace org.apache.qpid.transport.util
             {
                 return value;
             }
-            else
-            {
-                return swapByteOrder(value);
-            }
+            return swapByteOrder(value);
         }
 
         /// <summary>
@@ -120,10 +120,7 @@ namespace org.apache.qpid.transport.util
             {
                 return value;
             }
-            else
-            {
-                return swapByteOrder(value);
-            }
+            return swapByteOrder(value);
         }
 
         /// <summary>
@@ -131,16 +128,13 @@ namespace org.apache.qpid.transport.util
         /// </summary>
         /// <param name="value">Value to encode.</param>
         /// <returns>Little-endian encoded value.</returns>
-        public static Double GetLittleEndian(long value)
+        public static long GetLittleEndian(long value)
         {
             if (BitConverter.IsLittleEndian)
             {
                 return value;
             }
-            else
-            {
-                return swapByteOrder(value);
-            }
+            return swapByteOrder(value);
         }
 
         /// <summary>
