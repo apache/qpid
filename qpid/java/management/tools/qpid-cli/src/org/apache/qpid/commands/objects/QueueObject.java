025f124f0cb829de4cc5117b4762be3494e06bf4
Lahiru Gunathilake <lahiru@apache.org>
2008-10-07 09:07:34 +0000

    committing qpid-cli for the first time.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk@702402 13f79535-47bb-0310-9956-ffa450edef68

---
+++ qpid/java/management/tools/qpid-cli/src/org/apache/qpid/commands/objects/QueueObject.java
@@ -0,0 +1,86 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.apache.qpid.commands.objects;
+
+import javax.management.MBeanServerConnection;
+import javax.management.MBeanAttributeInfo;
+import javax.management.MBeanInfo;
+import javax.management.ObjectName;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lahiru
+ * Date: Jun 20, 2008
+ * Time: 8:49:56 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class QueueObject extends ObjectNames {
+    public QueueObject(MBeanServerConnection mbsc) {
+        ObjectNames(mbsc);
+    }
+
+    public void setQueryString(String object, String name, String vhost) {
+        if (name != null && vhost == null)
+            querystring = "org.apache.qpid:type=VirtualHost.Queue,name=" + name + ",*";
+        else if (name != null && vhost != null)
+            querystring = "org.apache.qpid:type=VirtualHost.Queue,VirtualHost=" + vhost + ",name=" + name + ",*";
+        else if (name == null && vhost != null)
+            querystring = "org.apache.qpid:type=VirtualHost.Queue,VirtualHost=" + vhost + ",*";
+        else
+            querystring = "org.apache.qpid:type=VirtualHost.Queue,*";
+    }
+
+    public int getmessagecount(ObjectName queue) {
+        int attr_count = 0;
+        String value;
+        Integer depth = null;
+
+        try {
+            MBeanInfo bean_info;
+            bean_info = mbsc.getMBeanInfo(queue);
+            MBeanAttributeInfo[] attr_info = bean_info.getAttributes();
+            if (attr_info == null)
+                return 0;
+            else {
+                for (MBeanAttributeInfo attr : attr_info) {
+                    Object toWrite = null;
+                    attr_count++;
+                    toWrite = mbsc.getAttribute(queue, attr.getName());
+                    if (attr_count == 7) {
+                        value = toWrite.toString();
+                        depth = new Integer(value);
+                    }
+                }
+
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        if (depth != null)
+            return depth.intValue();
+        else
+            return -1;
+    }
+
+
+}
