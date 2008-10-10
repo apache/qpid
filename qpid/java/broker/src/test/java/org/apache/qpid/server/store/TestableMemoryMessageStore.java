637a0b2ee76aeafd31e278ba0d259e34477fe748
Aidan Skinner <aidan@apache.org>
2008-10-10 14:41:08 +0000

    QPID-1314: Make sure all messags that are enqueued are dequeued.
    SimpleAMQQueue - dequeue messages if they are persistent, regardless of queue durability.
    SimpleAMQQueueTest - make sure that all messages which are stored are removed properly.
    TestableMemoryMessageStore - override enqueue/dequeue so it's possible to determine what is in the queue at any given point in time.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@703485 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/broker/src/test/java/org/apache/qpid/server/store/TestableMemoryMessageStore.java
+++ qpid/java/broker/src/test/java/org/apache/qpid/server/store/TestableMemoryMessageStore.java
@@ -20,12 +20,15 @@
  */
 package org.apache.qpid.server.store;
 
+import org.apache.qpid.AMQException;
+import org.apache.qpid.server.queue.AMQQueue;
 import org.apache.qpid.server.queue.MessageMetaData;
 import org.apache.qpid.framing.ContentBody;
 import org.apache.qpid.framing.abstraction.ContentChunk;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -35,6 +38,7 @@ public class TestableMemoryMessageStore extends MemoryMessageStore
 {
 
     MemoryMessageStore _mms = null;
+    private HashMap<Long, AMQQueue> _messages = new HashMap<Long, AMQQueue>();
 
     public TestableMemoryMessageStore(MemoryMessageStore mms)
     {
@@ -69,5 +73,20 @@ public class TestableMemoryMessageStore extends MemoryMessageStore
         {
             return _contentBodyMap;
         }
+    }
+    
+    public void enqueueMessage(StoreContext context, final AMQQueue queue, Long messageId) throws AMQException
+    {
+        getMessages().put(messageId, queue);
+    }
+
+    public void dequeueMessage(StoreContext context, final AMQQueue queue, Long messageId) throws AMQException
+    {
+        getMessages().remove(messageId);
+    }
+
+    public HashMap<Long, AMQQueue> getMessages()
+    {
+        return _messages;
     }
 }
