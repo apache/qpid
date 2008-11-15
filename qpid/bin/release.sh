b1095de662b66e8a59350a0ac1394390c85efaa8
Rafael H. Schloming <rhs@apache.org>
2008-11-15 13:58:23 +0000

    updated release sh script for M4
    git-svn-id: https://svn.apache.org/repos/asf/incubator/qpid/trunk@714261 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/bin/release.sh
+++ qpid/bin/release.sh
@@ -22,31 +22,47 @@
 # Script to pull together an Apache Release
 #
 
-svn delete https://svn.eu.apache.org/repos/asf/incubator/qpid/tags/M3
-svn copy https://svn.eu.apache.org/repos/asf/incubator/qpid/trunk  https://svn.eu.apache.org/repos/asf/incubator/qpid/tags/M3
-svn co https://svn.eu.apache.org/repos/asf/incubator/qpid/tags/M3 qpid-M3
-cd qpid-M3
-ln -s qpid/ qpid-incubating-M3
-tar -zhcf qpid-incubating-M3.tar.gz --exclude=.svn qpid-incubating-M3/
-rm qpid-incubating-M3
-tar -zxf qpid-incubating-M3.tar.gz
-tar -hzcf qpid-incubating-M3-ruby.tar.gz qpid-incubating-M3/ruby/ qpid-incubating-M3/specs/
-tar -zcf qpid-incubating-M3-python.tar.gz qpid-incubating-M3/python/ qpid-incubating-M3/specs/
-cd qpid-incubating-M3/cpp
+REV=$1
+VER=$2
+
+if [ -z "$REV" || -z "$VER" ]; then
+    echo "Usage: release.sh <revision> <version>"
+    exit 1
+fi
+
+set -xe
+
+svn export -r ${REV} https://svn.apache.org/repos/asf/incubator/qpid/trunk/qpid qpid-${VER}
+
+mkdir artifacts
+
+tar -czf artifacts/qpid-${VER}.tar.gz qpid-${VER}
+tar -czf artifacts/qpid-ruby-${VER}.tar.gz qpid-${VER}/ruby qpid-${VER}/specs
+tar -czf artifacts/qpid-python-${VER}.tar.gz qpid-${VER}/python qpid-${VER}/specs
+
+cd qpid-${VER}/cpp
 ./bootstrap
 ./configure
-make dist -j4
+make dist -j2
+
 cd ../java
 ant build release
+
 cd ../dotnet
 sh build-framing
 ./release mono-2.0
-cd ../../
-mkdir ../artifacts
-cp qpid-incubating-M3/java/release/*.tar.gz  ../artifacts
-cp *.tar.gz ../artifacts
-cp qpid-incubating-M3/cpp/*tar.gz ../artifacts/qpid-incubating-M3-cpp.tar.gz
-cp qpid-incubating-M3/dotnet/bin/mono-2.0/release/*.zip ../artifacts/qpid-incubating-M3-dotnet.zip
-cd ../artifacts
+
+cd client-010/gentool
+ant
+cd ..
+nant -t:mono-2.0 release-pkg
+
+cd ../../../
+cp qpid-${VER}/java/release/*.tar.gz  artifacts/qpid-java-${VER}.tar.gz
+cp qpid-${VER}/cpp/*.tar.gz artifacts/qpid-cpp-${VER}.tar.gz
+cp qpid-${VER}/dotnet/bin/mono-2.0/release/*.zip artifacts/qpid-dotnet-${VER}.zip
+cp qpid-${VER}/dotnet/client-010/bin/mono-2.0/debug/*.zip artifacts/qpid-dotnet-0-10-${VER}.zip
+
+cd artifacts
 sha1sum *.zip *.gz > SHA1SUM
 for i in `find . | egrep 'jar$|pom$|gz$|zip$|SHA1SUM'`; do gpg --sign --armor --detach $i; done;
