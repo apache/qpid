24b328bb7c0f137e66a4dfe6a2e322a10e897266
Aidan Skinner <aidan@apache.org>
2008-12-15 19:17:57 +0000

    QPID-1469: style fix
    patch from gemmellr@dcs.gla.ac.uk
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/qpid/trunk@726776 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/broker/src/test/java/org/apache/qpid/server/security/access/management/AMQUserManagementMBeanTest.java
+++ qpid/java/broker/src/test/java/org/apache/qpid/server/security/access/management/AMQUserManagementMBeanTest.java
@@ -30,124 +30,123 @@ import org.apache.qpid.server.security.auth.database.Base64MD5PasswordFilePrinci
 
 import junit.framework.TestCase;
 
-public class AMQUserManagementMBeanTest extends TestCase {
+public class AMQUserManagementMBeanTest extends TestCase
+{
+    private Base64MD5PasswordFilePrincipalDatabase _database;
+    private AMQUserManagementMBean _amqumMBean;
 
-	private Base64MD5PasswordFilePrincipalDatabase _database;
-	private AMQUserManagementMBean _amqumMBean;
+    private static final String _QPID_HOME =  System.getProperty("QPID_HOME");
 
-	private static final String _QPID_HOME =  System.getProperty("QPID_HOME");
-	
-	private static final String USERNAME = "testuser";
-	private static final String PASSWORD = "password";
-	private static final String JMXRIGHTS = "admin";
-	private static final String TEMP_PASSWORD_FILE_NAME = "tempPasswordFile.tmp";
-	private static final String TEMP_JMXACCESS_FILE_NAME = "tempJMXAccessFile.tmp";
+    private static final String USERNAME = "testuser";
+    private static final String PASSWORD = "password";
+    private static final String JMXRIGHTS = "admin";
+    private static final String TEMP_PASSWORD_FILE_NAME = "tempPasswordFile.tmp";
+    private static final String TEMP_JMXACCESS_FILE_NAME = "tempJMXAccessFile.tmp";
 
-	@Override
-	protected void setUp() throws Exception {
-
-		assertNotNull("QPID_HOME not set", _QPID_HOME);
-
-		_database = new Base64MD5PasswordFilePrincipalDatabase();
-		_amqumMBean = new AMQUserManagementMBean();
-	}
-	
-	@Override
-	protected void tearDown() throws Exception {
-		
-		File testFile = new File(_QPID_HOME + File.separator + TEMP_JMXACCESS_FILE_NAME + ".tmp");
-		if (testFile.exists())
-		{
-			testFile.delete();
-		}
-		
-		testFile = new File(_QPID_HOME + File.separator + TEMP_JMXACCESS_FILE_NAME + ".old");
-		if (testFile.exists())
-		{
-			testFile.delete();
-		}
-
-		testFile = new File(_QPID_HOME + File.separator + TEMP_PASSWORD_FILE_NAME + ".tmp");
-		if (testFile.exists())
-		{
-			testFile.delete();
-		}
-		
-		testFile = new File(_QPID_HOME + File.separator + TEMP_PASSWORD_FILE_NAME + ".old");
-		if (testFile.exists())
-		{
-			testFile.delete();
-		}
-	}
-
-	public void testDeleteUser() {
-		
-		loadTestPasswordFile();
-		loadTestAccessFile();
-		
-		boolean deleted = false;
-		
-		try{
-		    deleted = _amqumMBean.deleteUser(USERNAME);
-		}
-		catch(Exception e){
-			fail("Unable to delete user: " + e.getMessage());
-		}
-		
-		assertTrue(deleted);
-	}
-	
-	
-	
-	// ============================ Utility methods =========================
-	
-	private void loadTestPasswordFile()
+    @Override
+    protected void setUp() throws Exception
     {
-		try{
-			File tempPasswordFile = new File(_QPID_HOME + File.separator + TEMP_PASSWORD_FILE_NAME);
-			if (tempPasswordFile.exists())
-			{
-				tempPasswordFile.delete();
-			}
-			tempPasswordFile.deleteOnExit();
+        assertNotNull("QPID_HOME not set", _QPID_HOME);
 
-			BufferedWriter passwordWriter = new BufferedWriter(new FileWriter(tempPasswordFile));
-			passwordWriter.write(USERNAME + ":" + PASSWORD);
-			passwordWriter.newLine();
-			passwordWriter.flush();
-
-
-			_database.setPasswordFile(tempPasswordFile.toString());
-			_amqumMBean.setPrincipalDatabase(_database);
-		}
-		catch (IOException e)
-		{
-			fail("Unable to create test password file: " + e.getMessage());
-		}
+        _database = new Base64MD5PasswordFilePrincipalDatabase();
+        _amqumMBean = new AMQUserManagementMBean();
     }
 
-	private void loadTestAccessFile()
+    @Override
+    protected void tearDown() throws Exception
     {
-		try{
-			File tempAccessFile = new File(_QPID_HOME + File.separator + TEMP_JMXACCESS_FILE_NAME);
-			if (tempAccessFile.exists())
-			{
-				tempAccessFile.delete();
-			}
-			tempAccessFile.deleteOnExit();
+        File testFile = new File(_QPID_HOME + File.separator + TEMP_JMXACCESS_FILE_NAME + ".tmp");
+        if (testFile.exists())
+        {
+            testFile.delete();
+        }
 
-			BufferedWriter accessWriter = new BufferedWriter(new FileWriter(tempAccessFile));
-			accessWriter.write(USERNAME + "=" + JMXRIGHTS);
-			accessWriter.newLine();
-			accessWriter.flush();
+        testFile = new File(_QPID_HOME + File.separator + TEMP_JMXACCESS_FILE_NAME + ".old");
+        if (testFile.exists())
+        {
+            testFile.delete();
+        }
 
+        testFile = new File(_QPID_HOME + File.separator + TEMP_PASSWORD_FILE_NAME + ".tmp");
+        if (testFile.exists())
+        {
+            testFile.delete();
+        }
 
-			_amqumMBean.setAccessFile(tempAccessFile.toString());
-		}	
-		catch (Exception e)
-		{
-			fail("Unable to create test access file: " + e.getMessage());
-		}
+        testFile = new File(_QPID_HOME + File.separator + TEMP_PASSWORD_FILE_NAME + ".old");
+        if (testFile.exists())
+        {
+            testFile.delete();
+        }
     }
-	
+
+    public void testDeleteUser()
+    {
+        loadTestPasswordFile();
+        loadTestAccessFile();
+
+        boolean deleted = false;
+
+        try
+        {
+            deleted = _amqumMBean.deleteUser(USERNAME);
+        }
+        catch(Exception e){
+            fail("Unable to delete user: " + e.getMessage());
+        }
+
+        assertTrue(deleted);
+    }
+ 
+    
+    // ============================ Utility methods =========================
+
+    private void loadTestPasswordFile()
+    {
+        try
+        {
+            File tempPasswordFile = new File(_QPID_HOME + File.separator + TEMP_PASSWORD_FILE_NAME);
+            if (tempPasswordFile.exists())
+            {
+                tempPasswordFile.delete();
+            }
+            tempPasswordFile.deleteOnExit();
+
+            BufferedWriter passwordWriter = new BufferedWriter(new FileWriter(tempPasswordFile));
+            passwordWriter.write(USERNAME + ":" + PASSWORD);
+            passwordWriter.newLine();
+            passwordWriter.flush();
+
+            _database.setPasswordFile(tempPasswordFile.toString());
+            _amqumMBean.setPrincipalDatabase(_database);
+        }
+        catch (IOException e)
+        {
+            fail("Unable to create test password file: " + e.getMessage());
+        }
+    }
+
+    private void loadTestAccessFile()
+    {
+        try
+        {
+            File tempAccessFile = new File(_QPID_HOME + File.separator + TEMP_JMXACCESS_FILE_NAME);
+            if (tempAccessFile.exists())
+            {
+                tempAccessFile.delete();
+            }
+            tempAccessFile.deleteOnExit();
+
+            BufferedWriter accessWriter = new BufferedWriter(new FileWriter(tempAccessFile));
+            accessWriter.write(USERNAME + "=" + JMXRIGHTS);
+            accessWriter.newLine();
+            accessWriter.flush();
+
+            _amqumMBean.setAccessFile(tempAccessFile.toString());
+        }
+        catch (Exception e)
+        {
+            fail("Unable to create test access file: " + e.getMessage());
+        }
+    }
 }
