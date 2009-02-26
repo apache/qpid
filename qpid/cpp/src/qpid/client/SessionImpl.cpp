7b30a9adbf13432dd8d7bf0abe868ed773d2ad16
Gordon Sim <gsim@apache.org>
2009-02-26 19:57:13 +0000

    Send session.detached back to server in response to a detach.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@748286 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/qpid/client/SessionImpl.cpp
+++ qpid/cpp/src/qpid/client/SessionImpl.cpp
@@ -512,6 +512,7 @@ void SessionImpl::detach(const std::string& _name)
     if (id.getName() != _name) throw InternalErrorException("Incorrect session name");
     setState(DETACHED);
     QPID_LOG(info, "Session detached by peer: " << id);
+    proxy.detached(_name, DETACH_CODE_NORMAL);
 }
 
 void SessionImpl::detached(const std::string& _name, uint8_t _code) {
