ff77f0e6dac6ddc95e35184f0f332f74445a36e1
Manuel Teira Paz <mteira@apache.org>
2009-03-10 08:10:24 +0000

    examples/makedist.mk and examples/*/Makefile.am  - Avoid the usage of make ?= assignments (not portable).
    git-svn-id: https://svn.eu.apache.org/repos/asf/qpid/trunk@752014 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/examples/makedist.mk
+++ qpid/cpp/examples/makedist.mk
@@ -3,7 +3,8 @@ AM_CXXFLAGS = $(WARNING_CFLAGS)
 INCLUDES = -I$(top_srcdir)/src -I$(top_srcdir)/src/gen -I$(top_builddir)/src -I$(top_builddir)/src/gen
 CLIENT_LIB=$(top_builddir)/src/libqpidclient.la
 CONSOLE_LIB=$(top_builddir)/src/libqmfconsole.la
-MAKELDFLAG ?= qpidclient
+CLIENTFLAGS=-lqpidclient
+CONSOLEFLAGS=-lqmfconsole
 
 # Generate a simple non-automake Makefile for distribution.
 MAKEDIST=.libs/Makefile
@@ -12,7 +13,7 @@ $(MAKEDIST): Makefile
 	mkdir -p .libs
 	@$(ECHO) CXX=$(CXX)                     > $(MAKEDIST)
 	@$(ECHO) CXXFLAGS=$(CXXFLAGS)           >> $(MAKEDIST)
-	@$(ECHO) LDFLAGS=-l$(MAKELDFLAG)        >> $(MAKEDIST)
+	@$(ECHO) LDFLAGS=-l$(MAKELDFLAGS)        >> $(MAKEDIST)
 	@$(ECHO)                                >> $(MAKEDIST)
 	@$(ECHO) all: $(noinst_PROGRAMS)       >> $(MAKEDIST)
 	@$(ECHO)                                >> $(MAKEDIST)
