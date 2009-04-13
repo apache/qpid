f4721304dfb4a53d70f653622f28bbf1af3853ee
Martin Ritchie <ritchiem@apache.org>
2009-04-13 14:11:19 +0000

    QPID-1511: add scripts to generate example ssl stores, and add refrence in the relevant exception message Merged from branches/0.5-release : r757268
    merged from trunk r757270
    
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/branches/0.5-fix@764478 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/broker/src/main/java/org/apache/qpid/server/management/JMXManagedObjectRegistry.java
+++ qpid/java/broker/src/main/java/org/apache/qpid/server/management/JMXManagedObjectRegistry.java
@@ -155,7 +155,9 @@ public class JMXManagedObjectRegistry implements ManagedObjectRegistry
 
                 if (!ksf.exists())
                 {
-                    throw new FileNotFoundException("Cannot find JMX management SSL keystore file " + ksf);
+                    throw new FileNotFoundException("Cannot find JMX management SSL keystore file " + ksf + "\n"
+                                                  + "Check broker configuration, or see create-example-ssl-stores script"
+                                                  + "in the bin/ directory if you need to generate an example store.");
                 }
                 if (!ksf.canRead())
                 {
