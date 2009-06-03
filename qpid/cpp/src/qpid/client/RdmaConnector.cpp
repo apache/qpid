0e4541fb7ce30f66025aa3371ad6887066f3f80d
Andrew Stitcher <astitcher@apache.org>
2009-06-03 13:52:44 +0000

    Revert "QPID-1879 Removed some code obsoleted by this change"
    This reverts commit 60d66cae76b2bea90f076875e35cb69049fb2d3a.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@781376 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/qpid/client/RdmaConnector.cpp
+++ qpid/cpp/src/qpid/client/RdmaConnector.cpp
@@ -68,6 +68,7 @@ using boost::str;
 
     sys::Mutex pollingLock;
     bool polling;
+    bool joined;
 
     sys::ShutdownHandler* shutdownHandler;
     framing::InputHandler* input;
@@ -147,6 +148,7 @@ RdmaConnector::RdmaConnector(Poller::shared_ptr p,
       version(ver), 
       initiated(false),
       polling(false),
+      joined(true),
       shutdownHandler(0),
       aio(0),
       poller(p),
@@ -162,6 +164,7 @@ RdmaConnector::~RdmaConnector() {
 void RdmaConnector::connect(const std::string& host, int port){
     Mutex::ScopedLock l(pollingLock);
     assert(!polling);
+    assert(joined);
 
     // This stuff needs to abstracted out of here to a platform specific file
     ::addrinfo *res;
@@ -185,6 +188,7 @@ void RdmaConnector::connect(const std::string& host, int port){
     c->start(poller);
 
     polling = true;
+    joined = false;
 }
 
 // The following only gets run when connected
