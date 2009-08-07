5edb1d4c2b06a1582e2a3e1f1b7b2f6f1a0dd552
Michael Goulish <mgoulish@apache.org>
2009-08-07 07:38:20 +0000

    Gsim's temporary fix for replication bug.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@801903 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/broker/Broker.cpp
+++ cpp/src/qpid/broker/Broker.cpp
@@ -334,6 +334,7 @@ void Broker::shutdown() {
 Broker::~Broker() {
     shutdown();
     queueEvents.shutdown();
+    timer.stop(); // FIXME -- temporary fix for replication bug.
     finalize();                 // Finalize any plugins.
     if (config.auth)
         SaslAuthenticator::fini();
