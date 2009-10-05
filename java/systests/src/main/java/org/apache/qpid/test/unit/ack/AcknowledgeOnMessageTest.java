41628c7e879480eeb59cf29b235976e092cf1fee
Martin Ritchie <ritchiem@apache.org>
2009-10-05 15:02:24 +0000

    QPID-1816 : Updated AcknowledgeTests to cover all ack modes and the synchronous receive and asynchronous onMessage approaches. Also added tests that failover between acking. Currently these tests are setup to repoulate the broker between failover. However, in a clustered environment that would not be requried. Investigation is need on how best to setup failover tests in general so that clustered and non-clustered environments can be easily tested.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@821822 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/systests/src/main/java/org/apache/qpid/test/unit/ack/AcknowledgeOnMessageTest.java
@@ -0,0 +1,98 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+package org.apache.qpid.test.unit.ack;
+
+import org.apache.qpid.client.AMQDestination;
+import org.apache.qpid.client.AMQSession;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class AcknowledgeOnMessageTest extends AcknowledgeTest implements MessageListener
+{
+    private CountDownLatch _receviedAll;
+
+    @Override
+    public void setUp() throws Exception
+    {
+        super.setUp();
+        _receviedAll = new CountDownLatch(NUM_MESSAGES);
+    }
+
+    /**
+     * @param transacted
+     * @param mode
+     *
+     * @throws Exception
+     */
+    protected void testAcking(boolean transacted, int mode) throws Exception
+    {
+        init(transacted, mode);
+
+        _consumer.setMessageListener(this);
+
+        _connection.start();
+
+        if (!_receviedAll.await(DEFAULT_FAILOVER_TIME, TimeUnit.MILLISECONDS))
+        {
+            fail("failover did not complete");
+        }
+
+        _consumer.close();
+        _consumerSession.close();
+
+        assertEquals("Wrong number of messages on queue", 0,
+                     ((AMQSession) getConnection().createSession(false, Session.AUTO_ACKNOWLEDGE)).getQueueDepth((AMQDestination) _queue));
+    }
+
+    public void onMessage(Message message)
+    {
+        try
+        {
+            int count = NUM_MESSAGES - (int) _receviedAll.getCount();
+
+            assertEquals("Incorrect message received", count, message.getIntProperty(INDEX));
+
+            count++;
+            if (count < NUM_MESSAGES)
+            {
+                //Send the next message
+                _producer.send(createNextMessage(_consumerSession, count));
+            }
+
+            doAcknowlegement(message);
+
+            _receviedAll.countDown();
+        }
+        catch (Exception e)
+        {
+            fail(e);
+        }
+    }
+
+    protected void fail(Exception e)
+    {
+
+    }
+}
