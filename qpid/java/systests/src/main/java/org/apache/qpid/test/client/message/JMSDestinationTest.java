7ec3267d45da37c1997723a719965d2390b4bc6d
Martin Ritchie <ritchiem@apache.org>
2009-12-07 15:19:02 +0000

    QPID-1992 : Exclude message move test from CPP broker as it uses JMX.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@887950 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/systests/src/main/java/org/apache/qpid/test/client/message/JMSDestinationTest.java
+++ qpid/java/systests/src/main/java/org/apache/qpid/test/client/message/JMSDestinationTest.java
@@ -29,10 +29,10 @@ import javax.jms.Connection;
 import javax.jms.Destination;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.Topic;
-import javax.jms.MessageListener;
 import javax.management.openmbean.CompositeDataSupport;
 import javax.management.openmbean.TabularData;
 import java.util.Iterator;
@@ -50,7 +50,6 @@ public class JMSDestinationTest extends QpidTestCase implements MessageListener
 
     private Connection _connection;
     private Session _session;
-    private JMXTestUtils _jmxUtils;
 
     private static final String USER = "admin";
     private CountDownLatch _receiveMessage;
@@ -58,23 +57,11 @@ public class JMSDestinationTest extends QpidTestCase implements MessageListener
 
     public void setUp() throws Exception
     {
-        _jmxUtils = new JMXTestUtils(this, USER, USER);
-        _jmxUtils.setUp();
         super.setUp();
-
-        // Open the JMX Connection
-        _jmxUtils.open();
 
         _connection = getConnection();
 
         _session = _connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-    }
-
-    public void tearDown() throws Exception
-    {
-        _jmxUtils.close();
-
-        super.tearDown();
     }
 
     /**
@@ -145,55 +132,69 @@ public class JMSDestinationTest extends QpidTestCase implements MessageListener
      */
     public void testMovedToQueue() throws Exception
     {
+        // Setup JMXUtils
+        JMXTestUtils jmxUtils = new JMXTestUtils(this, USER, USER);
+        jmxUtils.setUp();
+        // Open the JMX Connection
+        jmxUtils.open();
+        try
+        {
 
-        Queue queue = _session.createQueue(getTestQueueName());
+            Queue queue = _session.createQueue(getTestQueueName());
 
-        _session.createConsumer(queue).close();
+            _session.createConsumer(queue).close();
 
-        sendMessage(_session, queue, 1);
+            sendMessage(_session, queue, 1);
 
-        Topic topic = _session.createTopic(getTestQueueName() + "Topic");
+            Topic topic = _session.createTopic(getTestQueueName() + "Topic");
 
-        MessageConsumer consumer = _session.createConsumer(topic);
+            MessageConsumer consumer = _session.createConsumer(topic);
 
-        // Use Management to move message.
+            // Use Management to move message.
 
-        ManagedQueue managedQueue = _jmxUtils.
-                getManagedObject(ManagedQueue.class,
-                                 _jmxUtils.getQueueObjectName(getConnectionFactory().getVirtualPath().substring(1),
-                                                              getTestQueueName()));
+            ManagedQueue managedQueue = jmxUtils.
+                    getManagedObject(ManagedQueue.class,
+                                     jmxUtils.getQueueObjectName(getConnectionFactory().getVirtualPath().substring(1),
+                                                                  getTestQueueName()));
 
-        // Find the first message on the queue
-        TabularData data = managedQueue.viewMessages(1L, 2L);
+            // Find the first message on the queue
+            TabularData data = managedQueue.viewMessages(1L, 2L);
 
-        Iterator values = data.values().iterator();
-        assertTrue("No Messages found via JMX", values.hasNext());
+            Iterator values = data.values().iterator();
+            assertTrue("No Messages found via JMX", values.hasNext());
 
-        // Get its message ID
-        Long msgID = (Long) ((CompositeDataSupport) values.next()).get("AMQ MessageId");
+            // Get its message ID
+            Long msgID = (Long) ((CompositeDataSupport) values.next()).get("AMQ MessageId");
 
-        // Start the connection and consume message that has been moved to the
-        // queue
-        _connection.start();
+            // Start the connection and consume message that has been moved to the
+            // queue
+            _connection.start();
 
-        Message message = consumer.receive(1000);
+            Message message = consumer.receive(1000);
 
-        //Validate we don't have a message on the queue before we start
-        assertNull("Message should be null", message);
+            //Validate we don't have a message on the queue before we start
+            assertNull("Message should be null", message);
 
-        // Move it to from the topic to the queue
-        managedQueue.moveMessages(msgID, msgID, ((AMQTopic) topic).getQueueName());
+            // Move it to from the topic to the queue
+            managedQueue.moveMessages(msgID, msgID, ((AMQTopic) topic).getQueueName());
 
-        // Retrieve the newly moved message
-        message = consumer.receive(1000);
+            // Retrieve the newly moved message
+            message = consumer.receive(1000);
 
-        assertNotNull("Message should not be null", message);
+            assertNotNull("Message should not be null", message);
 
-        Destination destination = message.getJMSDestination();
+            Destination destination = message.getJMSDestination();
 
-        assertNotNull("JMSDestination should not be null", destination);
+            assertNotNull("JMSDestination should not be null", destination);
 
-        assertEquals("Incorrect Destination type", queue.getClass(), destination.getClass());
+            assertEquals("Incorrect Destination type", queue.getClass(), destination.getClass());
+
+        }
+        finally
+        {
+            jmxUtils.close();
+        }
+
     }
 
     /**
@@ -217,8 +218,8 @@ public class JMSDestinationTest extends QpidTestCase implements MessageListener
         _receiveMessage = new CountDownLatch(1);
 
         consumer.setMessageListener(this);
-        
-        assertTrue("Timed out waiting for message to be received ",_receiveMessage.await(1, TimeUnit.SECONDS));
+
+        assertTrue("Timed out waiting for message to be received ", _receiveMessage.await(1, TimeUnit.SECONDS));
 
         assertNotNull("Message should not be null", _message);
 
