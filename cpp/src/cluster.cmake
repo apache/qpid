caaa1c00f62f11a1f046a7cf40583eedd2d873e3
Alan Conway <aconway@apache.org>
2009-12-10 18:40:37 +0000

    QPID-2236: cmake link error, missing boost.system library.
    Tested on boost-1.37.0-7.fc11.x86_64 which has the system library and
    boost-1.33.1-10.el5 which does not.
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@889358 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/cluster.cmake
+++ cpp/src/cluster.cmake
@@ -133,7 +133,7 @@ if (BUILD_CLUSTER)
       )
 
   add_library (cluster MODULE ${cluster_SOURCES})
-  target_link_libraries (cluster ${LIBCPG} ${CMAN_LIB} qpidbroker qpidclient ${Boost_FILESYSTEM_LIBRARY})
+  target_link_libraries (cluster ${LIBCPG} ${CMAN_LIB} qpidbroker qpidclient ${Boost_FILESYSTEM_LIBRARY} ${Boost_SYSTEM_LIBRARY})
   set_target_properties (cluster PROPERTIES PREFIX "")
   
   # Create a second shared library for linking with test executables,
