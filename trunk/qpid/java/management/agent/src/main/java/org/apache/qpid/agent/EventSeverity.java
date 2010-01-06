79a525703d1183533c5ffdae322dd24acf8e9358
Andrew Stitcher <astitcher@apache.org>
2010-01-06 22:35:52 +0000

    Removed erroneous branch copy
    git-svn-id: https://svn.apache.org/repos/asf/qpid/branches/0.6-release@896700 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/qpid/java/management/agent/src/main/java/org/apache/qpid/agent/EventSeverity.java
+++
@@ -1,6 +0,0 @@
-package org.apache.qpid.agent;
-
-public enum EventSeverity
-{
-    EMERGENCY, ALERT, CRIT, ERROR, WARN, NOTICE, INFO, DEBUG
-}
--- trunk/qpid/java/testkit/src/main/java/org/apache/qpid/testkit/ErrorHandler.java
+++
@@ -1,6 +0,0 @@
-package org.apache.qpid.testkit;
-
-public interface ErrorHandler {
-
-	public void handleError(String msg,Exception e);
-}
