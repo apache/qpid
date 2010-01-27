f13dceb97198486cd9bd9574babac6b39d05b1a6
Gordon Sim <gsim@apache.org>
2010-01-27 10:42:19 +0000

    QPID-664: change cancel() to close() for consistency
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@903599 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/messaging/ReceiverImpl.h
+++ cpp/src/qpid/messaging/ReceiverImpl.h
@@ -46,7 +46,7 @@ class ReceiverImpl : public virtual qpid::RefCounted
     virtual uint32_t getCapacity() = 0;
     virtual uint32_t available() = 0;
     virtual uint32_t pendingAck() = 0;
-    virtual void cancel() = 0;
+    virtual void close() = 0;
     virtual const std::string& getName() const = 0;
     virtual Session getSession() const = 0;
 };
