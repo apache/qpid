ebda1b1e894fb49caf3a1cd6a89c638fb0a4983c
Kenneth Anthony Giusti <kgiusti@apache.org>
2010-01-28 23:01:12 +0000

    QPID-2374: Null authenticator now checks for SSL if --encryption-required specified
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@904293 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/qpid/broker/SaslAuthenticator.cpp
+++ qpid/cpp/src/qpid/broker/SaslAuthenticator.cpp
@@ -148,8 +148,13 @@ void NullAuthenticator::getMechanisms(Array& mechanisms)
 void NullAuthenticator::start(const string& mechanism, const string& response)
 {
     if (encrypt) {
-        QPID_LOG(error, "Rejected un-encrypted connection.");
-        throw ConnectionForcedException("Connection must be encrypted.");
+        // encryption required - check to see if we are running over an
+        // encrypted SSL connection.
+        sasl_ssf_t external_ssf = (sasl_ssf_t) connection.getSSF();
+        if (external_ssf < 1) {   // < 1 == unencrypted
+            QPID_LOG(error, "Rejected un-encrypted connection.");
+            throw ConnectionForcedException("Connection must be encrypted.");
+        }
     }
     if (mechanism == "PLAIN") { // Old behavior
         if (response.size() > 0) {
