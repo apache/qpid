2574655d79a515c161ba09a3db725906572747fb
Stephen D. Huston <shuston@apache.org>
2010-02-02 15:12:59 +0000

    Add missing QPID_COMMON_EXTERN to pair-returning insert() - it's now called by qpidbroker.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@905669 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/include/qpid/framing/FieldTable.h
+++ cpp/include/qpid/framing/FieldTable.h
@@ -104,7 +104,7 @@ class FieldTable
     ValueMap::iterator end() { return values.end(); }
     ValueMap::iterator find(const std::string& s) { return values.find(s); }
 
-    std::pair <ValueMap::iterator, bool> insert(const ValueMap::value_type&);
+    QPID_COMMON_EXTERN std::pair <ValueMap::iterator, bool> insert(const ValueMap::value_type&);
     QPID_COMMON_EXTERN ValueMap::iterator insert(ValueMap::iterator, const ValueMap::value_type&);
     void clear() { values.clear(); }
 
