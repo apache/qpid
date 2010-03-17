3f3b196609ed3a656347fe8384b4e63c5c0bbf4c
Jonathan Robie <jonathan@apache.org>
2010-03-17 23:00:46 +0000

    Replaced ant-based build system with a simple Makefile. To build documents, you must install Apache Ant >= 0.95, the Docbook XSLT stylesheets, xsltproc, and xmllint.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@924545 13f79535-47bb-0310-9956-ffa450edef68

--- doc/book/build.sh
+++ doc/book/build.sh
@@ -4,7 +4,7 @@
 #
 #  Build a PDF from Docbook XML
 #
-#  This will be replaced by an ANT file soon.
+#  The Makefile is cleaner ....
 #
 ########################################################################
 
@@ -13,10 +13,10 @@ mkdir -p build
 mkdir -p pdf
 
 # Assemble all documents using XInclude
-xmllint --xinclude src/Book.xml >build/book.xml
+xmllint --xinclude src/Book.xml >build/qpid-book.xml
 
 # Create the .fo
-xsltproc /usr/share/sgml/docbook/xsl-stylesheets-1.75.2/fo/docbook.xsl build/book.xml >build/book.fo
+xsltproc /usr/share/sgml/docbook/xsl-stylesheets-1.75.2/fo/docbook.xsl build/qpid-book.xml >build/qpid-book.fo
 
 # Use Apache FOP to create the PDF
-fop build/book.fo pdf/qpid-book.pdf
+fop build/qpid-book.fo pdf/qpid-book.pdf
