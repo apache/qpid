2acb15145016076f9aa21c50c9f5fb478e9b3588
Jonathan Robie <jonathan@apache.org>
2010-04-15 20:04:00 +0000

    Added hello_world.cpp example for tutorial.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@934573 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cpp/examples/messaging/hello_world.cpp
@@ -0,0 +1,49 @@
+#include <qpid/messaging/Connection.h>
+#include <qpid/messaging/Message.h>
+#include <qpid/messaging/Receiver.h>
+#include <qpid/messaging/Sender.h>
+#include <qpid/messaging/Session.h>
+
+#include <iostream>
+#include <sstream>
+
+using namespace qpid::messaging;
+
+int main() {
+
+  Connection connection("localhost:5672");
+  try {
+    connection.open();
+    Session session = connection.createSession();
+
+    std::string queueName = "message_queue";
+
+    Sender sender = session.createSender(queueName);
+    
+    for (int i=0; i<5; i++) {
+      std::stringstream content;
+      content << "Message " << i;
+      std::cout << "Sending " << content.str() << std::endl;
+      sender.send(Message(content.str()));
+    }
+    sender.close();
+	
+    Receiver receiver = session.createReceiver(queueName);
+
+    Message message;
+    Duration timeout(1000); /* in milliseconds */
+    while (receiver.fetch(message, timeout)) {
+      std::cout << "Received " << message.getContent() << std::endl;
+      session.acknowledge();
+    }
+
+    receiver.close();
+
+    connection.close();
+    return 0;
+  } catch(const std::exception& error) {
+    std::cerr << error.what() << std::endl;
+    connection.close();
+    return 1;   
+  }
+}
