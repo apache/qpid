4b68c719190915825b9745505ea8da71014e6d15
Rajith Muditha Attapattu <rajith@apache.org>
2010-04-15 19:39:06 +0000

    This is related to QPID-2496 The changes include support the new addressing structure and most items on the list specified in the JIRA. The following is not included in the commit 1. Add subject as filter in JMS - for exchanges use it as binding key and for queues use it as a selector  - this needs to be thought through. Besides JMS already provides a way to handle this. 2. Implementation of DELETE option. Further testing needs to be done to figure out the impact.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@934563 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/client/src/main/java/org/apache/qpid/client/messaging/address/Link.java
@@ -0,0 +1,106 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+package org.apache.qpid.client.messaging.address;
+
+import org.apache.qpid.client.messaging.address.Node.QueueNode;
+
+public class Link
+{ 
+    public enum FilterType { SQL92, XQUERY, SUBJECT }
+    
+    protected String name;
+    protected String _filter;
+    protected FilterType _filterType = FilterType.SUBJECT;
+    protected boolean _isNoLocal;
+    protected boolean _isDurable;
+    protected int _capacity = 0;
+    protected Node node;
+    
+    public Node getNode()
+    {
+        return node;
+    }
+
+    public void setNode(Node node)
+    {
+        this.node = node;
+    }
+
+    public boolean isDurable()
+    {
+        return _isDurable;
+    }
+    
+    public void setDurable(boolean durable)
+    {
+        _isDurable = durable;
+    }
+     
+    public String getFilter()
+    {
+        return _filter;
+    }
+
+    public void setFilter(String filter)
+    {
+        this._filter = filter;
+    }
+
+    public FilterType getFilterType()
+    {
+        return _filterType;
+    }
+
+    public void setFilterType(FilterType type)
+    {
+        _filterType = type;
+    }
+
+    public boolean isNoLocal()
+    {
+        return _isNoLocal;
+    }
+
+    public void setNoLocal(boolean noLocal)
+    {
+        _isNoLocal = noLocal;
+    }
+
+    public int getCapacity()
+    {
+        return _capacity;
+    }
+
+    public void setCapacity(int capacity)
+    {
+        this._capacity = capacity;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
