67a1b77101944f891bd1d5c5681d64b56b9e92cf
Marnie McCormack <marnie@apache.org>
2010-04-23 09:37:54 +0000

    QPID-2433 Patch to handle spaces in windows paths from Emmanuel Bourg
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@937212 13f79535-47bb-0310-9956-ffa450edef68

--- java/broker/bin/qpid-server.bat
+++ java/broker/bin/qpid-server.bat
@@ -65,7 +65,7 @@ if "%AMQJ_LOGGING_LEVEL%" == "" set AMQJ_LOGGING_LEVEL=info
 
 REM Set the default system properties that we'll use now that they have
 REM all been initialised
-set SYSTEM_PROPS=-Damqj.logging.level=%AMQJ_LOGGING_LEVEL% -DQPID_HOME=%QPID_HOME% -DQPID_WORK=%QPID_WORK%
+set SYSTEM_PROPS=-Damqj.logging.level=%AMQJ_LOGGING_LEVEL% "-DQPID_HOME=%QPID_HOME%" "-DQPID_WORK=%QPID_WORK%"
 
 if "%EXTERNAL_CLASSPATH%" == "" set EXTERNAL_CLASSPATH=%CLASSPATH%
 
