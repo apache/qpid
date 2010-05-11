1273e0279bf7a2876714d60909893a2a5f9bc3bb
Martin Ritchie <ritchiem@apache.org>
2010-05-11 19:31:30 +0000

    QPID-2597 : Add scavenge code. Unit test for SQEL to follow.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/branches/0.5.x-dev@943240 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/broker/src/main/java/org/apache/qpid/server/queue/SimpleQueueEntryList.java
+++ qpid/java/broker/src/main/java/org/apache/qpid/server/queue/SimpleQueueEntryList.java
@@ -2,6 +2,8 @@ package org.apache.qpid.server.queue;
 
 import org.apache.qpid.server.store.StoreContext;
 
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 /*
@@ -43,6 +45,9 @@ public class SimpleQueueEntryList implements QueueEntryList
             AtomicReferenceFieldUpdater.newUpdater
             (QueueEntryImpl.class, QueueEntryImpl.class, "_next");
 
+    private AtomicLong _scavenges = new AtomicLong(0L);
+    private AtomicReference<Thread> _scavenger = new AtomicReference<Thread>(null);
+    private static final long SCAVENGE_COUNT = Integer.getInteger("qpid.queue.scavenge_count", 50);
 
 
 
@@ -69,6 +74,78 @@ public class SimpleQueueEntryList implements QueueEntryList
         }
     }
 
+    void scavenge()
+    {
+        _scavenges.incrementAndGet();
+        
+        if (_scavenges.get() < SCAVENGE_COUNT)
+        {
+            return;
+        }
+
+        try
+        {
+
+            if (_scavenger.compareAndSet(null, Thread.currentThread()))
+            {
+                // only delete the number of scavenges requested.
+                // This should keep things fair when we have multiple consumers
+                // using selectors that will be calling this.
+                // With multiple consumers this will also be called but
+                // advanceHead should take care of it in most instances.
+                // Often it will be the ExpiredMessageTask so will not
+                // affect throughput.
+                long deletesToPerform = _scavenges.getAndSet(0);
+
+
+                QueueEntryImpl root = _head;
+                QueueEntryImpl next = root.nextNode();
+
+                do
+                {
+
+                    while (next._next != null && next.isDeleted())
+                    {
+
+                        final QueueEntryImpl newhead = next.nextNode();
+                        if (newhead != null)
+                        {
+                            _nextUpdater.compareAndSet(root, next, newhead);
+                        }
+                        next = root.nextNode();
+                    }
+                    if (next._next != null)
+                    {
+                        if (!next.isDeleted())
+                        {
+                            root = next;
+                            next = root.nextNode();
+                            deletesToPerform--;
+                        }
+
+                        // Limit the number of scavenges performed by this
+                        // thread. For fairness.
+                        if (deletesToPerform == 0)
+                        {
+                            return;
+                        }
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+                while (next != null && next._next != null);
+            }
+        }
+        finally
+        {
+            _scavenger.compareAndSet(Thread.currentThread(), null);
+        }
+
+    }
 
     public AMQQueue getQueue()
     {
