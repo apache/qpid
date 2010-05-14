655bb569f6546580624c45e1651407d629a21ff3
Ted Ross <tross@apache.org>
2010-05-14 14:08:30 +0000

    QPID-2589 1) Added map-message examples 2) Added license text to example sources
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@944270 13f79535-47bb-0310-9956-ffa450edef68

---
+++ qpid/cpp/bindings/qpid/dotnet/examples/csharp.map.receiver/csharp.map.recevier.cs
@@ -0,0 +1,75 @@
+﻿/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using org.apache.qpid.messaging;
+
+namespace org.apache.qpid.messaging.examples
+{
+    class MapReceiver
+    {
+        static void Main(string[] args)
+        {
+//            string url = "amqp:tcp:localhost:5672";
+            string url = "10.16.18.254:5672";
+            if (args.Length > 0)
+                url = args[0];
+
+            //
+            // Create and open an AMQP connection to the broker URL
+            //
+            Connection connection = new Connection(url);
+            connection.open();
+
+            //
+            // Create a session and a receiver fir the direct exchange using the
+            // routing key "map_example".
+            //
+            Session session = connection.createSession();
+            Receiver receiver = session.createReceiver("amq.direct/map_example");
+
+            //
+            // Fetch the message from the broker (wait indefinitely by default)
+            //
+            Message message = receiver.fetch(new Duration(60000));
+
+            //
+            // Extract the structured content from the message.
+            //
+            Dictionary<string, object> content = new Dictionary<string, object>();
+            message.getContent(content);
+            Console.WriteLine("Received: {0}", content);
+
+            //
+            // Acknowledge the receipt of all received messages.
+            //
+            session.acknowledge();
+
+            //
+            // Close the receiver and the connection.
+            //
+            receiver.close();
+            connection.close();
+        }
+    }
+}
+
---
+++ qpid/cpp/bindings/qpid/dotnet/examples/csharp.map.sender/csharp.map.sender.cs
@@ -0,0 +1,87 @@
+﻿/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.apache.qpid.messaging;
+
+namespace org.apache.qpid.messaging.examples
+{
+    class MapSender
+    {
+        static void Main(string[] args)
+        {
+            string url = "amqp:tcp:localhost:5672";
+            if (args.Length > 0)
+                url = args[0];
+
+            //
+            // Create and open an AMQP connection to the broker URL
+            //
+            Connection connection = new Connection(url);
+            connection.open();
+
+            //
+            // Create a session and a sender to the direct exchange using the
+            // routing key "map_example".
+            //
+            Session session = connection.createSession();
+            Sender sender = session.createSender("amq.direct/map_example");
+
+            //
+            // Create structured content for the message.  This example builds a
+            // map of items including a nested map and a list of values.
+            //
+            Dictionary<string, object> content = new Dictionary<string, object>();
+            Dictionary<string, object> subMap = new Dictionary<string, object>();
+            List<object> colors = new List<object>();
+
+            content["id"] = 987654321;
+            content["name"] = "Widget";
+            content["percent"] = 0.99;
+
+            subMap["name"] = "Smith";
+            subMap["number"] = 354;
+
+            content["nested"] = subMap;
+
+            colors.Add("red");
+            colors.Add("green");
+            colors.Add("white");
+
+            content["colors"] = colors;
+
+            //
+            // Construct a message with the map content and send it synchronously
+            // via the sender.
+            //
+            Message message = new Message(content);
+            sender.send(message, true);
+
+            //
+            // Close the connection.
+            //
+            connection.close();
+        }
+    }
+}
