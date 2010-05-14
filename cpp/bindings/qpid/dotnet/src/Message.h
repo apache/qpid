db777f0e49a3d848d3227183aee4e2a074d7e26b
Ted Ross <tross@apache.org>
2010-05-14 14:01:35 +0000

    QPID-2589 - Patch from Chuck Rolke that exposes structured message content (map/list).
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@944263 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/bindings/qpid/dotnet/src/Message.h
+++ cpp/bindings/qpid/dotnet/src/Message.h
@@ -31,10 +31,18 @@ namespace apache {
 namespace qpid {
 namespace messaging {
 
+typedef System::Collections::Generic::Dictionary<
+            System::String^, 
+            System::Object^> 
+                VMap;
+
+typedef System::Collections::Generic::List<
+            System::Object^> 
+                VList;
+
     /// <summary>
     /// Message is a managed wrapper for a ::qpid::messaging::Message
     /// </summary>
-
 
     public ref class Message
     {
@@ -43,11 +51,57 @@ namespace messaging {
         // Kept object deletion code
         void Cleanup();
 
+        bool objIsMap (System::Object ^ op)
+        { 
+            return (*op).GetType() == pVMapType;
+        }
+
+        bool objIsList(System::Object ^ op)
+        { 
+            return (*op).GetType() == pVListType;
+        }
+
+        // The given object is a Dictionary.
+        // Add its elements to the qpid map.
+        void Encode(::qpid::types::Variant::Map & theMapp,
+                    VMap ^ theObjp);
+
+        // The given object is a List.
+        // Add its elements to the qpid list.
+        void Encode(::qpid::types::Variant::List & theListp,
+                    VList ^ theObjp);
+
+        // Returns a variant representing simple native type object.
+        // Not to be called for Map/List objects.
+        void EncodeObject(System::Object ^ theObjp,
+                          ::qpid::types::Variant & targetp);
+
+
+        void Decode(VMap ^ dict, ::qpid::types::Variant::Map & map);
+
+        void Decode(VList ^ vList, ::qpid::types::Variant::List & qpidList);
+
+
+        // map and list for type comparison
+        VMap  aVMap;
+        VList aVList;
+        System::Type ^ pVMapType;
+        System::Type ^ pVListType;
+
     public:
+        // Create from String
         Message(System::String ^ bytes);
+
+        // Create from object
+        Message(System::Object ^ obj);
+
+        // Create reference copy
         Message(::qpid::messaging::Message * msgp);
+
         ~Message();
         !Message();
+
+        // Copy constructor
         Message(const Message % rhs);
 
         // The kept object in the Messaging C++ DLL
@@ -88,5 +142,12 @@ namespace messaging {
         void setContent(System::String ^ content);
 
         System::String ^ getContent();
+
+        void getContent(System::Collections::Generic::Dictionary<
+                            System::String^, 
+                            System::Object^> ^ dict);
+
+        void getContent(System::Collections::Generic::List<
+                            System::Object^> ^);
     };
 }}}}
