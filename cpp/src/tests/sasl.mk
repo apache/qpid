248a87790b738e84c5afbcbe46705f998384434c
Alan Conway <aconway@apache.org>
2010-05-27 20:02:30 +0000

    Fix problems with cluster_authentication_soak test in VPATH build.
    - Fix SASL version testing logic in cluster_authentication_soak and SaslAuthenticator
    - Generate all SASL config in the build directory in sasl_test_setup.sh
    - Compile cluster_authentication_soak only if SASL is available.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@948968 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cpp/src/tests/sasl.mk
@@ -0,0 +1,31 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+# Test that are only relevant if SASL is enabled.
+if HAVE_SASL
+
+check_PROGRAMS+=cluster_authentication_soak
+cluster_authentication_soak_INCLUDES=$(PUBLIC_INCLUDES)
+cluster_authentication_soak_SOURCES=cluster_authentication_soak.cpp  ForkedBroker.h ForkedBroker.cpp
+cluster_authentication_soak_LDADD=$(lib_client) $(lib_broker)
+
+TESTS +=   run_cluster_authentication_test
+LONG_TESTS += run_cluster_authentication_soak
+
+endif # HAVE_SASL
---
+++ cpp/src/tests/sasl_test_setup.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+SASL_PW=/usr/sbin/saslpasswd2
+test -x $SASL_PW || { echo Skipping SASL test, saslpasswd2 not found; exit 0; }
+
+mkdir -p sasl_config
+
+# Create configuration file.
+cat > sasl_config/qpidd.conf <<EOF
+pwcheck_method: auxprop
+auxprop_plugin: sasldb
+sasldb_path: ./sasl_config/qpidd.sasldb
+sql_select: dummy select
+EOF
+
+# Populate temporary sasl db.
+SASLTEST_DB=./sasl_config/qpidd.sasldb
+rm -f $SASLTEST_DB
+echo zig | $SASL_PW -c -p -f $SASLTEST_DB -u QPID zig
+echo zag | $SASL_PW -c -p -f $SASLTEST_DB -u QPID zag
+
