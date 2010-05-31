94bfcab8072669ff6fa5bb8556539d0733a0fb53
Robert Gemmell <robbie@apache.org>
2010-05-31 16:07:44 +0000

    QPID-2585: Update extras OSGi plugin to work with latest interfaces
    Applied patch from Andrew Kennedy <andrew.international@gmail.com>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@949787 13f79535-47bb-0310-9956-ffa450edef68

--- java/broker-plugins/extras/src/test/java/org/apache/qpid/server/plugins/ExtrasTest.java
+++ java/broker-plugins/extras/src/test/java/org/apache/qpid/server/plugins/ExtrasTest.java
@@ -1,5 +1,4 @@
 /*
- *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -16,9 +15,7 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *
  */
-
 package org.apache.qpid.server.plugins;
 
 import junit.framework.TestCase;
@@ -31,11 +28,12 @@ import org.apache.qpid.server.util.TestApplicationRegistry;
 
 import java.util.Map;
 
-public class PluginTest extends TestCase
+public class ExtrasTest extends TestCase
 {
-
     private static final String TEST_EXCHANGE_CLASS = "org.apache.qpid.extras.exchanges.example.TestExchangeType";
+    
     private static final String PLUGIN_DIRECTORY = System.getProperty("example.plugin.target");
+    private static final String CACHE_DIRECTORY = System.getProperty("example.cache.target");
 
     IApplicationRegistry _registry;
 
@@ -43,10 +41,10 @@ public class PluginTest extends TestCase
     public void setUp() throws Exception
     {
         PropertiesConfiguration properties = new PropertiesConfiguration();
-
         properties.addProperty("plugin-directory", PLUGIN_DIRECTORY);
-
+        properties.addProperty("cache-directory", CACHE_DIRECTORY);
         ServerConfiguration config = new ServerConfiguration(properties);
+
         // This Test requires an application Registry
         ApplicationRegistry.initialise(new TestApplicationRegistry(config));
         _registry = ApplicationRegistry.getInstance();
@@ -58,23 +56,19 @@ public class PluginTest extends TestCase
         ApplicationRegistry.remove();
     }
 
-
     public void testLoadExchanges() throws Exception
     {
         PluginManager manager = _registry.getPluginManager();
         Map<String, ExchangeType<?>> exchanges = manager.getExchanges();
         assertNotNull("No exchanges found in " + PLUGIN_DIRECTORY, exchanges);
-        assertEquals("Wrong number of exchanges found in " + PLUGIN_DIRECTORY,
-                     2, exchanges.size());
-        assertNotNull("Wrong exchange found in " + PLUGIN_DIRECTORY,
-                      exchanges.get(TEST_EXCHANGE_CLASS));
-    }
-
+        assertEquals("Wrong number of exchanges found in " + PLUGIN_DIRECTORY, 2, exchanges.size());
+        assertNotNull("Wrong exchange found in " + PLUGIN_DIRECTORY, exchanges.get(TEST_EXCHANGE_CLASS));
+    } 
+    
     public void testNoExchanges() throws Exception
     {
-        PluginManager manager = new PluginManager("/path/to/nowhere");
+        PluginManager manager = new PluginManager("/path/to/nowhere", "/tmp");
         Map<String, ExchangeType<?>> exchanges = manager.getExchanges();
-        assertEquals("Exchanges found", 0, exchanges.size());
+        assertTrue("Exchanges found", exchanges.isEmpty());
     }
-
 }
