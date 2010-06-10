772c66f5e4562eb3508dfa32aeb2eb1bbeaee44e
Jonathan Robie <jonathan@apache.org>
2010-06-10 18:56:57 +0000

    Improved formatting.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@953420 13f79535-47bb-0310-9956-ffa450edef68

--- doc/book/build-book.sh
+++ doc/book/build-book.sh
@@ -11,16 +11,25 @@
 #
 ########################################################################
 
-rm -rf build
-mkdir -p build
-mkdir -p pdf
+rm -rf build/$1
+mkdir -p build/$1
+mkdir -p build/$1/html-single
+mkdir -p build/$1/html
+mkdir -p build/$1/pdf
+cp -r src/images build/$1/html-single
+cp -r src/images build/$1/html
 
+# Create single-page .html
+xsltproc --xinclude --stringparam  section.autolabel 1  --stringparam  callout.graphics 0  --stringparam  callout.unicode 0 /usr/share/sgml/docbook/xsl-stylesheets-1.75.2/html/docbook.xsl src/$1.xml >build/$1/html-single/$1.html
 
-# Create the .html
-xsltproc --stringparam  section.autolabel 1 --stringparam generate.section.toc.level 0  --stringparam generate.chapter.toc 0 --stringparam section.label.includes.component.label 1 /usr/share/sgml/docbook/xsl-stylesheets-1.75.2/html/docbook.xsl src/$1.xml >build/$1.html
+# Create chunked .html
+INFILE=$(readlink -f src/$1.xml)
+pushd build/$1/html
+xsltproc --xinclude --stringparam  chunk.section.depth 1  --stringparam  section.autolabel 1  --stringparam  callout.graphics 0  --stringparam  callout.unicode 0 /usr/share/sgml/docbook/xsl-stylesheets-1.75.2/html/chunk.xsl $INFILE
+popd
 
 # Create the .fo
-xsltproc --stringparam  section.autolabel 1  --stringparam section.label.includes.component.label 1 /usr/share/sgml/docbook/xsl-stylesheets-1.75.2/fo/docbook.xsl src/$1.xml >build/$1.fo
+xsltproc --xinclude --stringparam  section.autolabel 1  --stringparam  callout.graphics 0  --stringparam  callout.unicode 0 /usr/share/sgml/docbook/xsl-stylesheets-1.75.2/fo/docbook.xsl src/$1.xml >build/$1/pdf/$1.fo
 
 # Use Apache FOP to create the PDF
-fop build/$1.fo pdf/$1.pdf
+fop build/$1/pdf/$1.fo build/$1/pdf/$1.pdf
