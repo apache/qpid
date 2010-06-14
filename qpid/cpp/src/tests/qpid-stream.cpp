fce2eb5dbe35cdbf44e634a4dcc329540461b96b
Alan Conway <aconway@apache.org>
2010-06-14 13:44:00 +0000

    Rename tests qpid_* to qpid-* for consistency.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@954471 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/tests/qpid-stream.cpp
+++ qpid/cpp/src/tests/qpid-stream.cpp
@@ -7,9 +7,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -37,7 +37,7 @@ using namespace qpid::types;
 namespace qpid {
 namespace tests {
 
-struct Args : public qpid::Options 
+struct Args : public qpid::Options
 {
     std::string url;
     std::string address;
@@ -48,7 +48,7 @@ struct Args : public qpid::Options
     uint senderCapacity;
     uint ackFrequency;
 
-    Args() : 
+    Args() :
         url("amqp:tcp:127.0.0.1:5672"),
         address("test-queue"),
         size(512),
@@ -160,9 +160,9 @@ struct Consume : Client
             totalLatency += latency;
 
             if (received % opts.rate == 0) {
-                std::cout << "count=" << received 
-                          << ", avg=" << (totalLatency/received) 
-                          << ", min=" << minLatency 
+                std::cout << "count=" << received
+                          << ", avg=" << (totalLatency/received)
+                          << ", min=" << minLatency
                           << ", max=" << maxLatency << std::endl;
             }
         }
