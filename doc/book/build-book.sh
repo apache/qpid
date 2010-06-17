38301cf57474bae7c0bd60bd6e259fe2ac9e2a26
Jonathan Robie <jonathan@apache.org>
2010-06-17 18:51:23 +0000

    Fixed numbering for sections - now includes chapter.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@955706 13f79535-47bb-0310-9956-ffa450edef68

--- doc/book/build-book.sh
+++ doc/book/build-book.sh
@@ -20,16 +20,16 @@ cp -r src/images build/$1/html-single
 cp -r src/images build/$1/html
 
 # Create single-page .html
-xsltproc --xinclude --stringparam  section.autolabel 1  --stringparam  callout.graphics 0  --stringparam  callout.unicode 0 /usr/share/sgml/docbook/xsl-stylesheets-1.75.2/html/docbook.xsl src/$1.xml >build/$1/html-single/$1.html
+xsltproc --xinclude --stringparam  section.autolabel 1  --stringparam  callout.graphics 0  --stringparam  callout.unicode 0 --stringparam section.label.includes.component.label 1 /usr/share/sgml/docbook/xsl-stylesheets-1.75.2/html/docbook.xsl src/$1.xml >build/$1/html-single/$1.html
 
 # Create chunked .html
 INFILE=$(readlink -f src/$1.xml)
 pushd build/$1/html
-xsltproc --xinclude --stringparam  chunk.section.depth 1  --stringparam  section.autolabel 1  --stringparam  callout.graphics 0  --stringparam  callout.unicode 0 /usr/share/sgml/docbook/xsl-stylesheets-1.75.2/html/chunk.xsl $INFILE
+xsltproc --xinclude --stringparam  chunk.section.depth 1  --stringparam  section.autolabel 1  --stringparam  callout.graphics 0  --stringparam  callout.unicode 0 --stringparam section.label.includes.component.label 1 /usr/share/sgml/docbook/xsl-stylesheets-1.75.2/html/chunk.xsl $INFILE
 popd
 
 # Create the .fo
-xsltproc --xinclude --stringparam  section.autolabel 1  --stringparam  callout.graphics 0  --stringparam  callout.unicode 0 /usr/share/sgml/docbook/xsl-stylesheets-1.75.2/fo/docbook.xsl src/$1.xml >build/$1/pdf/$1.fo
+xsltproc --xinclude --stringparam  section.autolabel 1  --stringparam  callout.graphics 0  --stringparam  callout.unicode 0 --stringparam section.label.includes.component.label 1 /usr/share/sgml/docbook/xsl-stylesheets-1.75.2/fo/docbook.xsl src/$1.xml >build/$1/pdf/$1.fo
 
 # Use Apache FOP to create the PDF
 fop build/$1/pdf/$1.fo build/$1/pdf/$1.pdf
