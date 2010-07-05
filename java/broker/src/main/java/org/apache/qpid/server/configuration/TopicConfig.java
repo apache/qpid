4145f04675f48c3df16e3ac83ddc188ac3ec87cf
Martin Ritchie <ritchiem@apache.org>
2010-07-05 11:15:02 +0000

    QPID-2581 : Process new topic configuration
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@960544 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/broker/src/main/java/org/apache/qpid/server/configuration/TopicConfig.java
@@ -0,0 +1,72 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+package org.apache.qpid.server.configuration;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.qpid.server.configuration.plugins.ConfigurationPlugin;
+
+public class TopicConfig extends ConfigurationPlugin
+{
+    @Override
+    public String[] getElementsProcessed()
+    {
+        return new String[]{"name", "subscriptionName"};
+    }
+
+    public String getName()
+    {
+        // If we don't have a specific topic then this config is for all topics.
+        return getStringValue("name", "#");
+    }
+
+    public String getSubscriptionName()
+    {
+        return getStringValue("subscriptionName");
+    }
+
+    public void validateConfiguration() throws ConfigurationException
+    {
+        if (_configuration.isEmpty())
+        {
+            throw new ConfigurationException("Topic section cannot be empty.");
+        }
+
+        if (getStringValue("name") == null && getSubscriptionName() == null)
+        {
+            throw new ConfigurationException("Topic section must have a 'name' or 'subscriptionName' element.");
+        }
+
+        System.err.println("********* Created TC:"+this);
+    }
+
+
+    @Override
+    public String formatToString()
+    {
+        String response = "Topic:"+getName();
+        if (getSubscriptionName() != null)
+        {
+               response += ", SubscriptionName:"+getSubscriptionName();
+        }
+
+        return response;
+    }    
+}
