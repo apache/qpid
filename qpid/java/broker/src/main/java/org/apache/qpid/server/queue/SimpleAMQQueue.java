d2b3d69bd23792c0530ad04c2d3fc034e110e591
Robert Gemmell <robbie@apache.org>
2010-09-27 22:53:02 +0000

    QPID-2869: prevent entering the source queue as destination
    git-svn-id: https://svn.apache.org/repos/asf/qpid/branches/0.5.x-dev@1001947 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/broker/src/main/java/org/apache/qpid/server/queue/SimpleAMQQueue.java
+++ qpid/java/broker/src/main/java/org/apache/qpid/server/queue/SimpleAMQQueue.java
@@ -868,6 +868,10 @@ public class SimpleAMQQueue implements AMQQueue, Subscription.StateListener
         {
             throw new IllegalArgumentException("Queue '" + queueName + "' is not registered with the virtualhost.");
         }
+        else if (toQueue == this)
+        {
+            throw new IllegalArgumentException("The destination queue cant be the same as the source queue");
+        }
 
         MessageStore store = getVirtualHost().getMessageStore();
 
@@ -956,6 +960,10 @@ public class SimpleAMQQueue implements AMQQueue, Subscription.StateListener
         if (toQueue == null)
         {
             throw new IllegalArgumentException("Queue '" + queueName + "' is not registered with the virtualhost.");
+        }
+        else if (toQueue == this)
+        {
+            throw new IllegalArgumentException("The destination queue cant be the same as the source queue");
         }
         
         MessageStore store = getVirtualHost().getMessageStore();
