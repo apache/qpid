5138bec405f0623f67adaad50418a11279ec0cf4
Alan Conway <aconway@apache.org>
2010-09-27 19:41:33 +0000

    Scripts using rsync and ssh to assist cluster testing: - run a command on all hosts in a cluster. - copy files to all hosts in a cluster. - "make install" locally and copy install tree to cluster. - copy source tree to cluster and "make install" on each host.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1001888 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/tests/cluster.mk
+++ cpp/src/tests/cluster.mk
@@ -81,4 +81,7 @@ cluster_test_LDADD=$(lib_client) $(lib_broker) ../cluster.la -lboost_unit_test_f
 
 qpidtest_SCRIPTS += run_cluster_tests cluster_tests.py run_long_cluster_tests long_cluster_tests.py testlib.py cluster_tests.fail
 
+qpidtest_SCRIPTS += allhosts rsynchosts qpid-build-rinstall qpid-src-rinstall
+
+
 endif
