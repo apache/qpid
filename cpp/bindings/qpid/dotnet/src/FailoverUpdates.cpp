49dfef2977f7340803c4079d60b4d29223cbeafc
Charles E. Rolke <chug@apache.org>
2010-11-02 14:29:50 +0000

    QPID-2922 Qpid Cpp Messaging .NET Binding does not implement FailoverUpdate class
    This checkin provides the implemtation.
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1030061 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cpp/bindings/qpid/dotnet/src/FailoverUpdates.cpp
@@ -0,0 +1,94 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+#include <windows.h>
+#include <msclr\lock.h>
+#include <oletx2xa.h>
+#include <string>
+#include <limits>
+
+#include "qpid/messaging/FailoverUpdates.h"
+
+#include "Connection.h"
+#include "FailoverUpdates.h"
+#include "QpidException.h"
+
+namespace Org {
+namespace Apache {
+namespace Qpid {
+namespace Messaging {
+
+    /// <summary>
+    /// FailoverUpdates is a managed wrapper for a qpid::messaging::FailoverUpdates
+    /// </summary>
+
+    // constructors
+    //FailoverUpdates::FailoverUpdates(Connection ^ connection) :
+    //    failoverupdatesp(new ::qpid::messaging::FailoverUpdates(*(connection->NativeConnection)))
+    //{
+    //}
+
+    FailoverUpdates::FailoverUpdates(Connection ^ connection)
+    {
+        System::Exception           ^ newException = nullptr;
+
+        try 
+		{
+            failoverupdatesp = new ::qpid::messaging::FailoverUpdates(*(connection->NativeConnection));
+        } 
+        catch (const ::qpid::types::Exception & error) 
+		{
+            String ^ errmsg = gcnew String(error.what());
+            newException    = gcnew QpidException(errmsg);
+        }
+
+		if (newException != nullptr) 
+		{
+	        throw newException;
+		}
+
+        return;
+    }
+
+
+    // Destructor
+    FailoverUpdates::~FailoverUpdates()
+    {
+        Cleanup();
+    }
+
+
+    // Finalizer
+    FailoverUpdates::!FailoverUpdates()
+    {
+        Cleanup();
+    }
+
+
+    // Destroys kept object
+    // TODO: add lock
+    void FailoverUpdates::Cleanup()
+    {
+        if (NULL != failoverupdatesp)
+        {
+            delete failoverupdatesp;
+            failoverupdatesp = NULL;
+        }
+    }
+}}}}
