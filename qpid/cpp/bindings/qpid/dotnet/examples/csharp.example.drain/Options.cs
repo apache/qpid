a9bcb605800611dc28c6131564f9cdcca98aaa98
Charles E. Rolke <chug@apache.org>
2010-11-10 19:45:23 +0000

    QPID-2939 Qpid .NET Messaging Binding has stray references and using statements
    Clean them up.
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1033679 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/bindings/qpid/dotnet/examples/csharp.example.drain/Options.cs
+++ qpid/cpp/bindings/qpid/dotnet/examples/csharp.example.drain/Options.cs
@@ -20,12 +20,6 @@
 namespace Org.Apache.Qpid.Messaging.Examples
 {
     using System;
-    using System.Collections;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.IO;
-    using System.Text;
-    using System.Xml;
 
     public class Options
     {
