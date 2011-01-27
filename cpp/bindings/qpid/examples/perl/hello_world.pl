4317c6c71cc1701ad85b2fdfa21caf4f39bbdf68
Ted Ross <tross@apache.org>
2011-01-27 17:04:40 +0000

    QPID-3009 - Perl binding to Qpid messaging Applied patch contributed by Hao Chang Yu
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1064199 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cpp/bindings/qpid/examples/perl/hello_world.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+use strict;
+use warnings;
+use Data::Dumper;
+
+use cqpid;
+
+my $broker            = ( @ARGV > 0 ) ? $ARGV[0] : "localhost:5672";
+my $address           = ( @ARGV > 1 ) ? $ARGV[0] : "amq.topic";
+my $connectionOptions = ( @ARGV > 2 ) ? $ARGV[1] : "";
+
+my $connection = new cqpid::Connection($broker, $connectionOptions);
+
+eval {
+    $connection->open();
+    my $session = $connection->createSession();
+
+    my $receiver = $session->createReceiver($address);
+    my $sender   = $session->createSender($address);
+
+    $sender->send(new cqpid::Message("Hello world!"));
+
+    #my $duration = new cqpid::Duration(1000);
+    #print ">>>" . $duration->getMilliseconds() . "\n";
+
+    my $message = $receiver->fetch($cqpid::Duration::SECOND);
+
+    #$message->setDurable(1);
+    #print "Durable: " . $message->getDurable() . "\n";
+    #print Dumper($message->getProperties());
+
+    print $message->getContent() . "\n";
+    $session->acknowledge();
+
+    $connection->close();
+};
+
+die $@ if ($@);
---
+++ cpp/bindings/qpid/examples/perl/map_sender.pl
@@ -0,0 +1,50 @@
+#! /usr/bin/perl5
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+use strict;
+use warnings;
+use Data::Dumper;
+
+use cqpid;
+
+my $url               = ( @ARGV > 0 ) ? $ARGV[0] : "amqp:tcp:127.0.0.1:5672";
+my $address           = ( @ARGV > 1 ) ? $ARGV[1] : "message_queue; {create: always}";
+my $connectionOptions = ( @ARGV > 2 ) ? $ARGV[2] : "";
+
+my $connection = new cqpid::Connection($url, $connectionOptions);
+
+eval {
+    $connection->open();
+
+    my $session = $connection->createSession();
+    my $sender  = $session->createSender($address);
+
+    my $message = new cqpid::Message();
+    my $content = { id   => 987654321, 
+                    name => "Widget", 
+                    percent => sprintf("%.2f", 0.99), 
+                    colours => [ qw (red green white) ], 
+                   };
+    cqpid::encode($content, $message);
+    $sender->send($message, 1);
+
+    $connection->close();
+};
+
+die $@ if ($@);
