9b4d656503d144131ad8de64fc7241e0785dc2b2
Robert Gemmell <robbie@apache.org>
2011-02-17 16:16:31 +0000

    QPID-3028: make assembler and disassembler use a static ThreadLocal for its BBEncoder and BBDecoder, release buffer from BBDecoder when complete
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1071674 13f79535-47bb-0310-9956-ffa450edef68

--- java/common/src/main/java/org/apache/qpid/transport/network/Assembler.java
+++ java/common/src/main/java/org/apache/qpid/transport/network/Assembler.java
@@ -28,7 +28,6 @@ import java.util.Map;
 import java.nio.ByteBuffer;
 
 import org.apache.qpid.transport.codec.BBDecoder;
-import org.apache.qpid.transport.codec.Decoder;
 
 import org.apache.qpid.transport.Header;
 import org.apache.qpid.transport.Method;
@@ -36,7 +35,6 @@ import org.apache.qpid.transport.ProtocolError;
 import org.apache.qpid.transport.ProtocolEvent;
 import org.apache.qpid.transport.ProtocolHeader;
 import org.apache.qpid.transport.Receiver;
-import org.apache.qpid.transport.SegmentType;
 import org.apache.qpid.transport.Struct;
 
 
@@ -51,7 +49,7 @@ public class Assembler implements Receiver<NetworkEvent>, NetworkDelegate
     private final Receiver<ProtocolEvent> receiver;
     private final Map<Integer,List<Frame>> segments;
     private final Method[] incomplete;
-    private final ThreadLocal<BBDecoder> decoder = new ThreadLocal<BBDecoder>()
+    private static final ThreadLocal<BBDecoder> _decoder = new ThreadLocal<BBDecoder>()
     {
         public BBDecoder initialValue()
         {
@@ -169,7 +167,7 @@ public class Assembler implements Receiver<NetworkEvent>, NetworkDelegate
 
     private void assemble(Frame frame, ByteBuffer segment)
     {
-        BBDecoder dec = decoder.get();
+        BBDecoder dec = _decoder.get();
         dec.init(segment);
 
         int channel = frame.getChannel();
@@ -201,7 +199,7 @@ public class Assembler implements Receiver<NetworkEvent>, NetworkDelegate
             break;
         case HEADER:
             command = incomplete[channel];
-            List<Struct> structs = new ArrayList(2);
+            List<Struct> structs = new ArrayList<Struct>(2);
             while (dec.hasRemaining())
             {
                 structs.add(dec.readStruct32());
@@ -222,6 +220,8 @@ public class Assembler implements Receiver<NetworkEvent>, NetworkDelegate
         default:
             throw new IllegalStateException("unknown frame type: " + frame.getType());
         }
+
+        dec.releaseBuffer();
     }
 
 }
