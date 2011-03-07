ab3a8d6bf4f22b7271047bbf4377fdbb9135bbeb
Rajith Muditha Attapattu <rajith@apache.org>
2011-03-07 21:38:52 +0000

    QPID-3109 Added a null check for the destination in both MessageConsumer and MessageProducer close methods. According to the JMS spec the MessageProducer can be created with a null destination and still be used properly by specifying a destination on the send. Therefore this null check especially important on the message producer side.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1078961 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/client/src/main/java/org/apache/qpid/client/BasicMessageConsumer_0_10.java
+++ qpid/java/client/src/main/java/org/apache/qpid/client/BasicMessageConsumer_0_10.java
@@ -23,6 +23,7 @@ import org.apache.qpid.client.AMQDestination.AddressOption;
 import org.apache.qpid.client.AMQDestination.DestSyntax;
 import org.apache.qpid.client.failover.FailoverException;
 import org.apache.qpid.client.message.*;
+import org.apache.qpid.client.messaging.address.Node.QueueNode;
 import org.apache.qpid.client.protocol.AMQProtocolHandler;
 import org.apache.qpid.framing.AMQShortString;
 import org.apache.qpid.framing.FieldTable;
@@ -515,7 +516,7 @@ public class BasicMessageConsumer_0_10 extends BasicMessageConsumer<UnprocessedM
     void cleanupQueue() throws AMQException, FailoverException
     {
         AMQDestination dest = this.getDestination();
-        if (dest.getDestSyntax() == AMQDestination.DestSyntax.ADDR)
+        if (dest != null && dest.getDestSyntax() == AMQDestination.DestSyntax.ADDR)
         {
             if (dest.getDelete() == AddressOption.ALWAYS ||
                 dest.getDelete() == AddressOption.RECEIVER )
