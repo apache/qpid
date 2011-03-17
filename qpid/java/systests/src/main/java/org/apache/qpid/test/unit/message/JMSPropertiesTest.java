d0cd511290c6035ac4d5e9f5d48e76d49d5194d1
Rajith Muditha Attapattu <rajith@apache.org>
2011-03-17 22:05:26 +0000

    Added a test case for the issue highlighted by QPID-2930
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1082719 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/systests/src/main/java/org/apache/qpid/test/unit/message/JMSPropertiesTest.java
+++ qpid/java/systests/src/main/java/org/apache/qpid/test/unit/message/JMSPropertiesTest.java
@@ -31,6 +31,7 @@ import org.apache.qpid.test.utils.QpidBrokerTestCase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.jms.Connection;
 import javax.jms.Destination;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
@@ -39,6 +40,8 @@ import javax.jms.MessageProducer;
 import javax.jms.ObjectMessage;
 import javax.jms.Queue;
 import javax.jms.Session;
+import javax.jms.Topic;
+
 import java.util.Enumeration;
 
 /**
@@ -163,4 +166,29 @@ public class JMSPropertiesTest extends QpidBrokerTestCase
         con.close();
     }
 
+    /**
+     * Test Goal : test if the message properties can be retrieved properly with out an error
+     *             and also test if unsupported properties are filtered out. See QPID-2930.
+     */
+    public void testGetPropertyNames() throws Exception
+    {
+        Connection con = getConnection("guest", "guest");
+        Session ssn = (AMQSession) con.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        con.start();
+        
+        Topic topic = ssn.createTopic("amq.direct/test");
+        MessageConsumer consumer = ssn.createConsumer(topic);
+        MessageProducer prod = ssn.createProducer(topic);
+        prod.send(ssn.createMessage());
+        
+        Message msg = consumer.receive(1000);
+        assertNotNull(msg);
+        
+    	Enumeration<String> enu = msg.getPropertyNames();
+    	while (enu.hasMoreElements()) 
+    	{ 
+    		String name = enu.nextElement(); 
+    		String value = msg.getStringProperty(name);
+       } 
+    }
 }
