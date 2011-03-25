19f781317e57fc3679ebba1e26176f2d7976e877
Robert Gemmell <robbie@apache.org>
2011-03-25 13:00:53 +0000

    QPID-3166: add system test using multiple batch transacted producers with multiple consumers using unique selectors. Exposes issue detailed in QPID-3165.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/branches/0.5.x-dev@1085353 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/systests/src/main/java/org/apache/qpid/test/utils/QpidTestCase.java
+++ qpid/java/systests/src/main/java/org/apache/qpid/test/utils/QpidTestCase.java
@@ -1209,7 +1209,8 @@ public class QpidTestCase extends TestCase
 
         MessageProducer producer = session.createProducer(destination);
 
-        for (int i = offset; i < (count + offset); i++)
+        int i = offset;
+        for (; i < (count + offset); i++)
         {
             Message next = createNextMessage(session, i);
 
@@ -1232,7 +1233,7 @@ public class QpidTestCase extends TestCase
         // we have no batchSize or
         // our count is not divible by batchSize. 
         if (session.getTransacted() &&
-            ( batchSize == 0 || count % batchSize != 0))
+            ( batchSize == 0 || (i-1) % batchSize != 0))
         {
             session.commit();
         }
