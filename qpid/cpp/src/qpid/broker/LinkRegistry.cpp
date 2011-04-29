db64c6662a3a1a54abb31a6368138ca0a3752f8c
Alan Conway <aconway@apache.org>
2011-04-29 15:21:20 +0000

    QPID-3235: clustered qpidd broker fails ocassionly the cluster_tests.ShortTests.test_route_update
    Inconsistent stats changes on a Link were causing cluster
    inconsistency. Fix is to disable those stats changes in a cluster.
    
    Updated cluster_tests.py to reliably generate the error every time
    without the fix.
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1097838 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/qpid/broker/LinkRegistry.cpp
+++ qpid/cpp/src/qpid/broker/LinkRegistry.cpp
@@ -7,9 +7,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -381,7 +381,7 @@ std::string LinkRegistry::createKey(const std::string& host,  uint16_t port) {
     return keystream.str();
 }
 
-void LinkRegistry::setPassive(bool p) 
+void LinkRegistry::setPassive(bool p)
 {
     Mutex::ScopedLock locker(lock);
     passiveChanged = p != passive;
