710a8418fd8dc006e357df91e327b64995472642
Andrew Stitcher <astitcher@apache.org>
2011-05-26 20:38:35 +0000

    NO-JIRA: Small improvements to Socket code - Rewrite SocketAddress::operator= in terms of copy constructor - Improve error diagnostic
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1128066 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/sys/posix/SocketAddress.cpp
+++ cpp/src/qpid/sys/posix/SocketAddress.cpp
@@ -27,6 +27,8 @@
 #include <string.h>
 #include <netdb.h>
 
+#include <algorithm>
+
 namespace qpid {
 namespace sys {
 
@@ -46,15 +48,9 @@ SocketAddress::SocketAddress(const SocketAddress& sa) :
 
 SocketAddress& SocketAddress::operator=(const SocketAddress& sa)
 {
-    if (&sa != this) {
-        host = sa.host;
-        port = sa.port;
+    SocketAddress temp(sa);
 
-        if (addrInfo) {
-            ::freeaddrinfo(addrInfo);
-            addrInfo = 0;
-        }
-    }
+    std::swap(temp, *this);
     return *this;
 }
 
@@ -88,7 +84,7 @@ const ::addrinfo& getAddrInfo(const SocketAddress& sa)
 
         int n = ::getaddrinfo(node, service, &hints, &sa.addrInfo);
         if (n != 0)
-            throw Exception(QPID_MSG("Cannot resolve " << sa.host << ": " << ::gai_strerror(n)));
+            throw Exception(QPID_MSG("Cannot resolve " << sa.asString() << ": " << ::gai_strerror(n)));
     }
 
     return *sa.addrInfo;
