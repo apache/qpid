28182d6022f7a09065312428294faa38152f5ef3
Alan Conway <aconway@apache.org>
2011-06-17 18:35:48 +0000

    NO-JIRA: Fix compile error on windows due to assigning an AtomicValue.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1136971 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/sys/AtomicValue_gcc.h
+++ cpp/src/qpid/sys/AtomicValue_gcc.h
@@ -10,9 +10,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -39,6 +39,9 @@ class AtomicValue
   public:
     AtomicValue(T init=0) : value(init) {}
 
+    // Not atomic. Don't call concurrently with atomic ops.
+    AtomicValue<T>& operator=(T newValue) { value = newValue; return *this; }
+
     // Update and return new value.
     inline T operator+=(T n) { return __sync_add_and_fetch(&value, n); }
     inline T operator-=(T n) { return __sync_sub_and_fetch(&value, n); }
@@ -54,11 +57,11 @@ class AtomicValue
     /** If current value == testval then set to newval. Returns the old value. */
     T valueCompareAndSwap(T testval, T newval) { return __sync_val_compare_and_swap(&value, testval, newval); }
 
-    /** If current value == testval then set to newval. Returns true if the swap was performed. */    
+    /** If current value == testval then set to newval. Returns true if the swap was performed. */
     bool boolCompareAndSwap(T testval, T newval) { return __sync_bool_compare_and_swap(&value, testval, newval); }
 
     T get() const { return const_cast<AtomicValue<T>*>(this)->fetchAndAdd(static_cast<T>(0)); }
-        
+
   private:
     T value;
 };
