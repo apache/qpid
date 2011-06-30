6fd21cd3db5e4ba67690471af3f0e39fc00f9bff
Justin Ross <jross@apache.org>
2011-06-30 18:05:22 +0000

    QPID-3331: Update trunk versions to 0.13 for the next release
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1141643 13f79535-47bb-0310-9956-ffa450edef68

--- java/broker/src/main/java/org/apache/qpid/server/plugins/PluginManager.java
+++ java/broker/src/main/java/org/apache/qpid/server/plugins/PluginManager.java
@@ -63,7 +63,7 @@ public class PluginManager implements Closeable
     private static final Logger _logger = Logger.getLogger(PluginManager.class);
 
     private static final int FELIX_STOP_TIMEOUT = 30000;
-    private static final String QPID_VER_SUFFIX = "version=0.11,";
+    private static final String QPID_VER_SUFFIX = "version=0.13,";
 
     private Framework _felix;
 
