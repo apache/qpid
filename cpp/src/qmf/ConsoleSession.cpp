a3d97466c5c51a1d07c2d1b41011f38bd02d9a28
Ted Ross <tross@apache.org>
2011-07-12 11:49:32 +0000

    QPID-3275 - QMF Console asynchronous correlation-id should be scoped to the session, not the specific agent
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1145557 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qmf/ConsoleSession.cpp
+++ cpp/src/qmf/ConsoleSession.cpp
@@ -68,7 +68,7 @@ Subscription ConsoleSession::subscribe(const string& q, const string& f, const s
 ConsoleSessionImpl::ConsoleSessionImpl(Connection& c, const string& options) :
     connection(c), domain("default"), maxAgentAgeMinutes(5), listenOnDirect(true), strictSecurity(false),
     opened(false), thread(0), threadCanceled(false), lastVisit(0), lastAgePass(0),
-    connectedBrokerInAgentList(false), schemaCache(new SchemaCache())
+    connectedBrokerInAgentList(false), schemaCache(new SchemaCache()), nextCorrelator(1)
 {
     if (!options.empty()) {
         qpid::messaging::AddressParser parser(options);
