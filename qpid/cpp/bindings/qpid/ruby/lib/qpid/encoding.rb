a6cc50a6269371d7e68cd8861b451de8b05a73e3
Ted Ross <tross@apache.org>
2011-07-15 16:57:19 +0000

    QPID-3306 - Provides a more Ruby-like set of APIs on top of the bindings created by swig. Applied patch from Darryl Pierce
    Created the encode and decode methods in
     Qpid::Messaging.
    
    These methods differ from the same methods in Cqpid in that they handle
    working with symbols and other non-string values.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1147239 13f79535-47bb-0310-9956-ffa450edef68

---
+++ qpid/cpp/bindings/qpid/ruby/lib/qpid/encoding.rb
@@ -0,0 +1,56 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+require 'cqpid'
+
+module Qpid
+
+  module Messaging
+
+    # Encodes the supplied content into the given message.
+    def self.encode content, message, encoding = nil
+      prepared = content
+      case content
+      when Hash
+        prepared = {}
+        content.each_pair do |key,value|
+          prepared[key.to_s] = value.to_s
+        end
+        Cqpid::encode prepared, message.message_impl
+      when Array
+        prepared = []
+        content.each {|value| prepared << value.to_s}
+        Cqpid::encode prepared, message.message_impl
+      end
+    end
+
+    # Decodes and returns the message's content.
+    def self.decode(message, content_type = nil)
+      content_type = message.content_type unless content_type
+
+      case content_type
+        when "amqp/map":  Cqpid.decodeMap message.message_impl
+        when "amqp/list": Cqpid.decodeList message.message_impl
+      end
+    end
+
+  end
+
+end
+
