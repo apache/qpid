144a676c34e338358d203344f053f951303674f2
Alan Conway <aconway@apache.org>
2011-10-07 20:59:22 +0000

    QPID-2920: Update new-cluster-design and new-cluster-plan to reflect current status.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/branches/qpid-2920-active@1180238 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/qpid/cluster/exp/overview.h
+++ qpid/cpp/src/qpid/cluster/exp/overview.h
@@ -3,15 +3,13 @@
 
 <h1>New cluster implementation overview</h>
 
-The code is broken down into 3 areas indicated by a suffix on class names:
+Naming conventions: There are 3 types of classes indicated by a suffix on class names:
 
-- Replica: State that is replicated to the entire cluster.
+- *Handler: Dispatch CPG messages by calling Replica objects in the deliver thread.
+
+- *Replica: State that is replicated to the entire cluster.
   Only called by Handlers in the deliver thread. May call on Contexts.
 
-- Context: State private to this member and associated with a local entity
+- *Context: State private to this member and associated with a local entity
   such as the Broker or a Queue. Called in deliver and connection threads.
-  
-- Handler: Dispatch CPG messages by calling Replica objects in the deliver thread.
-
-
 **/
