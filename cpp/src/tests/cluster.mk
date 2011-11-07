ce896aa8ee8d910db26abbe1e15930e8ca8735b4
Andrew Stitcher <astitcher@apache.org>
2011-11-07 20:56:53 +0000

    QPID-3586: Changes for QPID-3464 break cmake build in tar.gz distribution - Added in the new CMake files to EXTRA_DIST in the Makefile.am files so   that they get built into the tarballs - Added some cluster test files back into the .../tests/cluster.mk file   as they seem to have been dropped by mistake (and having them missing   also breaks the cmake build in a similar way)
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1198929 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/tests/cluster.mk
+++ cpp/src/tests/cluster.mk
@@ -83,14 +83,16 @@ LONG_TESTS +=					\
 qpidtest_PROGRAMS += cluster_test
 
 cluster_test_SOURCES =				\
-	cluster_test.cpp			\
-	unit_test.cpp				\
-	ClusterFixture.cpp			\
-	ClusterFixture.h			\
-	ForkedBroker.h				\
-	ForkedBroker.cpp			\
-	PartialFailure.cpp			\
-	ClusterFailover.cpp
+    cluster_test.cpp			\
+    unit_test.cpp				\
+    ClusterFixture.cpp			\
+    ClusterFixture.h			\
+    ForkedBroker.h				\
+    ForkedBroker.cpp			\
+    PartialFailure.cpp			\
+    ClusterFailover.cpp         \
+    InitialStatusMap.cpp        \
+    StoreStatus.cpp
 
 cluster_test_LDADD=$(lib_client) $(lib_broker) ../cluster.la -lboost_unit_test_framework
 
