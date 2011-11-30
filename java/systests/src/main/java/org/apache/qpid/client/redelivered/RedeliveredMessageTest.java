48d5695205f12c604bf3994b97d8903ce3c52d87
Keith Wall <kwall@apache.org>
2011-11-30 13:58:14 +0000

    QPID-3642: Fix for redelivery regression found by python tests
    Applied patch from Andrew MacBean <andymacbean@gmail.com> and Oleksandr Rudyy<orudyy@gmail.com>
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1208435 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/systests/src/main/java/org/apache/qpid/client/redelivered/RedeliveredMessageTest.java
@@ -0,0 +1,50 @@
+package org.apache.qpid.client.redelivered;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import org.apache.qpid.test.utils.QpidBrokerTestCase;
+
+public class RedeliveredMessageTest extends QpidBrokerTestCase
+{
+    private Connection _connection;
+
+    public void setUp() throws Exception
+    {
+        super.setUp();
+        _connection = getConnection();
+    }
+
+    public void testRedeliveredFlagOnSessionClose() throws Exception
+    {
+        Session session = _connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        Destination destination = session.createQueue(getTestQueueName());
+        MessageConsumer consumer = session.createConsumer(destination);
+
+        final int numberOfMessages = 3;
+        sendMessage(session, destination, numberOfMessages);
+
+        _connection.start();
+
+        for(int i = 0; i < numberOfMessages; i++)
+        {
+            final Message m = consumer.receive(1000l);
+            assertNotNull("Message is not recieved at " + i, m);
+            assertFalse("Redelivered should be not set", m.getJMSRedelivered());
+        }
+
+        session.close();
+        session = _connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        destination = session.createQueue(getTestQueueName());
+        consumer = session.createConsumer(destination);
+
+        for(int i = 0; i < numberOfMessages; i++)
+        {
+            final Message m = consumer.receive(1000l);
+            assertNotNull("Message is not recieved at " + i, m);
+            assertTrue("Redelivered should be set", m.getJMSRedelivered());
+        }
+    }
+}
