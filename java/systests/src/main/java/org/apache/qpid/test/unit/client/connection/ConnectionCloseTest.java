8bb4c92fd3df90732e428d34aa730f71230d1484
Keith Wall <kwall@apache.org>
2012-01-04 22:55:54 +0000

    QPID-3566: Remove test case testSendReceiveClose test
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1227373 13f79535-47bb-0310-9956-ffa450edef68

--- java/systests/src/main/java/org/apache/qpid/test/unit/client/connection/ConnectionCloseTest.java
+++ java/systests/src/main/java/org/apache/qpid/test/unit/client/connection/ConnectionCloseTest.java
@@ -20,19 +20,11 @@
  */
 package org.apache.qpid.test.unit.client.connection;
 
-import org.apache.qpid.test.utils.QpidBrokerTestCase;
-import org.apache.qpid.transport.util.Logger;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.jms.Connection;
 import javax.jms.JMSException;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageProducer;
-import javax.jms.Queue;
 import javax.jms.Session;
-import javax.jms.TextMessage;
+
+import org.apache.qpid.test.utils.QpidBrokerTestCase;
 
 /**
  * ConnectionCloseTest
@@ -41,63 +33,6 @@ import javax.jms.TextMessage;
 
 public class ConnectionCloseTest extends QpidBrokerTestCase
 {
-
-    private static final Logger log = Logger.get(ConnectionCloseTest.class);
-
-    public void testSendReceiveClose() throws Exception
-    {
-        Map<Thread,StackTraceElement[]> before = Thread.getAllStackTraces();
-
-        for (int i = 0; i < 50; i++)
-        {
-            if ((i % 10) == 0)
-            {
-                log.warn("%d messages sent and received", i);
-            }
-
-            Connection receiver = getConnection();
-            receiver.start();
-            Session rssn = receiver.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            Queue queue = rssn.createQueue("connection-close-test-queue");
-            MessageConsumer cons = rssn.createConsumer(queue);
-
-            Connection sender = getConnection();
-            sender.start();
-            Session sssn = sender.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            MessageProducer prod = sssn.createProducer(queue);
-            prod.send(sssn.createTextMessage("test"));
-            sender.close();
-
-            TextMessage m = (TextMessage) cons.receive(2000);
-            assertNotNull("message was lost", m);
-            assertEquals(m.getText(), "test");
-            receiver.close();
-        }
-
-        // The finalizer is notifying connector thread waiting on a selector key.
-        // This should leave the finalizer enough time to notify those threads 
-        synchronized (this)
-        {
-            this.wait(10000);
-        }
-
-        Map<Thread,StackTraceElement[]> after = Thread.getAllStackTraces();
-        
-        Map<Thread,StackTraceElement[]> delta = new HashMap<Thread,StackTraceElement[]>(after);
-        for (Thread t : before.keySet())
-        {
-            delta.remove(t);
-        }
-
-        dumpStacks(delta);
-
-        int deltaThreshold = (isExternalBroker()? 1 : 2) //InVM creates more thread pools in the same VM
-                            * (Runtime.getRuntime().availableProcessors() + 1) + 5; 
-
-        assertTrue("Spurious thread creation exceeded threshold, " +
-                   delta.size() + " threads created.",
-                   delta.size() < deltaThreshold);
-    }
 
     /**
      * This test is added due to QPID-3453 to test connection closing when AMQ
@@ -121,16 +56,6 @@ public class ConnectionCloseTest extends QpidBrokerTestCase
         {
             // session closing should not fail
             fail("Cannot close connection:" + e.getMessage());
-        }
-    }
-
-    private void dumpStacks(Map<Thread,StackTraceElement[]> map)
-    {
-        for (Map.Entry<Thread,StackTraceElement[]> entry : map.entrySet())
-        {
-            Throwable t = new Throwable();
-            t.setStackTrace(entry.getValue());
-            log.warn(t, entry.getKey().toString());
         }
     }
 
