afffff47f6fc8f81e285e58d7e92c556a96a95af
Ted Ross <tross@apache.org>
2012-01-05 19:45:01 +0000

    QPID-3727 - Applied patch from Darryl Pierce
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1227786 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/bindings/qpid/ruby/lib/qpid/address.rb
+++ cpp/bindings/qpid/ruby/lib/qpid/address.rb
@@ -38,75 +38,137 @@ module Qpid
     # Or they can be lists of values, where they are contained within square
     # brackets but still comma delimited, such as:
     #
-    # [value1,value2,value3]
+    #   [value1,value2,value3]
     #
     # The following are the list of supported options:
     #
-    # create:: Indicates if the address should be created; values are *always*,
-    #          *never*, *sender* or *reciever*.
+    # [:create]
+    #   Indicates if the address should be created; values are *always*,
+    #   *never*, *sender* or *reciever*.
     #
-    # assert:: Indicates whether or not to assert any specified node properties;
-    #          values are *always*, *never*, *sender* or *receiver*.
+    # [:assert]
+    #   Indicates whether or not to assert any specified node properties;
+    #   values are *always*, *never*, *sender* or *receiver*.
     #
-    # delete:: Indicates whether or not to delete the addressed node when a
-    #          sender or receiver is cancelled; values are *always*, *never*,
-    #          *sender* or *receiver*.
+    # [:delete]
+    #   Indicates whether or not to delete the addressed node when a sender
+    #   or receiver is cancelled; values are *always*, *never*, *sender* or
+    #   *receiver*.
     #
-    # node:: A nested map describing properties for the addressed node.
-    #        Properties are *type* (*topic* or *queue*), *durable* (a boolean),
-    #        *x-declare* (a nested map of amqp 0.10-specific options) and
-    #        *x-bindings*. (nested list which specifies a queue, exchange or
-    #        a binding key and arguments.
+    # [:node]
+    #   A nested map describing properties for the addressed node. Properties
+    #   are *type* (*topic* or *queue*), *durable* (a boolean), *x-declare*
+    #   (a nested map of amqp 0.10-specific options) and *x-bindings*. (nested
+    #   list which specifies a queue, exchange or a binding key and arguments.
     #
-    # link:: A nested map through which properties of the link can be specified;
-    #        properties are *durable*, *reliability*, *x-declare*, *x-subscribe*
-    #        and *x-bindings*.
+    # [:link]
+    #   A nested map through which properties of the link can be specified;
+    #   properties are *durable*, *reliability*, *x-declare*, *x-subscribe*
+    #   and *x-bindings*.
     #
-    # mode:: (*For receivers only*) indicates whether the receiver should consume
-    #        or browse messages; values are *consume* (the default) and *browse*.
+    # [:mode]
+    #   (*For receivers only*) indicates whether the receiver should consume
+    #   or browse messages; values are *consume* (the default) and *browse*.
+    #
     class Address
 
+      # Creates a new +Address+ object.
+      #
+      # ==== Options
+      #
+      # * name - The name for the +Address+.
+      # * subject - The subject for the +Address+
+      # * :create - See the class documentation.
+      # * :assert - See the class documentation.
+      # * :delete - See the class documentation.
+      # * :node   - See the class documentation.
+      # * :link   - See the class documentation.
+      # * :mode   - See the class documentation.
+      #
+      # ==== Examples
+      #
+      #   addr = Qpid::Messaging::Address.new "my-queue"
+      #   addr = Qpid::Messaging::Address.new "my-queue", "testing", :create => :always
+      #
       def initialize(name, subject, options = {}, _type = "", address_impl = nil)
         @address_impl = address_impl || Cqpid::Address.new(name, subject, convert_options(options), _type)
       end
 
-       def address_impl # :nodoc:
-         @address_impl
-       end
+      def address_impl # :nodoc:
+        @address_impl
+      end
 
-       # Returns the name.
+      # Returns the name for the +Address+.
+      #
+      # ==== Examples
+      #
+      #   puts "The address name is #{addr.name}."
+      #
       def name; @address_impl.getName; end
 
-      # Sets the name.
+      # Sets the name for the +Address+.
+      #
+      # ==== Examples
+      #
+      #   addr.name = "my-new-queue"
+      #
       def name=(name); @address_impl.setName name; end
 
-      # Returns the subject.
+      # Returns the subject for the +Address+.
+      #
+      # ==== Examples
+      #
+      #   puts "The subject is #{addr.subject}."
+      #
       def subject; @address_impl.getSubject; end
 
-      # Sets the subject.
+      # Sets the subject for the +Address+.
+      #
+      # ==== Examples
+      #
+      #   addr.subject = "testing"
+      #
       def subject=(subject); @address_impl.setSubject(subject); end
 
-      # Returns the type.
+      # Returns the type for the +Address+.
+      #
+      # ==== Examples
+      #
+      #   puts "The address is a #{address.address_type}."
+      #
       #---
       # We cannot use "type" since that clashes with the Ruby object.type
       # identifier.
-      def _type; @address_impl.getType; end
+      def address_type; @address_impl.getType; end
 
-      # Sets the type.
+      # Sets the type for the +Address+.
       #
-      # The type of the address determines how Sender and Receiver objects
+      # The type of the address determines how +Sender+ and +Receiver+ objects
       # are constructed for it. If no type is specified then it will be
       # determined by querying the broker.
-      def _type=(_type); @address_impl.setType(_type); end
+      #
+      # ===== Options
+      #
+      # * type - the address type
+      #
+      def address_type=(type); @address_impl.setType(type); end
 
       # Returns the options.
       def options; @address_impl.getOptions; end
 
       # Sets the options for the address.
-      # Any symbols are converted to strings.
-      def options=(options); @address_impl.setOptions(convert_options(options)); end
+      #
+      # *NOTE:* See the class documentation for more details on options.
+      #
+      # ==== Examples
+      #
+      #   addr.options = :create => :always
+      #
+      def options=(options = {}); @address_impl.setOptions(convert_options(options)); end
 
-      def to_s; @address_impl.str; end
+      def to_s # :nodoc:
+        @address_impl.str
+      end
 
       private
 
