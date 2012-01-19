317df59895404093eef4ca69ddf566e062b4f81a
Alan Conway <aconway@apache.org>
2012-01-19 23:03:03 +0000

    QPID-3603: Move init code for WiringReplicator out of Bridge into ha::Backup.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/branches/qpid-3603-2@1233641 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/qpid/ha/Backup.cpp
+++ qpid/cpp/src/qpid/ha/Backup.cpp
@@ -21,10 +21,78 @@
 #include "Backup.h"
 #include "Settings.h"
 #include "qpid/Url.h"
+#include "qpid/amqp_0_10/Codecs.h"
+#include "qpid/broker/Bridge.h"
 #include "qpid/broker/Broker.h"
+#include "qpid/broker/SessionHandler.h"
+#include "qpid/framing/AMQP_ServerProxy.h"
+#include "qpid/framing/AMQFrame.h"
+#include "qpid/framing/FieldTable.h"
+#include "qpid/framing/MessageTransferBody.h"
+#include "qpid/types/Variant.h"
 
 namespace qpid {
 namespace ha {
+
+using namespace framing;
+using namespace broker;
+using types::Variant;
+
+namespace {
+const std::string QPID_WIRING_REPLICATOR("qpid.wiring-replicator");
+}
+
+// Initialize a bridge as a wiring replicator.
+void bridgeInitWiringReplicator(Bridge& bridge, SessionHandler& sessionHandler) {
+    framing::AMQP_ServerProxy peer(sessionHandler.out);
+    std::string queueName = bridge.getQueueName();
+    const qmf::org::apache::qpid::broker::ArgsLinkBridge& args(bridge.getArgs());
+
+    //declare and bind an event queue
+    peer.getQueue().declare(queueName, "", false, false, true, true, FieldTable());
+    peer.getExchange().bind(queueName, "qmf.default.topic", "agent.ind.event.org_apache_qpid_broker.#", FieldTable());
+    //subscribe to the queue
+    peer.getMessage().subscribe(queueName, args.i_dest, 1, 0, false, "", 0, FieldTable());
+    peer.getMessage().flow(args.i_dest, 0, 0xFFFFFFFF);
+    peer.getMessage().flow(args.i_dest, 1, 0xFFFFFFFF);
+
+    //issue a query request for queues and another for exchanges using event queue as the reply-to address
+    for (int i = 0; i < 2; ++i) {//TODO: cleanup this code into reusable utility functions
+        Variant::Map request;
+        request["_what"] = "OBJECT";
+        Variant::Map schema;
+        schema["_class_name"] = (i == 0 ? "queue" : "exchange");
+        schema["_package_name"] = "org.apache.qpid.broker";
+        request["_schema_id"] = schema;
+
+        AMQFrame method((MessageTransferBody(ProtocolVersion(), "qmf.default.direct", 0, 0)));
+        method.setBof(true);
+        method.setEof(false);
+        method.setBos(true);
+        method.setEos(true);
+        AMQHeaderBody headerBody;
+        MessageProperties* props = headerBody.get<MessageProperties>(true);
+        props->setReplyTo(qpid::framing::ReplyTo("", queueName));
+        props->setAppId("qmf2");
+        props->getApplicationHeaders().setString("qmf.opcode", "_query_request");
+        headerBody.get<qpid::framing::DeliveryProperties>(true)->setRoutingKey("broker");
+        AMQFrame header(headerBody);
+        header.setBof(false);
+        header.setEof(false);
+        header.setBos(true);
+        header.setEos(true);
+        AMQContentBody data;
+        qpid::amqp_0_10::MapCodec::encode(request, data.getData());
+        AMQFrame content(data);
+        content.setBof(false);
+        content.setEof(true);
+        content.setBos(true);
+        content.setEos(true);
+        sessionHandler.out->handle(method);
+        sessionHandler.out->handle(header);
+        sessionHandler.out->handle(content);
+    }
+}
 
 Backup::Backup(broker::Broker& b, const Settings& s) : broker(b), settings(s) {
     // Create a link to replicate wiring
@@ -41,15 +109,17 @@ Backup::Backup(broker::Broker& b, const Settings& s) : broker(b), settings(s) {
         broker.getLinks().declare( // Declare the bridge
             url[0].host, url[0].port,
             false,              // durable
-            "qpid.wiring-replicator", // src
-            "qpid.wiring-replicator", // dest
+            QPID_WIRING_REPLICATOR, // src
+            QPID_WIRING_REPLICATOR, // dest
             "x",                // key
             false,              // isQueue
             false,              // isLocal
             "",                 // id/tag
             "",                 // excludes
             false,              // dynamic
-            0);                 // sync?
+            0,                  // sync?
+            bridgeInitWiringReplicator
+        );
     }
     // FIXME aconway 2011-11-17: need to enhance the link code to
     // handle discovery of the primary broker and fail-over correctly.
