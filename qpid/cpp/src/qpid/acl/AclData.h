bf33abec376f20e1cf5d7579ee449339097ab1ec
Charles E. Rolke <chug@apache.org>
2012-02-17 17:16:33 +0000

    NO-JIRA ACL whitespace police. No code change.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1245660 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/qpid/acl/AclData.h
+++ qpid/cpp/src/qpid/acl/AclData.h
@@ -32,53 +32,53 @@ class AclData {
 
 public:
 
-   typedef std::map<qpid::acl::Property, std::string> propertyMap;
-   typedef propertyMap::const_iterator propertyMapItr;
-   struct rule {
-	  
-	   bool log;
-	   bool logOnly;  // this is a rule is to log only
-	   
-	   // key value map
-      //??
-      propertyMap props;
-	  
-	  
-	  rule (propertyMap& p):log(false),logOnly(false),props(p) {};
+    typedef std::map<qpid::acl::Property, std::string> propertyMap;
+    typedef propertyMap::const_iterator propertyMapItr;
+    struct rule {
 
-	  std::string toString () const {
-	  	std::ostringstream ruleStr;
-	  	ruleStr << "[log=" << log << ", logOnly=" << logOnly << " props{";
-	  	for (propertyMapItr pMItr = props.begin(); pMItr != props.end(); pMItr++) {
-	  		ruleStr << " " << AclHelper::getPropertyStr((Property) pMItr-> first) << "=" << pMItr->second;
-	  	}
-	  	ruleStr << " }]";
-	  	return ruleStr.str();
-	  }
-   };
-   typedef  std::vector<rule> ruleSet;
-   typedef  ruleSet::const_iterator ruleSetItr;
-   typedef  std::map<std::string, ruleSet > actionObject; // user 
-   typedef  actionObject::iterator actObjItr;
-   typedef  actionObject* aclAction;
-  
-   // Action*[] -> Object*[] -> map<user -> set<Rule> >
-   aclAction* actionList[qpid::acl::ACTIONSIZE];
-   qpid::acl::AclResult decisionMode;  // determines if the rule set is a deny or allow mode. 
-   bool transferAcl;
-   std::string aclSource; 
-   
-   AclResult lookup(const std::string& id, const Action& action, const ObjectType& objType, const std::string& name, std::map<Property, std::string>* params=0);
-   AclResult lookup(const std::string& id, const Action& action, const ObjectType& objType, const std::string& ExchangeName, const std::string& RoutingKey);
-   AclResult getACLResult(bool logOnly, bool log);
-  
-   bool matchProp(const std::string & src, const std::string& src1);
-   void clear ();
-  
-   AclData();
-   virtual ~AclData();
+        bool log;
+        bool logOnly;  // this is a rule is to log only
+
+        // key value map
+        //??
+        propertyMap props;
+
+
+        rule (propertyMap& p):log(false),logOnly(false),props(p) {};
+
+        std::string toString () const {
+            std::ostringstream ruleStr;
+            ruleStr << "[log=" << log << ", logOnly=" << logOnly << " props{";
+            for (propertyMapItr pMItr = props.begin(); pMItr != props.end(); pMItr++) {
+                ruleStr << " " << AclHelper::getPropertyStr((Property) pMItr-> first) << "=" << pMItr->second;
+            }
+            ruleStr << " }]";
+            return ruleStr.str();
+        }
+    };
+    typedef  std::vector<rule> ruleSet;
+    typedef  ruleSet::const_iterator ruleSetItr;
+    typedef  std::map<std::string, ruleSet > actionObject; // user 
+    typedef  actionObject::iterator actObjItr;
+    typedef  actionObject* aclAction;
+
+    // Action*[] -> Object*[] -> map<user -> set<Rule> >
+    aclAction* actionList[qpid::acl::ACTIONSIZE];
+    qpid::acl::AclResult decisionMode;  // determines if the rule set is a deny or allow mode. 
+    bool transferAcl;
+    std::string aclSource; 
+
+    AclResult lookup(const std::string& id, const Action& action, const ObjectType& objType, const std::string& name, std::map<Property, std::string>* params=0);
+    AclResult lookup(const std::string& id, const Action& action, const ObjectType& objType, const std::string& ExchangeName, const std::string& RoutingKey);
+    AclResult getACLResult(bool logOnly, bool log);
+
+    bool matchProp(const std::string & src, const std::string& src1);
+    void clear ();
+
+    AclData();
+    virtual ~AclData();
 };
-    
+
 }} // namespace qpid::acl
 
 #endif // QPID_ACL_ACLDATA_H
