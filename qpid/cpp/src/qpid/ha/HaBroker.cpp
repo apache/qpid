459dc28a54dfc931ee112a9cc310973d1ad01cb5
Alan Conway <aconway@apache.org>
2012-02-17 14:14:14 +0000

    QPID-3603: Get rid of broker_url="primary" hack, promote primaries via management.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/branches/qpid-3603-7@1245535 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/qpid/ha/HaBroker.cpp
+++ qpid/cpp/src/qpid/ha/HaBroker.cpp
@@ -55,22 +55,16 @@ HaBroker::HaBroker(broker::Broker& b, const Settings& s)
       settings(s),
       clientUrl(url(s.clientUrl, "ha-client-url")),
       brokerUrl(url(s.brokerUrl, "ha-broker-url")),
+      backup(new Backup(b, s)),
       mgmtObject(0)
 {
-    // FIXME aconway 2011-11-22: temporary hack to identify primary.
-    bool primary = (settings.brokerUrl == PRIMARY);
-    QPID_LOG(notice, "HA: " << (primary ? "Primary" : "Backup")
-             << " initialized: client-url=" << clientUrl
+    // Note all HA brokers start out in backup mode.
+    QPID_LOG(notice, "HA: Backup initialized: client-url=" << clientUrl
              << " broker-url=" << brokerUrl);
-    if (!primary) backup.reset(new Backup(broker, s));
     // Register a factory for replicating subscriptions.
     broker.getConsumerFactories().add(
         boost::shared_ptr<ReplicatingSubscription::Factory>(
             new ReplicatingSubscription::Factory()));
-    // Register a connection excluder
-    broker.getConnectionObservers().add(
-        boost::shared_ptr<broker::ConnectionObserver>(
-            new ConnectionExcluder(boost::bind(&HaBroker::isPrimary, this))));
 
     ManagementAgent* ma = broker.getManagementAgent();
     if (!ma)
@@ -78,8 +72,7 @@ HaBroker::HaBroker(broker::Broker& b, const Settings& s)
     if (ma) {
         _qmf::Package  packageInit(ma);
         mgmtObject = new _qmf::HaBroker(ma, this);
-        // FIXME aconway 2011-11-11: Placeholder - initialize cluster role.
-        mgmtObject->set_status(isPrimary() ? PRIMARY : BACKUP);
+        mgmtObject->set_status(BACKUP);
         ma->addObject(mgmtObject);
     }
 }
@@ -87,21 +80,22 @@ HaBroker::HaBroker(broker::Broker& b, const Settings& s)
 HaBroker::~HaBroker() {}
 
 Manageable::status_t HaBroker::ManagementMethod (uint32_t methodId, Args& args, string&) {
+    sys::Mutex::ScopedLock l(lock);
     switch (methodId) {
       case _qmf::HaBroker::METHOD_SETSTATUS: {
           std::string status = dynamic_cast<_qmf::ArgsHaBrokerSetStatus&>(args).i_status;
           if (status == PRIMARY) {
-              if (!isPrimary()) {
+              if (backup.get()) {
+                  // FIXME aconway 2012-01-26: create primary state before resetting backup
+                  // as it allows client connections.
                   backup.reset();
-                  QPID_LOG(notice, "HA Primary: promoted from backup");
+                  QPID_LOG(notice, "HA: Primary promoted from backup");
               }
           } else if (status == BACKUP) {
-              if (isPrimary()) {
-                  backup.reset(new Backup(broker, settings));
-                  QPID_LOG(notice, "HA Backup: demoted from primary.");
-              }
+              if (!backup.get())
+                  throw Exception("HA: Primary cannot be demoted");
           } else {
-              QPID_LOG(error, "Attempt to set invalid HA status: " << status);
+              throw Exception("Invalid HA status: "+status);
           }
           mgmtObject->set_status(status);
           break;
@@ -110,10 +104,6 @@ Manageable::status_t HaBroker::ManagementMethod (uint32_t methodId, Args& args, 
         return Manageable::STATUS_UNKNOWN_METHOD;
     }
     return Manageable::STATUS_OK;
-}
-
-bool HaBroker::isPrimary() const {
-    return !backup.get();       // TODO aconway 2012-01-18: temporary test.
 }
 
 }} // namespace qpid::ha
