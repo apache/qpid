e205020f172b0be386a2259ea699eeed6db22b1a
Robert Godfrey <rgodfrey@apache.org>
2012-02-20 21:21:43 +0000

    QPID-3588 : Python message echo test fails against Java broker because only expiration is passed to client, not TTL.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1291449 13f79535-47bb-0310-9956-ffa450edef68

--- java/broker/src/main/java/org/apache/qpid/server/subscription/Subscription_0_10.java
+++ java/broker/src/main/java/org/apache/qpid/server/subscription/Subscription_0_10.java
@@ -409,6 +409,10 @@ public class Subscription_0_10 implements Subscription, FlowCreditManager.FlowCr
                 {
                     deliveryProps.setTimestamp(origDeliveryProps.getTimestamp());
                 }
+                if(origDeliveryProps.hasTtl())
+                {
+                    deliveryProps.setTtl(origDeliveryProps.getTtl());
+                }
 
 
             }
