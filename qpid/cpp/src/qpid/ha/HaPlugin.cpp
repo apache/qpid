f41acf6529950afc916382754901c65cb0aca23c
Alan Conway <aconway@apache.org>
2012-04-05 19:31:21 +0000

    QPID-3603: Update HA documentatiion
    Also renamed --ha-replicate-default to --ha-replicate
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1310026 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/qpid/ha/HaPlugin.cpp
+++ qpid/cpp/src/qpid/ha/HaPlugin.cpp
@@ -37,7 +37,7 @@ struct Options : public qpid::Options {
              "URL that backup brokers use to connect and fail over.")
             ("ha-public-brokers", optValue(settings.clientUrl,"URL"),
              "URL that clients use to connect and fail over, defaults to ha-brokers.")
-            ("ha-replicate-default",
+            ("ha-replicate",
              optValue(settings.replicateDefault, "LEVEL"),
             "Replication level for creating queues and exchanges if there is no qpid.replicate argument supplied. LEVEL is 'none', 'configuration' or 'all'")
             ("ha-expected-backups", optValue(settings.expectedBackups, "N"),
