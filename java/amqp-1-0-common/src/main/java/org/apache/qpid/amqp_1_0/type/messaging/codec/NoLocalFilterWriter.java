453e8e4be5e03be8311ff62e4d4c53f943ff647f
Robert Godfrey <rgodfrey@apache.org>
2012-04-16 08:43:24 +0000

    QPID-3949 : [Java AMQP 1.0] Update selector/no-local definitions to use new descriptors
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1326532 13f79535-47bb-0310-9956-ffa450edef68

--- java/amqp-1-0-common/src/main/java/org/apache/qpid/amqp_1_0/type/messaging/codec/NoLocalFilterWriter.java
+++ java/amqp-1-0-common/src/main/java/org/apache/qpid/amqp_1_0/type/messaging/codec/NoLocalFilterWriter.java
@@ -26,7 +26,6 @@ package org.apache.qpid.amqp_1_0.type.messaging.codec;
 import org.apache.qpid.amqp_1_0.codec.AbstractDescribedTypeWriter;
 import org.apache.qpid.amqp_1_0.codec.ListWriter;
 import org.apache.qpid.amqp_1_0.codec.ValueWriter;
-import org.apache.qpid.amqp_1_0.type.Symbol;
 import org.apache.qpid.amqp_1_0.type.messaging.NoLocalFilter;
 
 public class NoLocalFilterWriter extends AbstractDescribedTypeWriter<NoLocalFilter>
@@ -63,7 +62,7 @@ public class NoLocalFilterWriter extends AbstractDescribedTypeWriter<NoLocalFilt
 
     protected Object getDescriptor()
     {
-        return Symbol.valueOf("amqp:no-local-filter:list");
+        return 0x0000468C00000003L;
     }
 
     @Override
