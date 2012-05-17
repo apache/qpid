4d7259bcff1e731dfe61314885470aba70fc76bf
Robert Godfrey <rgodfrey@apache.org>
2012-05-17 20:15:22 +0000

    QPID-4007 : [Java Broker] Add Kerberos authentication support
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1339840 13f79535-47bb-0310-9956-ffa450edef68

--- java/broker/src/main/java/org/apache/qpid/server/plugins/PluginManager.java
+++ java/broker/src/main/java/org/apache/qpid/server/plugins/PluginManager.java
@@ -23,6 +23,7 @@ import org.apache.felix.framework.Felix;
 import org.apache.felix.framework.util.StringMap;
 import org.apache.log4j.Logger;
 import org.apache.qpid.server.security.auth.manager.AnonymousAuthenticationManager;
+import org.apache.qpid.server.security.auth.manager.KerberosAuthenticationManager;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
@@ -158,7 +159,8 @@ public class PluginManager implements Closeable
                 new SlowConsumerDetectionPolicyConfigurationFactory(),
                 new SlowConsumerDetectionQueueConfigurationFactory(),
                 PrincipalDatabaseAuthenticationManager.PrincipalDatabaseAuthenticationManagerConfiguration.FACTORY,
-                AnonymousAuthenticationManager.AnonymousAuthenticationManagerConfiguration.FACTORY))
+                AnonymousAuthenticationManager.AnonymousAuthenticationManagerConfiguration.FACTORY,
+                KerberosAuthenticationManager.KerberosAuthenticationManagerConfiguration.FACTORY))
         {
             _configPlugins.put(configFactory.getParentPaths(), configFactory);
         }
@@ -174,7 +176,8 @@ public class PluginManager implements Closeable
         }
 
         for (AuthenticationManagerPluginFactory<? extends Plugin> pluginFactory : Arrays.asList(
-                PrincipalDatabaseAuthenticationManager.FACTORY, AnonymousAuthenticationManager.FACTORY))
+                PrincipalDatabaseAuthenticationManager.FACTORY, AnonymousAuthenticationManager.FACTORY,
+                KerberosAuthenticationManager.FACTORY))
         {
             _authenticationManagerPlugins.put(pluginFactory.getPluginName(), pluginFactory);
         }
