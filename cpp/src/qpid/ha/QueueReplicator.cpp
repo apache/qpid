47096c52a8c3f581bcf65e9bb50043eb9bd1cea9
Alan Conway <aconway@apache.org>
2012-06-12 21:20:36 +0000

    QPID-3603: Improvements to HA log messages, clarify primary/backp role.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1349543 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/ha/QueueReplicator.cpp
+++ cpp/src/qpid/ha/QueueReplicator.cpp
@@ -159,6 +159,7 @@ void QueueReplicator::route(Deliverable& msg)
         sys::Mutex::ScopedLock l(lock);
         if (!isEventKey(key)) {
             msg.deliverTo(queue);
+            // FIXME aconway 2012-06-10: race, position may have moved
             QPID_LOG(trace, logPrefix << "Enqueued message " << queue->getPosition());
         }
         else if (key == DEQUEUE_EVENT_KEY) {
