6669f066a00db9f8c1cf0003e4d74af476b3a6f4
Alan Conway <aconway@apache.org>
2012-06-26 13:38:07 +0000

    NO-JIRA: Fix build error on windows, introduced in r1352992
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1354012 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/broker/windows/SaslAuthenticator.cpp
+++ cpp/src/qpid/broker/windows/SaslAuthenticator.cpp
@@ -81,7 +81,7 @@ void SaslAuthenticator::fini(void)
     return;
 }
 
-std::auto_ptr<SaslAuthenticator> SaslAuthenticator::createAuthenticator(Connection& c, bool)
+std::auto_ptr<SaslAuthenticator> SaslAuthenticator::createAuthenticator(Connection& c)
 {
     if (c.getBroker().getOptions().auth) {
         return std::auto_ptr<SaslAuthenticator>(new SspiAuthenticator(c));
