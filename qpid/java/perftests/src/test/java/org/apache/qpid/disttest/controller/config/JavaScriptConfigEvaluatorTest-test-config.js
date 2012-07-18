8b6e924b6d11be40f693d59f306eba495de7b959
Keith Wall <kwall@apache.org>
2012-07-18 11:45:01 +0000

    QPID-4103: Refactor test approach to avoid comparing JSON strings.
    Applied patch from Philip Harvey <phil@philharveyonline.com>.
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1362891 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/perftests/src/test/java/org/apache/qpid/disttest/controller/config/JavaScriptConfigEvaluatorTest-test-config.js
+++ qpid/java/perftests/src/test/java/org/apache/qpid/disttest/controller/config/JavaScriptConfigEvaluatorTest-test-config.js
@@ -1,33 +1,22 @@
 jsonObject = {
-  "_tests":
-    QPID.iterations( { "__ACK_MODE": [ 0, 1 ] },
+  "_countries":
+    QPID.iterations( { "__ITERATING_VALUE": [ 0, 1 ] },
       {
           // this is a comment - it wouldn't be allowed if this were pure JSON
 
-          "_name": "Test 1",
-          "_queues": [
+          "_name": "Country",
+          "_regions": QPID.times(2,
             {
-              "_name": "Json-Queue-Name"
-            }
-          ],
-
-          "_clients": QPID.times(2,
-            {
-              "_name": "repeatingClient__CLIENT_INDEX",
-              "_connections": [
+              "_name": "repeatingRegion__REGION_INDEX",
+              "_towns": [
                 {
-                  "_name": "connection1",
-                  "_sessions": [
-                    {
-                      "_sessionName": "session1",
-                      "_acknowledgeMode": "__ACK_MODE",
-                      "_consumers": []
-                    }
-                  ]
+                  "_name": "town1",
+                  "_iteratingAttribute": "__ITERATING_VALUE",
+                  "_consumers": []
                 }
               ]
             },
-            "__CLIENT_INDEX"
+            "__REGION_INDEX"
         )
     })
 
