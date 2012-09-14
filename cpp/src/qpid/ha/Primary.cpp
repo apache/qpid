2076c0c8d7215973f32fcf8f2297d631e985d5ae
Alan Conway <aconway@apache.org>
2012-09-14 19:01:32 +0000

    NO-JIRA: HA minor log message improvement.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1384886 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/ha/Primary.cpp
+++ cpp/src/qpid/ha/Primary.cpp
@@ -201,7 +201,7 @@ void Primary::opened(broker::Connection& connection) {
         Mutex::ScopedLock l(lock);
         BackupMap::iterator i = backups.find(info.getSystemId());
         if (i == backups.end()) {
-            QPID_LOG(debug, logPrefix << "New backup connected: " << info);
+            QPID_LOG(info, logPrefix << "New backup connected: " << info);
             boost::shared_ptr<RemoteBackup> backup(
                 new RemoteBackup(info, haBroker.getReplicationTest(), true));
             {
@@ -212,7 +212,7 @@ void Primary::opened(broker::Connection& connection) {
             backups[info.getSystemId()] = backup;
         }
         else {
-            QPID_LOG(debug, logPrefix << "Known backup connected: " << info);
+            QPID_LOG(info, logPrefix << "Known backup connected: " << info);
             i->second->setConnected(true);
             checkReady(i, l);
         }
