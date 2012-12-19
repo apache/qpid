0bb56e7d2cb945f65b218a735ccd809ddfa9bf5e
Alan Conway <aconway@apache.org>
2012-12-19 21:23:57 +0000

    QPID-4514: Remove obsolete cluster code: MessageStore
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1424134 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/qpid/broker/NullMessageStore.h
+++ qpid/cpp/src/qpid/broker/NullMessageStore.h
@@ -44,7 +44,6 @@ class QPID_BROKER_CLASS_EXTERN NullMessageStore : public MessageStore
     QPID_BROKER_EXTERN NullMessageStore();
 
     QPID_BROKER_EXTERN virtual bool init(const Options* options);
-    QPID_BROKER_EXTERN virtual void truncateInit(const bool pushDownStoreFiles = false);
     QPID_BROKER_EXTERN virtual std::auto_ptr<TransactionContext> begin();
     QPID_BROKER_EXTERN virtual std::auto_ptr<TPCTransactionContext> begin(const std::string& xid);
     QPID_BROKER_EXTERN virtual void prepare(TPCTransactionContext& txn);
