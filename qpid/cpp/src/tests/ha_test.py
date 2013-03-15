742052a4e3ca4693d2a991a0e21df8dde12c1b82
Kenneth Anthony Giusti <kgiusti@apache.org>
2013-03-15 13:36:09 +0000

    QPID-4649: use qpid uuid library instead of python
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1456950 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/src/tests/ha_test.py
+++ qpid/cpp/src/tests/ha_test.py
@@ -21,12 +21,11 @@
 import os, signal, sys, time, imp, re, subprocess, glob, random, logging, shutil, math, unittest, random
 import traceback
 from qpid.messaging import Message, NotFound, ConnectionError, ReceiverError, Connection, Timeout, Disposition, REJECTED, Empty
-from qpid.datatypes import uuid4
+from qpid.datatypes import uuid4, UUID
 from brokertest import *
 from threading import Thread, Lock, Condition
 from logging import getLogger, WARN, ERROR, DEBUG, INFO
 from qpidtoollibs import BrokerAgent
-from uuid import UUID
 
 log = getLogger(__name__)
 
