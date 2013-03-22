1be801114ec45d278e73dd71703f8ef5738bee2a
Alex Rudyy <orudyy@apache.org>
2013-03-22 13:13:16 +0000

    QPID-4596: Restrict the creation of PasswordCredentialManagingAuthenticationProvider to one instance
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1459755 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/broker/src/main/java/org/apache/qpid/server/model/adapter/AuthenticationProviderFactory.java
+++ qpid/java/broker/src/main/java/org/apache/qpid/server/model/adapter/AuthenticationProviderFactory.java
@@ -27,8 +27,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import org.apache.qpid.server.configuration.IllegalConfigurationException;
 import org.apache.qpid.server.model.AuthenticationProvider;
 import org.apache.qpid.server.model.Broker;
+import org.apache.qpid.server.model.PasswordCredentialManagingAuthenticationProvider;
 import org.apache.qpid.server.plugin.AuthenticationManagerFactory;
 import org.apache.qpid.server.plugin.QpidServiceLoader;
 import org.apache.qpid.server.security.auth.manager.AuthenticationManager;
@@ -68,6 +70,21 @@ public class AuthenticationProviderFactory
                 AuthenticationProviderAdapter<?> authenticationProvider;
                 if (manager instanceof PrincipalDatabaseAuthenticationManager)
                 {
+                    // a temporary restriction to prevent creation of several instances
+                    // of PlainPasswordFileAuthenticationProvider/Base64MD5PasswordFileAuthenticationProvider
+                    // due to current limitation of JMX management which cannot cope
+                    // with several user management MBeans as MBean type is used as a name.
+
+                    // TODO: Remove this check after fixing of JMX management
+                    for (AuthenticationProvider provider : broker.getAuthenticationProviders())
+                    {
+                        if (provider instanceof PasswordCredentialManagingAuthenticationProvider)
+                        {
+                            throw new IllegalConfigurationException("An authentication provider which can manage users alredy exists ["
+                                    + provider.getName() + "]. Only one instance is allowed.");
+                        }
+                    }
+
                     authenticationProvider = new PrincipalDatabaseAuthenticationManagerAdapter(id, broker,
                             (PrincipalDatabaseAuthenticationManager) manager, attributes, factory.getAttributeNames());
                 }
