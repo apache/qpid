2c820faa7bf393ec518de5a5254f0e851c19580b
Alex Rudyy <orudyy@apache.org>
2013-04-23 11:11:11 +0000

    QPID-4768: Resolve compatibility issues in web management console for old versions of IE and FF
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1470897 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/broker-plugins/management-http/src/main/java/resources/js/qpid/authorization/checkUser.js
+++ qpid/java/broker-plugins/management-http/src/main/java/resources/js/qpid/authorization/checkUser.js
@@ -19,60 +19,18 @@
  *
  */
 
-require(["dijit/form/DropDownButton",
-         "dijit/TooltipDialog",
-         "dijit/form/TextBox",
-         "dojo/_base/xhr",
-         "dojo/dom",
-         "dojo/dom-construct",
+require(["dojo/dom",
          "qpid/authorization/sasl",
-         "dojo/domReady!"], function(DropDownButton, TooltipDialog, TextBox, xhr, dom, domConstruct, sasl){
-
-var dialog = new TooltipDialog({
-    content:
-        '<strong><label for="username" style="display:inline-block;width:100px;">Username:</label></strong>' +
-        '<div data-dojo-type="dijit.form.TextBox" id="username"></div><br/>' +
-            '<strong><label for="pass" style="display:inline-block;width:100px;">Password:</label></strong>' +
-            '<div data-dojo-type="dijit.form.TextBox" type="password" id="pass"></div><br/>' +
-        '<button data-dojo-type="dijit.form.Button" type="submit" id="loginButton">Login</button>'
-});
-
-var button = new DropDownButton({
-    label: "Login",
-    dropDown: dialog
-});
-
-var usernameSpan = domConstruct.create("span", {
-    innerHTML: '<strong>User: </strong> <span id="authenticatedUser"></span><a href="logout">[logout]</a>',
-    style: { display: "none" }
-});
-
-
-var loginDiv = dom.byId("login");
-loginDiv.appendChild(usernameSpan);
-loginDiv.appendChild(button.domNode);
+         "dojo/domReady!"], function(dom, sasl){
 
 var updateUI = function updateUI(data)
 {
     if(data.user)
     {
-        dojo.byId("authenticatedUser").innerHTML = data.user;
-        dojo.style(button.domNode, {display: 'none'});
-        dojo.style(usernameSpan, {display: 'block'});
-    }
-    else
-    {
-        dojo.style(button.domNode, {display: 'block'});
-        dojo.style(usernameSpan, {display: 'none'});
+        dom.byId("authenticatedUser").innerHTML = data.user;
+        dojo.style(dom.byId("login"), {display: 'block'});
     }
 };
 
-dijit.byId("loginButton").on("click", function(){
-    sasl.authenticate(dojo.byId("username").value, dojo.byId("pass").value, updateUI);
-});
-
-dialog.startup();
-
 sasl.getUser(updateUI);
-
 });
