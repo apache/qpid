6e803305dce1c0036d4c859cd28e61a99ce57006
Ted Ross <tross@apache.org>
2013-06-28 13:42:12 +0000

    QPID-4967 - Added the Python routing engine and integrated its tests into ctest
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1497770 13f79535-47bb-0310-9956-ffa450edef68

---
+++ extras/dispatch/src/py/router/configuration.py
@@ -0,0 +1,47 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+class Configuration(object):
+  """
+  This module manages and holds the configuration and tuning parameters for a router.
+  """
+  def __init__(self, overrides={}):
+    ##
+    ## Load default values
+    ##
+    self.values = { 'hello_interval'      :  1.0,
+                    'hello_max_age'       :  3.0,
+                    'ra_interval'         : 30.0,
+                    'remote_ls_max_age'   : 60.0,
+                    'mobile_addr_max_age' : 60.0  }
+
+    ##
+    ## Apply supplied overrides
+    ##
+    for k, v in overrides.items():
+      self.values[k] = v
+
+  def __getattr__(self, key):
+    if key in self.values:
+      return self.values[key]
+    raise KeyError
+
+  def __repr__(self):
+    return "%r" % self.values
+
