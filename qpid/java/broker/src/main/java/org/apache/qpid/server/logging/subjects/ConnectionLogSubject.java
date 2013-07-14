e745b78b3f111daa2c76ddb9cd1afd4ca10417e1
Robert Godfrey <rgodfrey@apache.org>
2013-07-14 22:55:54 +0000

    QPID-4659 : [Java Broker] fix protocol version specific code in logging, subscriptions
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1503076 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/java/broker/src/main/java/org/apache/qpid/server/logging/subjects/ConnectionLogSubject.java
+++ qpid/java/broker/src/main/java/org/apache/qpid/server/logging/subjects/ConnectionLogSubject.java
@@ -20,34 +20,33 @@
  */
 package org.apache.qpid.server.logging.subjects;
 
-import org.apache.qpid.server.protocol.AMQProtocolSession;
+import java.text.MessageFormat;
+import org.apache.qpid.server.protocol.AMQConnectionModel;
 
 import static org.apache.qpid.server.logging.subjects.LogSubjectFormat.CONNECTION_FORMAT;
 import static org.apache.qpid.server.logging.subjects.LogSubjectFormat.SOCKET_FORMAT;
 import static org.apache.qpid.server.logging.subjects.LogSubjectFormat.USER_FORMAT;
 
-import java.text.MessageFormat;
-
 /** The Connection LogSubject */
 public class ConnectionLogSubject extends AbstractLogSubject
 {
 
-    public ConnectionLogSubject(AMQProtocolSession session)
+    // The Session this Actor is representing
+    private AMQConnectionModel _session;
+
+    public ConnectionLogSubject(AMQConnectionModel session)
     {
         _session = session;
     }
-
-    // The Session this Actor is representing
-    private AMQProtocolSession _session;
 
     // Used to stop re-creating the _logString when we reach our final format
     private boolean _upToDate = false;
 
     /**
      * Update the LogString as the Connection process proceeds.
-     * 
+     *
      * When the Session has an authorized ID add that to the string.
-     * 
+     *
      * When the Session then gains a Vhost add that to the string, at this point
      * we can set upToDate = true as the _logString will not need to be updated
      * from this point onwards.
@@ -56,44 +55,44 @@ public class ConnectionLogSubject extends AbstractLogSubject
     {
         if (!_upToDate)
         {
-            if (_session.getAuthorizedPrincipal() != null)
+            if (_session.getPrincipalAsString() != null)
             {
-                if (_session.getVirtualHost() != null)
+                if (_session.getVirtualHostName() != null)
                 {
                     /**
                      * LOG FORMAT used by the AMQPConnectorActor follows
                      * ConnectionLogSubject.CONNECTION_FORMAT :
                      * con:{0}({1}@{2}/{3})
-                     * 
+                     *
                      * Uses a MessageFormat call to insert the required values
                      * according to these indices:
-                     * 
+                     *
                      * 0 - Connection ID 1 - User ID 2 - IP 3 - Virtualhost
                      */
                     setLogString("[" + MessageFormat.format(CONNECTION_FORMAT,
-                                                            _session.getSessionID(),
-                                                            _session.getAuthorizedPrincipal().getName(),
-                                                            _session.getRemoteAddress(),
-                                                            _session.getVirtualHost().getName())
+                                                            _session.getConnectionId(),
+                                                            _session.getPrincipalAsString(),
+                                                            _session.getRemoteAddressString(),
+                                                            _session.getVirtualHostName())
                                  + "] ");
 
                     _upToDate = true;
-                } 
+                }
                 else
                 {
                     setLogString("[" + MessageFormat.format(USER_FORMAT,
-                                                            _session.getSessionID(),
-                                                            _session.getAuthorizedPrincipal().getName(),
-                                                            _session.getRemoteAddress())
+                                                            _session.getConnectionId(),
+                                                            _session.getPrincipalAsString(),
+                                                            _session.getRemoteAddressString())
                                  + "] ");
 
                 }
-            } 
+            }
             else
             {
                     setLogString("[" + MessageFormat.format(SOCKET_FORMAT,
-                                                            _session.getSessionID(),
-                                                            _session.getRemoteAddress())
+                                                            _session.getConnectionId(),
+                                                            _session.getRemoteAddressString())
                                  + "] ");
             }
         }
