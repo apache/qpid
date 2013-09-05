f941fdfa1bc031e613822b49d6ca9b197e4ce311
Ted Ross <tross@apache.org>
2013-09-05 14:41:30 +0000

    QPID-5068 - Fixed a bug in the replacement of delivery annotations.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1520334 13f79535-47bb-0310-9956-ffa450edef68

--- extras/dispatch/src/message_private.h
+++ extras/dispatch/src/message_private.h
@@ -45,10 +45,11 @@
  */
 
 typedef struct {
-    dx_buffer_t *buffer;  // Buffer that contains the first octet of the field, null if the field is not present
-    size_t       offset;  // Offset in the buffer to the first octet
-    size_t       length;  // Length of the field or zero if unneeded
-    int          parsed;  // non-zero iff the buffer chain has been parsed to find this field
+    dx_buffer_t *buffer;     // Buffer that contains the first octet of the field, null if the field is not present
+    size_t       offset;     // Offset in the buffer to the first octet
+    size_t       length;     // Length of the field or zero if unneeded
+    size_t       hdr_length; // Length of the field's header (not included in the length of the field)
+    int          parsed;     // non-zero iff the buffer chain has been parsed to find this field
 } dx_field_location_t;
 
 
