6334fbadec3b3ec39507272509276b14dc986fef
Alex Rudyy <orudyy@apache.org>
2013-09-14 11:29:25 +0000

    QPID-5138: Add preferences REST interfaces to get, set and delete preferences
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1523221 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/broker-plugins/management-http/src/main/java/org/apache/qpid/server/management/plugin/servlet/rest/PreferencesServlet.java
@@ -0,0 +1,109 @@
+package org.apache.qpid.server.management.plugin.servlet.rest;
+
+import java.io.IOException;
+import java.net.SocketAddress;
+import java.security.Principal;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.security.auth.Subject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.qpid.server.management.plugin.HttpManagementUtil;
+import org.apache.qpid.server.model.AuthenticationProvider;
+import org.apache.qpid.server.model.Broker;
+import org.apache.qpid.server.model.PreferencesProvider;
+import org.apache.qpid.server.security.auth.AuthenticatedPrincipal;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class PreferencesServlet extends AbstractServlet
+{
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGetWithSubjectAndActor(HttpServletRequest request, HttpServletResponse response) throws IOException,
+            ServletException
+    {
+        PreferencesProvider preferencesProvider = getPreferencesProvider(request);
+        String userName = getAuthenticatedUserName(request);
+        Map<String, Object> preferences = preferencesProvider.getPreferences(userName);
+        if (preferences == null)
+        {
+            preferences = Collections.<String, Object>emptyMap();
+        }
+        sendJsonResponse(preferences, response);
+    }
+
+    /*
+     * replace preferences
+     */
+    @Override
+    protected void doPutWithSubjectAndActor(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        PreferencesProvider preferencesProvider = getPreferencesProvider(request);
+        String userName = getAuthenticatedUserName(request);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        @SuppressWarnings("unchecked")
+        Map<String, Object> newPreferences = mapper.readValue(request.getInputStream(), LinkedHashMap.class);
+
+        preferencesProvider.deletePreferences(userName);
+        Map<String, Object> preferences = preferencesProvider.setPreferences(userName, newPreferences);
+        if (preferences == null)
+        {
+            preferences = Collections.<String, Object>emptyMap();
+        }
+        sendJsonResponse(preferences, response);
+    }
+
+    /*
+     * update preferences
+     */
+    @Override
+    protected void doPostWithSubjectAndActor(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        PreferencesProvider preferencesProvider = getPreferencesProvider(request);
+        String userName = getAuthenticatedUserName(request);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        @SuppressWarnings("unchecked")
+        Map<String, Object> newPreferences = mapper.readValue(request.getInputStream(), LinkedHashMap.class);
+        Map<String, Object> preferences = preferencesProvider.setPreferences(userName, newPreferences);
+        if (preferences == null)
+        {
+            preferences = Collections.<String, Object>emptyMap();
+        }
+        sendJsonResponse(preferences, response);
+    }
+
+    private String getAuthenticatedUserName(HttpServletRequest request)
+    {
+        Subject subject = getAuthorisedSubject(request);
+        Principal principal = AuthenticatedPrincipal.getAuthenticatedPrincipalFromSubject(subject);
+        return principal.getName();
+    }
+
+    private AuthenticationProvider getAuthenticationProvider(HttpServletRequest request)
+    {
+        Broker broker = getBroker();
+        SocketAddress localAddress = HttpManagementUtil.getSocketAddress(request);
+        return broker.getAuthenticationProvider(localAddress);
+    }
+
+    private PreferencesProvider getPreferencesProvider(HttpServletRequest request)
+    {
+        PreferencesProvider preferencesProvider = getAuthenticationProvider(request).getPreferencesProvider();
+        if (preferencesProvider == null)
+        {
+            throw new IllegalStateException("Preferences provider is not configured");
+        }
+        return preferencesProvider;
+    }
+}
