02b136e430d0b8ccfb183d60465c640d99bc8fa9
Darryl L. Pierce <mcpierce@apache.org>
2013-10-15 13:10:38 +0000

    Revert "QPID-5213: Fix QMF code to use qpid_messaging rather than cqpid."
    This reverts commit 067e9aa1656b9f92e5910bbb6d530f9df6d0dad3.
    
    It had the wrong JIRA number in the commit.
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1532323 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/bindings/qmf2/python/qmf2.py
+++ qpid/cpp/bindings/qmf2/python/qmf2.py
@@ -18,7 +18,7 @@
 #
 
 import cqmf2
-import qpid_messaging
+import cqpid
 from threading import Thread
 import time
 
@@ -124,7 +124,7 @@ class AgentHandler(Thread):
   def run(self):
     event = cqmf2.AgentEvent()
     while self.__running:
-      valid = self.__agent._impl.nextEvent(event, qpid_messaging.Duration.SECOND)
+      valid = self.__agent._impl.nextEvent(event, cqpid.Duration.SECOND)
       if valid and self.__running:
         if event.getType() == cqmf2.AGENT_METHOD:
           self.method(event, event.getMethodName(), event.getArguments(), event.getArgumentSubtypes(),
@@ -156,7 +156,7 @@ class ConsoleHandler(Thread):
   def run(self):
     event = cqmf2.ConsoleEvent()
     while self.__running:
-      valid = self.__session._impl.nextEvent(event, qpid_messaging.Duration.SECOND)
+      valid = self.__session._impl.nextEvent(event, cqpid.Duration.SECOND)
       if valid and self.__running:
         if event.getType() == cqmf2.CONSOLE_AGENT_ADD:
           self.agentAdded(Agent(event.getAgent()))
@@ -434,7 +434,7 @@ class Agent(object):
       q_arg = q._impl
     else:
       q_arg = q
-    dur = qpid_messaging.Duration(qpid_messaging.Duration.SECOND.getMilliseconds() * timeout)
+    dur = cqpid.Duration(cqpid.Duration.SECOND.getMilliseconds() * timeout)
     result = self._impl.query(q_arg, dur)
     if result.getType() == cqmf2.CONSOLE_EXCEPTION:
       raise Exception(Data(result.getData(0)))
@@ -449,7 +449,7 @@ class Agent(object):
   def loadSchemaInfo(self, timeout=30):
     """
     """
-    dur = qpid_messaging.Duration(qpid_messaging.Duration.SECOND.getMilliseconds() * timeout)
+    dur = cqpid.Duration(cqpid.Duration.SECOND.getMilliseconds() * timeout)
     self._impl.querySchema(dur)
 
   def getPackages(self):
@@ -473,7 +473,7 @@ class Agent(object):
   def getSchema(self, schemaId, timeout=30):
     """
     """
-    dur = qpid_messaging.Duration(qpid_messaging.Duration.SECOND.getMilliseconds() * timeout)
+    dur = cqpid.Duration(cqpid.Duration.SECOND.getMilliseconds() * timeout)
     return Schema(self._impl.getSchema(schemaId._impl, dur))
 
   ## TODO: Async query
@@ -555,7 +555,7 @@ class Data(object):
     return Agent(self._impl.getAgent())
 
   def update(self, timeout=5):
-    dur = qpid_messaging.Duration(qpid_messaging.Duration.SECOND.getMilliseconds() * timeout)
+    dur = cqpid.Duration(cqpid.Duration.SECOND.getMilliseconds() * timeout)
     agent = self._impl.getAgent()
     query = cqmf2.Query(self._impl.getAddr())
     result = agent.query(query, dur)
@@ -590,7 +590,7 @@ class Data(object):
     timeout = 30
     if '_timeout' in kwargs:
       timeout = kwargs['_timeout']
-    dur = qpid_messaging.Duration(qpid_messaging.Duration.SECOND.getMilliseconds() * timeout)
+    dur = cqpid.Duration(cqpid.Duration.SECOND.getMilliseconds() * timeout)
 
     ##
     ## Get the list of arguments from the schema, isolate those that are IN or IN_OUT,
