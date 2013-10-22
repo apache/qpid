0f04effbc2e491f2cd697ba411501335750d50b8
Gordon Sim <gsim@apache.org>
2013-10-22 10:43:19 +0000

    QPID-5232: fix handling of autodelete for subscription queues of default reliability
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1534593 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/broker/amqp/Session.cpp
+++ cpp/src/qpid/broker/amqp/Session.cpp
@@ -396,7 +396,7 @@ void Session::setupOutgoing(pn_link_t* link, pn_terminus_t* source, const std::s
         authorise.access(node.exchange);//do separate access check before trying to create the queue
         bool shared = is_capability_requested(SHARED, pn_terminus_capabilities(source));
         bool durable = pn_terminus_get_durability(source);
-        bool autodelete = !durable && pn_link_remote_snd_settle_mode(link) == PN_SND_SETTLED;
+        bool autodelete = !durable && pn_link_remote_snd_settle_mode(link) != PN_SND_UNSETTLED;
         QueueSettings settings(durable, autodelete);
         std::string altExchange;
         if (node.topic) {
