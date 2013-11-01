a46b490a47539344a6ef0264c1104c6fdd029c9a
Gordon Sim <gsim@apache.org>
2013-11-01 16:55:59 +0000

    QPID-5286: replenish credit when using get
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1537985 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/messaging/Receiver.cpp
+++ cpp/src/qpid/messaging/Receiver.cpp
@@ -38,7 +38,11 @@ Receiver::Receiver(ReceiverImpl* impl) { PI::ctor(*this, impl); }
 Receiver::Receiver(const Receiver& s) : Handle<ReceiverImpl>() { PI::copy(*this, s); }
 Receiver::~Receiver() { PI::dtor(*this); }
 Receiver& Receiver::operator=(const Receiver& s) { return PI::assign(*this, s); }
-bool Receiver::get(Message& message, Duration timeout) { return impl->get(message, timeout); }
+bool Receiver::get(Message& message, Duration timeout)
+{
+    MessageImplAccess::get(message).clear();
+    return impl->get(message, timeout);
+}
 Message Receiver::get(Duration timeout) { return impl->get(timeout); }
 bool Receiver::fetch(Message& message, Duration timeout)
 {
