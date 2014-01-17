88a8ed7c656e5863c638b2189294e19a22a6118c
Pavel Moravec <pmoravec@apache.org>
2014-01-17 12:32:29 +0000

    QPID-5486: Creating paged queue can overwrite existing qpidd files
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1559090 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/qpid/sys/posix/MemoryMappedFile.cpp
+++ cpp/src/qpid/sys/posix/MemoryMappedFile.cpp
@@ -65,8 +65,7 @@ std::string MemoryMappedFile::open(const std::string& name, const std::string& d
 {
     std::string path = getFileName(name, directory);
 
-    if (access(path.c_str(), F_OK) != -1) throw qpid::Exception(QPID_MSG("Attempting to re-write file " << path << " for paged queue \"" << name << "\""));
-    int flags = O_CREAT | O_TRUNC | O_RDWR;
+    int flags = O_CREAT | O_EXCL | O_RDWR;
     int fd = ::open(path.c_str(), flags, S_IRUSR | S_IWUSR);
     if (fd == -1) throw qpid::Exception(QPID_MSG("Failed to open memory mapped file " << path << ": " << qpid::sys::strError(errno) << " [flags=" << flags << "]"));
     state->fd = fd;
