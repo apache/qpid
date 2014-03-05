53232683de1ec248e46aeaff70ce48c02fcd0381
Robert Godfrey <rgodfrey@apache.org>
2014-03-05 16:29:48 +0000

    NO-JIRA : formatting of license above package statement
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1574564 13f79535-47bb-0310-9956-ffa450edef68

--- java/broker-core/src/main/java/org/apache/qpid/server/model/ManagedAttribute.java
+++ java/broker-core/src/main/java/org/apache/qpid/server/model/ManagedAttribute.java
@@ -1,4 +1,3 @@
-package org.apache.qpid.server.model;
 /*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
@@ -19,6 +18,7 @@ package org.apache.qpid.server.model;
  * under the License.
  *
  */
+package org.apache.qpid.server.model;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
