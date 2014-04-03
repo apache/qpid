0120762b90c8709afea67143aa2dc3152ef27554
Kim van der Riet <kpvdr@apache.org>
2014-04-03 20:24:03 +0000

    QPID-5661: Set default cmake build to exclude linearstore
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk/qpid@1584379 13f79535-47bb-0310-9956-ffa450edef68

--- cpp/src/linearstore.cmake
+++ cpp/src/linearstore.cmake
@@ -39,8 +39,8 @@ else (DEFINED linearstore_force)
                 #
                 # allow linearstore to be built
                 #
-                message(STATUS "BerkeleyDB for C++ and libaio found, Linearstore support enabled")
-                set (linearstore_default ON)
+                message(STATUS "BerkeleyDB for C++ and libaio found, Linearstore support may be enabled (currently experimental and disabled by default)")
+                set (linearstore_default OFF) # Temporarily disabled
             else (HAVE_AIO AND HAVE_AIO_H)
                 if (NOT HAVE_AIO)
                     message(STATUS "Linearstore requires libaio which is absent.")
