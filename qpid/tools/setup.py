dba548b498b8718242353fb3b492d5e29b31e9e9
Justin Ross <jross@apache.org>
2014-08-07 20:26:37 +0000

    QPID-5813: Advance the branch release version to 0.30
    git-svn-id: https://svn.apache.org/repos/asf/qpid/branches/0.30@1616588 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/tools/setup.py
+++ qpid/tools/setup.py
@@ -34,7 +34,7 @@ containing QMF commands to the Qpid broker.
 """
 
 setup(name="qpid-tools",
-      version="0.29",
+      version="0.30",
       author="Apache Qpid",
       author_email="dev@qpid.apache.org",
       package_dir={'' : 'src/py'},
