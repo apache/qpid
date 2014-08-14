apache/qpid

commit 6bed18c3cccaae9c5ef2e8f29aab473a3b44bfca

QPID-5985: check for incoming messages - without waiting - even when timout is 0
git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1617256 13f79535-47bb-0310-9956-ffa450edef68

qpid/cpp/src/qpid/client/amqp0_10/IncomingMessages.cpp
@@ -175,7 +175,7 @@ bool IncomingMessages::getNextDestination(std::string& destination, qpid::sys::D
 {
     sys::Mutex::ScopedLock l(lock);
     AbsTime deadline(AbsTime::now(), timeout);
-    while (received.empty() && AbsTime::now() < deadline) {
+    while (received.empty()) {
         if (inUse) {
             //someone is already waiting on the sessions incoming queue
             lock.wait(deadline);

qpid/cpp/src/tests/MessagingSessionTests.cpp
@@ -1450,6 +1450,31 @@ QPID_AUTO_TEST_CASE(testSessionCheckError)
     }
 }
 
+QPID_AUTO_TEST_CASE(testImmediateNextReceiver)
+{
+    QueueFixture fix;
+    Sender sender = fix.session.createSender(fix.queue);
+    Message out("test message");
+    sender.send(out);
+    fix.session.createReceiver(fix.queue).setCapacity(1);
+    Receiver next;
+    qpid::sys::AbsTime start = qpid::sys::now();
+    try {
+        while (!fix.session.nextReceiver(next, qpid::messaging::Duration::IMMEDIATE)) {
+            qpid::sys::Duration running(start, qpid::sys::now());
+            if (running > 5*qpid::sys::TIME_SEC) {
+                throw qpid::types::Exception("Timed out spinning on nextReceiver(IMMEDIATE)");
+            }
+        }
+        Message in;
+        BOOST_CHECK(next.fetch(in, qpid::messaging::Duration::IMMEDIATE));
+        BOOST_CHECK_EQUAL(in.getContent(), out.getContent());
+        next.close();
+    } catch (const std::exception& e) {
+        BOOST_FAIL(e.what());
+    }
+}
+
 QPID_AUTO_TEST_SUITE_END()
 
 }} // namespace qpid::tests

commit 3a7a19b755c5eade585956b61c75dfb29e7ac526

QPID-5985: fixed regression introduced by previous commit (r1617256), ensuring that call does not block indefinitely
git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1617924 13f79535-47bb-0310-9956-ffa450edef68

qpid/cpp/src/qpid/client/amqp0_10/IncomingMessages.cpp
@@ -186,6 +186,7 @@ bool IncomingMessages::getNextDestination(std::string& destination, qpid::sys::D
             //wait for an incoming message
             wait(timeout == qpid::sys::TIME_INFINITE ? qpid::sys::TIME_INFINITE : qpid::sys::Duration(AbsTime::now(), deadline));
         }
+        if (!(AbsTime::now() < deadline)) break;
     }
     if (!received.empty()) {
         destination = received.front()->as<MessageTransferBody>()->getDestination();

qpid/cpp/src/tests/MessagingSessionTests.cpp
@@ -1465,11 +1465,26 @@ QPID_AUTO_TEST_CASE(testImmediateNextReceiver)
             if (running > 5*qpid::sys::TIME_SEC) {
                 throw qpid::types::Exception("Timed out spinning on nextReceiver(IMMEDIATE)");
             }
+            qpid::sys::usleep(1); // for valgrind
         }
         Message in;
         BOOST_CHECK(next.fetch(in, qpid::messaging::Duration::IMMEDIATE));
         BOOST_CHECK_EQUAL(in.getContent(), out.getContent());
         next.close();
+    } catch (const std::exception& e) {
+        BOOST_FAIL(e.what());
+    }
+}
+
+QPID_AUTO_TEST_CASE(testImmediateNextReceiverNoMessage)
+{
+    QueueFixture fix;
+    Receiver r = fix.session.createReceiver(fix.queue);
+    r.setCapacity(1);
+    Receiver next;
+    try {
+        BOOST_CHECK(!fix.session.nextReceiver(next, qpid::messaging::Duration::IMMEDIATE));
+        r.close();
     } catch (const std::exception& e) {
         BOOST_FAIL(e.what());
     }