f609ee876788ee9198907c3a7594e4c69a468665
Justin Ross <jross@apache.org>
2016-05-26 21:52:56 +0000

    QPID-7281: Relocate the find_package invocation for python; use the resulting config to run the test-time dependency check
    git-svn-id: https://svn.apache.org/repos/asf/qpid/trunk@1745657 13f79535-47bb-0310-9956-ffa450edef68

--- qpid/cpp/CTestCustom.cmake
+++ qpid/cpp/CTestCustom.cmake
@@ -1,1 +1,1 @@
-set(CTEST_CUSTOM_PRE_TEST "${CMAKE_BINARY_DIR}/src/tests/check_dependencies.py")
+set(CTEST_CUSTOM_PRE_TEST "${PYTHON_EXECUTABLE} ${CMAKE_BINARY_DIR}/src/tests/check_dependencies.py")
